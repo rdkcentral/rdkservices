6674ccdac0dae8a3a42dcffa36d7b5c7de695ffd
Sergey Borushevsky <Sergey_Borushevsky2@cable.comcast.com>
2020-07-15 19:53:26 +0000

    DELIA-44326 : [Thunder][Dis[laySettings] - HDMI Audio Delay Issues
    Fixed exception handling fromp stoi call.

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -1892,9 +1892,9 @@ namespace WPEFramework {
 			int audioDelayMs = 0;
             try {
                 audioDelayMs = stoi(sAudioDelayMs);
-            } catch (const device::Exception& err) {
-               LOG_DEVICE_EXCEPTION1(sAudioDelayMs); 
-               returnResponse(false);
+            } catch (const std::exception &err) {
+                LOGERR("Failed to parse audioDelay '%s'", sAudioDelayMs.c_str());
+                returnResponse(false);
             }
 
             bool success = true;
@@ -2008,10 +2008,10 @@ namespace WPEFramework {
             {
                 audioDelayOffsetMs = stoi(sAudioDelayOffsetMs);
             }
-            catch (const device::Exception& err)
+            catch (const std::exception &err)
             {
-               LOG_DEVICE_EXCEPTION1(sAudioDelayOffsetMs); 
-               returnResponse(false);
+                LOGERR("Failed to parse audioDelayOffset '%s'", sAudioDelayOffsetMs.c_str());
+                returnResponse(false);
             }
 
             bool success = true;
