234af2abf2c03cb0b78f9cda0e284932133ff24f
Stephen Foulds <stephen.foulds@consult.red>
2020-10-06 13:28:03 +0000

    RFC fix

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -268,6 +268,7 @@ namespace WPEFramework {
 
             mCurrentService = service;
             CompositorController::setEventListener(mEventListener);
+#ifdef RFC_ENABLED
             RFC_ParamData_t param;
             bool ret = getRFCConfig("Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Power.UserInactivityNotification.Enable", param);
             if (true == ret && param.type == WDMP_BOOLEAN && (strncasecmp(param.value,"true",4) == 0))
@@ -286,6 +287,7 @@ namespace WPEFramework {
                 }
               }
             }
+#endif
 
             service->Register(mClientsMonitor);
 
