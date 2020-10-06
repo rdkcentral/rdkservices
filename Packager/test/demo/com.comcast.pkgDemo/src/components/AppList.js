505a80ccd8d23a9a9a8175a0abc7b231f8b15e13
FitzerIRL <Hugh_Fitzpatrick@hotmail.com>
2020-10-06 11:30:52 -0400

    Adding headers

--- Packager/test/demo/com.comcast.pkgDemo/src/components/AppList.js
+++ Packager/test/demo/com.comcast.pkgDemo/src/components/AppList.js
@@ -1,9 +1,27 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import StoreTile   from "./StoreTile";
-import AppTile from "./AppTile";
+import StoreTile from "./StoreTile";
+import AppTile   from "./AppTile";
 
-export default class AppList extends lng.Component {
-
+export default class AppList extends lng.Component
+{
     static _template() {
       return {
           flex: {direction: 'row', padding: 20, paddingTop: 50, wrap: false },
