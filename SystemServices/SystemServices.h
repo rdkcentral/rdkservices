1e1b5ccdd13ada8ac3eb447bb2b1bf9dde210e4b
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2020-12-04 17:17:48 +0000

    RDK-30602: Added Support for querying and publishing the Secure Timer Status.
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>

--- SystemServices/SystemServices.h
+++ SystemServices/SystemServices.h
@@ -52,6 +52,10 @@
 #define EVT_ONMACADDRESSRETRIEVED         "onMacAddressesRetreived"
 #define EVT_ONREBOOTREQUEST               "onRebootRequest"
 
+#ifdef ENABLE_SYSTIMEMGR_SUPPORT
+#define EVT_ONTIMESTATUSCHANGED           "onTimeStatusChanged"
+#endif// ENABLE_SYSTIMEMGR_SUPPORT
+
 namespace WPEFramework {
     namespace Plugin {
 
@@ -138,6 +142,9 @@ namespace WPEFramework {
                 void onFirmwareUpdateStateChange(int state);
                 void onTemperatureThresholdChanged(string thresholdType,
                         bool exceed, float temperature);
+#ifdef ENABLE_SYSTIMEMGR_SUPPORT
+                void onTimeStatusChanged(string timequality,string timesource, string utctime);
+#endif// ENABLE_SYSTIMEMGR_SUPPORT
                 void onRebootRequest(string reason);
                 /* Events : End */
 
@@ -155,6 +162,10 @@ namespace WPEFramework {
                 uint32_t getDevicePowerState(const JsonObject& parameters,JsonObject& response);
                 uint32_t setDevicePowerState(const JsonObject& parameters,JsonObject& response);
 #endif /* HAS_API_SYSTEM && HAS_API_POWERSTATE */
+
+#ifdef ENABLE_SYSTIMEMGR_SUPPORT
+                uint32_t getSystemTimeStatus(const JsonObject& parameters,JsonObject& response);
+#endif// ENABLE_SYSTIMEMGR_SUPPORT
                 uint32_t isRebootRequested(const JsonObject& parameters,JsonObject& response);
                 uint32_t setGZEnabled(const JsonObject& parameters,JsonObject& response);
                 uint32_t isGZEnabled(const JsonObject& parameters,JsonObject& response);
