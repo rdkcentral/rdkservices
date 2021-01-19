fd21a8a426eff9bebfebdd77ffbf3d4d9799e3b9
madanagopalt <madanagopal123@gmail.com>
2021-01-20 03:59:51 +0530

    RDK-30650: wildcard * support for removekeyintercept (#617)
    Co-authored-by: mthiru270 <madanagopal_thirumalai@comcast.com>
    Co-authored-by: Michael Fiess <10201495+mfiess@users.noreply.github.com>

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -891,7 +891,17 @@ namespace WPEFramework {
                 //optional param?
                 const JsonArray modifiers = parameters.HasLabel("modifiers") ? parameters["modifiers"].Array() : JsonArray();
 
-                const uint32_t keyCode = parameters["keyCode"].Number();
+                uint32_t keyCode = parameters["keyCode"].Number();
+                // check for * parameter
+                JsonValue keyCodeValue = parameters["keyCode"];
+                if (keyCodeValue.Content() == JsonValue::type::STRING)
+                {
+                    std::string keyCodeStringValue = parameters["keyCode"].String();
+                    if (keyCodeStringValue.compare("*") == 0)
+                    {
+                        keyCode = 255;
+                    }
+                }
                 string client;
                 if (parameters.HasLabel("client"))
                 {
