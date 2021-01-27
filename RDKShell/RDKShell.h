dc7ca483ca307ce1f8c4b2b179b336132276165c
Michael Fiess <10201495+mfiess@users.noreply.github.com>
2021-01-27 13:23:58 -0500

    Revert "RDKTV-95 - propagate visibility setting to web browser (#682)" (#751)
    This reverts commit 5af5efc47519f40833e5cad8ac7eb48442b8e06b.

--- RDKShell/RDKShell.h
+++ RDKShell/RDKShell.h
@@ -307,10 +307,6 @@ namespace WPEFramework {
             //std::mutex m_callMutex;
         };
 
-        struct PluginData
-        {
-          std::string mClassName;
-        };
         class PluginStateChangeData
         {
            public:
