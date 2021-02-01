8e0b878d45b0c02a7154f13be597592e8d23f8ef
mthiru270 <madanagopal_thirumalai@comcast.com>
2021-02-01 08:07:02 +0530

    RDKTV-1350: fix rdkshell crash after createdisplay

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -2033,7 +2033,9 @@ namespace WPEFramework {
                     joParams.ToString(strParams);
                     joResult.ToString(strResult);
                     launchType = RDKShellLaunchType::CREATE;
+                    gRdkShellMutex.lock();
                     RdkShell::CompositorController::createDisplay(callsign, displayName, width, height);
+                    gRdkShellMutex.unlock();
                 }
 
                 WPEFramework::Core::JSON::String configString;
