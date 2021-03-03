448e5b29eea789a3cde6cb0c79d03b063c05bace
mthiru270 <madanagopal_thirumalai@comcast.com>
2021-03-03 13:31:14 +0530

    RDKTV-2334 - reset connection before deleting state data

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -265,6 +265,7 @@ namespace WPEFramework {
                         PluginStateChangeData* stateChangeData = pluginStateChangeEntry->second;
                         if (nullptr != stateChangeData)
                         {
+                            stateChangeData->resetConnection();
                             delete stateChangeData;
                         }
                         pluginStateChangeEntry->second = nullptr;
@@ -4422,6 +4423,11 @@ namespace WPEFramework {
         void PluginStateChangeData::enableLaunch(bool enable)
         {
             mLaunchEnabled = enable;
+        }
+
+        void PluginStateChangeData::resetConnection()
+        {
+            mPluginConnection = nullptr;
         }
 
         void PluginStateChangeData::onStateChangeEvent(const JsonObject& params)
