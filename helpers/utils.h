88aa3c628e6a6e9ae69feee4d9e0f955490b656a
ANANTHMARIMUTHU <Anooj_Cheriyan@comcast.com>
2021-04-01 15:03:08 +0000

    RDKTV-3860: WPEFramework_crashes_setBassEnhancer
    Reason for change:
    WPEFramework crashes setBassEnhancer
    valid int check added
    Test Procedure: None
    Risks: Low
    
    Change-Id: I1e61e4a1241e87072963ef4f493dae9c380eade1
    Signed-off-by:Anooj Cheriyan <Anooj_Cheriyan@comcast.com>
    (cherry picked from commit 308b76315f6d51897d7533d4a68b81d40d7e8872)

--- helpers/utils.h
+++ helpers/utils.h
@@ -349,6 +349,7 @@ namespace Utils
     bool isPluginActivated(const char* callSign);
 
     bool getRFCConfig(char* paramName, RFC_ParamData_t& paramOutput);
+    bool isValidInt(char* x);
 
     //class for std::thread RAII
     class ThreadRAII 
