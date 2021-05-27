1122e70a61ab25f0d59b13fb535f348fbaca1d71
ANANTHMARIMUTHU <Nikita_Poltorapavlo2@cable.comcast.com>
2021-05-27 19:52:44 +0300

    RDKTV-7140 : Include .ts Files in org.rdk.UsbAccess.1.getFileList
    Reason for change: Extend regex.
    Test Procedure: Invoke api above.
    Risks: Low
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>

--- UsbAccess/UsbAccess.cpp
+++ UsbAccess/UsbAccess.cpp
@@ -27,7 +27,7 @@ const string WPEFramework::Plugin::UsbAccess::LINK_PATH = "/tmp/usbdrive";
 const string WPEFramework::Plugin::UsbAccess::EVT_ON_USB_MOUNT_CHANGED = "onUSBMountChanged";
 const string WPEFramework::Plugin::UsbAccess::REGEX_BIN = "[\\w-]*\\.{0,1}[\\w-]*\\.bin";
 const string WPEFramework::Plugin::UsbAccess::REGEX_FILE =
-        "[\\w-]*\\.{0,1}[\\w-]*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc)";
+        "[\\w-]*\\.{0,1}[\\w-]*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc|ts)";
 const string WPEFramework::Plugin::UsbAccess::PATH_DEVICE_PROPERTIES = "/etc/device.properties";
 const std::list<string> WPEFramework::Plugin::UsbAccess::ADDITIONAL_FW_PATHS {"UsbTestFWUpdate", "UsbProdFWUpdate"};
 
