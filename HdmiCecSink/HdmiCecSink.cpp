9b4bad739fa8a991d1a5074b341df3e8c2efa053
Karthicksky <85236562+Karthicksky@users.noreply.github.com>
2021-06-08 18:00:11 +0530

    RDKTV-7170 : DEEPSLEEP and CEC wake up
    Reason for change: RDK request active source should come from app
    Test Procedure: deepsleep wakeup, deepsleep cec wakeup
    Risks: Create None
    Signed-off-by: KRN11 karthickkumar.ravisankar@sky.uk"

--- HdmiCecSink/HdmiCecSink.cpp
+++ HdmiCecSink/HdmiCecSink.cpp
@@ -716,11 +716,7 @@ namespace WPEFramework
 					{
 						_instance->deviceList[_instance->m_logicalAddressAllocated].m_powerStatus = PowerStatus(powerState);
 
-						if ( powerState == DEVICE_POWER_STATE_ON )
-						{
-							HdmiCecSink::_instance->onPowerStateON();
-						}
-						else
+						if ( powerState != DEVICE_POWER_STATE_ON )
 						{
 							HdmiCecSink::_instance->m_currentActiveSource = -1;
 						}
@@ -734,14 +730,6 @@ namespace WPEFramework
            }
        }
 
-	  void HdmiCecSink::onPowerStateON()
-       {
-       		if ( powerState == DEVICE_POWER_STATE_ON )
-       		{
-       			/*while wakeup From Standby, Ask for Active Source*/
-				_instance->requestActiveSource(); 
-       		}
-       }
 
 	  void HdmiCecSink::sendStandbyMessage()
       {
@@ -2352,11 +2340,6 @@ namespace WPEFramework
 						_instance->smConnection->addFrameListener(_instance->msgFrameListener);
 						_instance->smConnection->sendTo(LogicalAddress(LogicalAddress::BROADCAST), 
 								MessageEncoder().encode(ReportPhysicalAddress(physical_addr, _instance->deviceList[_instance->m_logicalAddressAllocated].m_deviceType)), 5000);	
-
-						 if ( powerState == DEVICE_POWER_STATE_ON )
-						 {
-							_instance->requestActiveSource(); 
-						 }
 
 						_instance->m_sleepTime = HDMICECSINK_PING_INTERVAL_MS;
 						_instance->m_pollThreadState = POLL_THREAD_STATE_IDLE;
