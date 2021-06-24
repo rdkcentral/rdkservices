8c68f70584f92184a81f078510382e5653149c40
rpadin319 <renjith_warier@comcast.com>
2021-06-24 18:02:34 +0000

    RDK-32485 - method to check if launch FTA at startup is enabled

--- RDKShell/RDKShell.h
+++ RDKShell/RDKShell.h
@@ -124,6 +124,7 @@ namespace WPEFramework {
             static const string RDKSHELL_METHOD_GET_LAST_WAKEUP_KEY;
             static const string RDKSHELL_METHOD_ENABLE_LOGS_FLUSHING;
             static const string RDKSHELL_METHOD_GET_LOGS_FLUSHING_ENABLED;
+            static const string RDKSHELL_METHOD_CHECK_FOR_FTA_LAUNCH;
 
             // events
             static const string RDKSHELL_EVENT_ON_USER_INACTIVITY;
@@ -223,6 +224,7 @@ namespace WPEFramework {
             uint32_t getLastWakeupKeyWrapper(const JsonObject& parameters, JsonObject& response);
             uint32_t enableLogsFlushingWrapper(const JsonObject& parameters, JsonObject& response);
             uint32_t getLogsFlushingEnabledWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t willLaunchFactoryAppAtStartupWrapper(const JsonObject& parameters, JsonObject& response);
 
         private/*internal methods*/:
             RDKShell(const RDKShell&) = delete;
