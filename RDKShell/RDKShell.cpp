b5884dcd63327acd2c783f89e92193a64eadafc1
Michael Fiess <10201495+mfiess@users.noreply.github.com>
2021-06-30 16:31:54 -0400

    Revert "RDK-32485 - don't create display for residentApp if FTA is set to launch (#1386)"
    This reverts commit e8c7b3a17b00dbf6559f05a6755000bbc75c45ab.

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -570,11 +570,6 @@ namespace WPEFramework {
                    {
                        return;
                    }
-                   if ((service->Callsign() == RESIDENTAPP_CALLSIGN) && sFactoryModeStart && sFactoryModeBlockResidentApp && !sForceResidentAppLaunch)
-                   {
-                      std::cout << "skip create display for resident app if FTA is set to launch"<<std::endl;
-                      return;
-                   }
                    JsonObject serviceConfig = JsonObject(configLine.c_str());
                    if (serviceConfig.HasLabel("clientidentifier"))
                    {
