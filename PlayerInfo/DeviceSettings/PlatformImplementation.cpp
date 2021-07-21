ca9aba11ac5fd98b2366e838734b34203595ff1c
Michal Kucharczyk <mkucharczyk.contractor@libertyglobal.com>
2021-07-21 17:14:57 +0200

    ONEM-21356: playerinfo: iarm's device::maneger init added
    (cherry picked from commit 3ad69518aa1e1c4d3dc1371e1b6b016da2a4c1a6)

--- PlayerInfo/DeviceSettings/PlatformImplementation.cpp
+++ PlayerInfo/DeviceSettings/PlatformImplementation.cpp
@@ -33,6 +33,8 @@
 #include "libIBusDaemon.h"
 #include "dsMgr.h"
 
+#include "manager.hpp"
+
 namespace WPEFramework {
 namespace Plugin {
 
@@ -118,6 +120,7 @@ public:
         UpdateAudioCodecInfo();
         UpdateVideoCodecInfo();
         Utils::IARM::init();
+        device::Manager::Initialize();
         IARM_Result_t res;
         IARM_CHECK( IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_AUDIO_MODE, AudioModeHandler) );
         PlayerInfoImplementation::_instance = this;
