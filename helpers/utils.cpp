e064339a89bc15ca0be91cc04704885bce5ddebd
Anooj Cheriyan <Anooj_Cheriyan@comcast.com>
2021-07-28 14:49:49 +0000

    RDKTV-9417 : HDMI-CEC state does not persist
    Reason for change:
    HDMI-CEC_state_does_not_persist
    Test Procedure: None
    Risks: Low
    
    Change-Id: I89a8bfe8cba6e796fd54b7579a48f967a9facea0
    Signed-off-by:Anooj Cheriyan <Anooj_Cheriyan@comcast.com>

--- helpers/utils.cpp
+++ helpers/utils.cpp
@@ -381,3 +381,39 @@ bool Utils::isValidInt(char* x)
     return Checked;
 }
 
+void Utils::syncPersistFile (const string file) {
+    FILE * fp = NULL;
+    fp = fopen(file.c_str(), "r");
+    if (fp == NULL) {
+        printf("fopen NULL\n");
+        return;
+    }
+    fflush(fp);
+    fsync(fileno(fp));
+    fclose(fp);
+}
+
+void Utils::persistJsonSettings(const string strFile, const string strKey, const JsonValue& jsValue)
+{
+    Core::File file;
+    file = strFile.c_str();
+
+    file.Open(false);
+    if (!file.IsOpen())
+        file.Create();
+
+    JsonObject cecSetting;
+    cecSetting.IElement::FromFile(file);
+    file.Destroy();
+    file.Create();
+    cecSetting[strKey.c_str()] = jsValue;
+    cecSetting.IElement::ToFile(file);
+
+    file.Close();
+
+    //Sync the settings
+    Utils::syncPersistFile (strFile);
+
+    return;
+}
+
