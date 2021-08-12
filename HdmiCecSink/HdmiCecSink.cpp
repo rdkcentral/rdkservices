d22fac8b0867e8a811ce422564d95fe7d27383fe
bijas babu <bijas.babu@sky.uk>
2021-08-12 14:05:39 +0100

    RDKTV-9950: HDMI Certification - CEC failure-9.3-3
    Reason for change: Added change to return no ack for report physical address which is required for cec certification
    
    Test Procedure: none
    Risks: low
    
    Signed-off-by: Bijas Babu <bijas.babu@sky.uk>

--- HdmiCecSink/HdmiCecSink.cpp
+++ HdmiCecSink/HdmiCecSink.cpp
@@ -256,7 +256,7 @@ namespace WPEFramework
                  try
                  { 
                      LOGINFO(" sending ReportPhysicalAddress response physical_addr :%s logicalAddress :%x \n",physical_addr.toString().c_str(), logicalAddress.toInt());
-                     conn.sendTo(LogicalAddress(LogicalAddress::BROADCAST), MessageEncoder().encode(ReportPhysicalAddress(physical_addr,logicalAddress.toInt()))); 
+                     conn.sendTo(LogicalAddress(LogicalAddress::BROADCAST), MessageEncoder().encode(ReportPhysicalAddress(physical_addr,logicalAddress.toInt())),1000); 
                  } 
                  catch(...)
                  {
