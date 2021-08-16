6189e19eb4f95911a8eae542d9a1d455981e5bd8
smathe135 <Simi_Mathew@comcast.com>
2021-08-16 21:57:50 +0530

    DELIA-51606: Set default voice for voice guidance language
    Reason for change: Set a default voice when the voice guidance
    language is updated if the application does not set it along with
    the voice.
    Test Procedure:
    1. Change the voice guidance language in the
    accessibility setting. Navigate to web apps and verify voice guidance
    works.
    2. No voice guidance regression
    3. Verify persistence of the voice guidance settings
    Risks: Low
    
    Signed-off-by: Simi Mathew <simim@tataelxsi.co.in>
    
    RDKTV-9258: Set property tts-mode for TTS audio
    
    Reason for change: Amlogic supports tts-mode for
    TTS audio
    Test Procedure:
    1. Turn on voice guidance and navigate through tiles.
    while voice guidance is narrating a longer sentence,
    execute the following
    gst-launch-1.0 httpsrc location="http://localhost:50050/
    nuanceEve/tts?voice=ava&language=en-US&rate=50&text=How_are_you"
    ! audio/x-raw, rate=22050, channels=1, format=S16LE,
    layout=interleaved ! audioconvert ! audioresample !
    amlhalasink direct-mode=false
    Audio from both sources should be heard without any
    glitches.
    2. No regression for voice guidance
    
    Risks: Low
    
    Signed-off-by: Simi Mathew <simim@tataelxsi.co.in>
    
    RDKTV-9312: Control TTS volume
    Reason for change: Inc/Dec TTS volume based on user Vg volume setting for Amlogic
    Test Procedure: mentioned in ticket
    Risks: Low
    Signed-off-by: Manoj Bhatta <manoj_bhatta@comcast.com>

--- TextToSpeech/impl/TTSManager.cpp
+++ TextToSpeech/impl/TTSManager.cpp
@@ -99,7 +99,15 @@ TTS_Error TTSManager::setConfiguration(Configuration &configuration) {
     m_defaultConfiguration.setEndPoint(configuration.ttsEndPoint);
     m_defaultConfiguration.setSecureEndPoint(configuration.ttsEndPointSecured);
     updated |= m_defaultConfiguration.setLanguage(configuration.language);
-    updated |= m_defaultConfiguration.setVoice(configuration.voice);
+    /* Set default voice for the language only if XRE does not set the voice */
+    std::string defaultVoice = configuration.voice;
+    if(configuration.voice.empty())
+    {
+        std::vector<std::string> voices;
+        listVoices(configuration.language, voices);
+        defaultVoice = voices.front();
+    }
+    updated |= m_defaultConfiguration.setVoice(defaultVoice);
     updated |= m_defaultConfiguration.setVolume(configuration.volume);
     updated |= m_defaultConfiguration.setRate(configuration.rate);
 
