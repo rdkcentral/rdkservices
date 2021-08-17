a6c427183b8d576e31c8c9b1bbb76c8064d5e46f
Anand Kandasamy <Anand_Kandasamy@comcast.com>
2021-08-17 14:01:46 -0400

    RDK-28534: Comcast Security Agent changes

--- SecurityAgent/SecurityAgent.cpp
+++ SecurityAgent/SecurityAgent.cpp
@@ -107,6 +107,9 @@ namespace Plugin {
         if (aclFile.Exists() == false) {
             aclFile = service->DataPath() + config.ACL.Value();
         }
+
+        SYSLOG(Logging::Startup, (_T("SecurityAgent: Reading acl file %s"), aclFile.Name().c_str()));
+
         if ((aclFile.Exists() == true) && (aclFile.Open(true) == true)) {
 
             if (_acl.Load(aclFile) == Core::ERROR_INCOMPLETE_CONFIG) {
@@ -129,6 +132,9 @@ namespace Plugin {
         if (connector.empty() == true) {
             connector = service->VolatilePath() + _T("token");
         }
+
+        SYSLOG(Logging::Notification,(_T("SecurityAgent TokenDispatcher connector path %s"),connector.c_str()));
+
         _engine = Core::ProxyType<RPC::InvokeServer>::Create(&Core::IWorkerPool::Instance());
         _dispatcher.reset(new TokenDispatcher(Core::NodeId(connector.c_str()), service->ProxyStubPath(), this, _engine));
 
@@ -181,6 +187,10 @@ namespace Plugin {
 
     /* virtual */ uint32_t SecurityAgent::CreateToken(const uint16_t length, const uint8_t buffer[], string& token)
     {
+        string strBuffer;
+        strBuffer.assign(reinterpret_cast<const char*>(buffer),length);
+        SYSLOG(Logging::Notification, (_T("Creating Token for %s"), strBuffer.c_str()));
+
         // Generate the token from the buffer coming in...
         auto newToken = JWTFactory::Instance().Element();
 
