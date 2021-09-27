7c8b942291ed536b29fa1f38e9be70cf6cc390b6
Haseena Sainul <h.sainul@metrological.com>
2021-09-27 09:26:30 +0000

    LocationSync: Syncup with Metro plugin changes

--- LocationSync/LocationSyncJsonRpc.cpp
+++ LocationSync/LocationSyncJsonRpc.cpp
@@ -17,9 +17,9 @@
  * limitations under the License.
  */
  
+#include "Module.h"
 #include <interfaces/json/JsonData_LocationSync.h>
 #include "LocationSync.h"
-#include "Module.h"
 
 namespace WPEFramework {
 
