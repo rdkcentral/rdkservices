159f2016222d35495ceb7f1233220222d76bb64c
Dawid Trendota <dawid.trendota@consult.red>
2021-12-07 09:38:34 +0100

    ONEMPERS-287 XCast: retry reconnection till success

--- XCast/XCast.cpp
+++ XCast/XCast.cpp
@@ -373,7 +373,10 @@ void XCast::onLocateCastTimer()
     int status = _rtConnector->connectToRemoteService();
     if(status != 0)
     {
-        locateCastObjectRetryCount++;
+        if(locateCastObjectRetryCount < 4)
+        {
+            locateCastObjectRetryCount++;
+        }
         if(locateCastObjectRetryCount == 1)
         {
             LOGINFO("Retry after 5 sec...");
