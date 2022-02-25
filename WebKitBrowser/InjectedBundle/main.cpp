c0acd3298121ebc8c25f8bceff11026d51c6ff9b
ANANTHMARIMUTHU <emutavchi@users.noreply.github.com>
2022-02-25 10:23:06 -0500

    Merge pull request #2171 from LibertyGlobal/ONEM_24421
    ONEM-24421 fixed argument format.

--- WebKitBrowser/InjectedBundle/main.cpp
+++ WebKitBrowser/InjectedBundle/main.cpp
@@ -112,7 +112,7 @@ public:
         // We have something to report back, do so...
         uint32_t result = _comClient->Open(RPC::CommunicationTimeOut);
         if (result != Core::ERROR_NONE) {
-            TRACE(Trace::Error, (_T("Could not open connection to node %s. Error: %s"), _comClient->Source().RemoteId(), Core::NumberType<uint32_t>(result).Text()));
+            TRACE(Trace::Error, (_T("Could not open connection to node %s. Error: %s"), _comClient->Source().RemoteId().c_str(), Core::NumberType<uint32_t>(result).Text().c_str()));
         } else {
             // Due to the LXC container support all ID's get mapped. For the TraceBuffer, use the host given ID.
             Trace::TraceUnit::Instance().Open(_comClient->ConnectionId());
