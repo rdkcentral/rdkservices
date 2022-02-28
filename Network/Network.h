2073d2bdcd9d0123eceec637150fd4d96fdcbcce
Thamim Razith <ThamimRazith_AbbasAli@comcast.com>
2022-02-28 22:37:29 +0000

    RDK-35175: Thunder API for Manual IP Assignment - Phase 2
    Reason for change: UPdated the geIPSetting and SetIPSetting api for phase 2
    Test Procedure: please referred from RDK-35348
    Risks: Low
    Signed-off-by: Thamim Razith ThamimRazith_AbbasAli@comcast.com
    (cherry picked from commit 4ba3b421ffa91236a637426b12d4f3429d8cdac9)
    
    RDK-35175 : Address Version Number Variable & Removed the TAB Space
    
    The MAJOR and MINOR version is declared but not used Address Version Number Variable & Removed the TAB Space
    
    (cherry picked from commit 2bdb753f24fdbf2d4b0bb28a4497e291a1fc9c88)
    
    RDK-35175: Thunder API for Manual IP Assignment - Phase 2
    
    Reason for change: Correct the JSON Response parameter for the geIPSetting api
    Test Procedure: please referred from RDK-35348
    Risks: Low
    Signed-off-by: Thamim Razith ThamimRazith_AbbasAli@comcast.com
    (cherry picked from commit 99949caa681c6dbc87bd908ee701a2d66111c93e)
    
    RDK-35457: rdkservices Documentation update
    Reason for change: rdkservice documentation
    Test Procedure: please referred from RDK-34849
    Risks: Low
    Signed-off-by: Thamim Razith ThamimRazith_AbbasAli@comcast.com
    (cherry picked from commit a563664e083b03cac8275e9eaa947fe9aba569aa)

--- Network/Network.h
+++ Network/Network.h
@@ -25,6 +25,7 @@
 #include "Module.h"
 #include "NetUtils.h"
 #include "utils.h"
+#include "AbstractPlugin.h"
 #include "upnpdiscoverymanager.h"
 
 
@@ -47,7 +48,7 @@ namespace WPEFramework {
         // As the registration/unregistration of notifications is realized by the class PluginHost::JSONRPC,
         // this class exposes a public method called, Notify(), using this methods, all subscribed clients
         // will receive a JSONRPC message as a notification, in case this method is called.
-        class Network : public PluginHost::IPlugin, public PluginHost::JSONRPC {
+        class Network : public AbstractPlugin {
         private:
 
             // We do not allow this plugin to be copied !!
@@ -74,7 +75,9 @@ namespace WPEFramework {
             uint32_t ping(const JsonObject& parameters, JsonObject& response);
             uint32_t pingNamedEndpoint(const JsonObject& parameters, JsonObject& response);
             uint32_t setIPSettings(const JsonObject& parameters, JsonObject& response);
+            uint32_t setIPSettings2(const JsonObject& parameters, JsonObject& response);
             uint32_t getIPSettings(const JsonObject& parameters, JsonObject& response);
+            uint32_t getIPSettings2(const JsonObject& parameters, JsonObject& response);
             uint32_t getSTBIPFamily(const JsonObject& parameters, JsonObject& response);
             uint32_t isConnectedToInternet(const JsonObject& parameters, JsonObject& response);
             uint32_t setConnectivityTestEndpoints(const JsonObject& parameters, JsonObject& response);
@@ -89,7 +92,7 @@ namespace WPEFramework {
             static void eventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
             void iarmEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
 
-	    // Netmask Validation
+            // Netmask Validation
             bool isValidCIDRv4(std::string interface);
             // Internal methods
             bool _getDefaultInterface(std::string& interface, std::string& gateway);
@@ -102,6 +105,8 @@ namespace WPEFramework {
 
             JsonObject _doPing(const std::string& guid, const std::string& endPoint, int packets);
             JsonObject _doPingNamedEndpoint(const std::string& guid, const std::string& endpointName, int packets);
+            bool getIPSettingsInternal(const JsonObject& parameters, JsonObject& response,int errCode);
+            uint32_t setIPSettingsInternal(const JsonObject& parameters, JsonObject& response);
 
         public:
             Network();
@@ -122,6 +127,8 @@ namespace WPEFramework {
         public:
             static Network *_instance;
             static Network *getInstance() {return _instance;}
+            static const short API_VERSION_NUMBER_MINOR;
+            static const short API_VERSION_NUMBER_MAJOR;
 
         private:
             NetUtils m_netUtils;
@@ -133,6 +140,7 @@ namespace WPEFramework {
             uint16_t m_stunBindTimeout;
             uint16_t m_stunCacheTimeout;
             bool m_stunSync;
+            uint32_t m_apiVersionNumber;
         };
     } // namespace Plugin
 } // namespace WPEFramework
