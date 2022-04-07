56929f401c2e016998edb3cdd577e0ea5d7b77ed
rpraba142 <Ramkumar_Prabaharan@comcast.com>
2022-04-07 12:32:28 +0000

    RDK-35330: API to set frame rate
    Reason for change: Changes for API to set frame rate
    
    Test Procedure: TBD
    
    Risks: None

--- RDKShell/RDKShell.h
+++ RDKShell/RDKShell.h
@@ -130,6 +130,7 @@ namespace WPEFramework {
             static const string RDKSHELL_METHOD_GET_CURSOR_SIZE;
             static const string RDKSHELL_METHOD_SET_CURSOR_SIZE;
             static const string RDKSHELL_METHOD_IGNORE_KEY_INPUTS;
+            static const string RDKSHELL_METHOD_SET_FRAME_RATE;
 
             // events
             static const string RDKSHELL_EVENT_ON_USER_INACTIVITY;
@@ -237,6 +238,7 @@ namespace WPEFramework {
             uint32_t setCursorSizeWrapper(const JsonObject& parameters, JsonObject& response);
             uint32_t getCursorSizeWrapper(const JsonObject& parameters, JsonObject& response);
             uint32_t ignoreKeyInputsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setFrameRateWrapper(const JsonObject& parameters, JsonObject& response);
 
         private/*internal methods*/:
             RDKShell(const RDKShell&) = delete;
