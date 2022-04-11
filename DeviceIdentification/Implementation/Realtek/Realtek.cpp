711d3e9b2a48283bf080167041c23bebe5dafce9
pkumbh631] <punam_kumbhar@comcast.com>
2022-04-11 13:57:44 +0530

    [RDKDEV-262] DeviceIdentification: syncup from metro version

--- DeviceIdentification/Implementation/Realtek/Realtek.cpp
+++ DeviceIdentification/Implementation/Realtek/Realtek.cpp
@@ -17,118 +17,102 @@
  * limitations under the License.
  */
 #include "../../Module.h"
-#include <interfaces/IDeviceIdentification.h>
+
 #include <fstream>
 
 namespace WPEFramework {
 namespace Plugin {
 
-class DeviceImplementation : public Exchange::IDeviceProperties, public PluginHost::ISubSystem::IIdentifier {
-	static constexpr const TCHAR* CPUInfoFile= _T("/proc/cpuinfo");
-	static constexpr const TCHAR* VERSIONFile= _T("/version.txt");
+    class DeviceImplementation : public PluginHost::ISubSystem::IIdentifier {
+        static constexpr const TCHAR* CPUInfoFile = _T("/proc/cpuinfo");
+        static constexpr const TCHAR* VERSIONFile = _T("/version.txt");
 
-public:
-    DeviceImplementation()
-    {
-        UpdateChipset(_chipset);
-        UpdateFirmwareVersion(_firmwareVersion);
-        UpdateIdentifier();
-    }
-
-    DeviceImplementation(const DeviceImplementation&) = delete;
-    DeviceImplementation& operator= (const DeviceImplementation&) = delete;
-    virtual ~DeviceImplementation()
-    {
-         /* Nothing to do here. */
-    }
-
-public:
-    // Device Propertirs interface
-    const string Chipset() const override
-    {
-        return _chipset;
-    }
-    const string FirmwareVersion() const override
-    {
-        return _firmwareVersion;
-    }
-
-    // Identifier interface
-    uint8_t Identifier(const uint8_t length, uint8_t buffer[]) const override
-    {
-        uint8_t result = 0;
-        if ((_identity.length())) {
-            result = (_identity.length() > length ? length : _identity.length());
-            ::memcpy(buffer, _identity.c_str(), result);
-        } else {
-            SYSLOG(Logging::Notification, (_T("Cannot determine system identity")));
+    public:
+        DeviceImplementation()
+        {
+            UpdateFirmwareVersion(_firmwareVersion);
+            UpdateIdentifier();
         }
-        return result;
-    }
 
-    BEGIN_INTERFACE_MAP(DeviceImplementation)
-        INTERFACE_ENTRY(Exchange::IDeviceProperties)
+        DeviceImplementation(const DeviceImplementation&) = delete;
+        DeviceImplementation& operator=(const DeviceImplementation&) = delete;
+        virtual ~DeviceImplementation()
+        {
+            /* Nothing to do here. */
+        }
+
+    public:
+        // Device Propertirs interface
+
+        // Identifier interface
+        uint8_t Identifier(const uint8_t length, uint8_t buffer[]) const override
+        {
+            uint8_t result = 0;
+            if ((_identity.length())) {
+                result = (_identity.length() > length ? length : _identity.length());
+                ::memcpy(buffer, _identity.c_str(), result);
+            } else {
+                SYSLOG(Logging::Notification, (_T("Cannot determine system identity")));
+            }
+            return result;
+        }
+        string Architecture() const override
+        {
+            return Core::SystemInfo::Instance().Architecture();
+        }
+        string Chipset() const override
+        {
+            return Core::SystemInfo::Instance().Chipset();
+        }
+        string FirmwareVersion() const override
+        {
+            return _firmwareVersion;
+        }
+
+        BEGIN_INTERFACE_MAP(DeviceImplementation)
         INTERFACE_ENTRY(PluginHost::ISubSystem::IIdentifier)
-    END_INTERFACE_MAP
+        END_INTERFACE_MAP
 
-private:
-    inline void UpdateFirmwareVersion(string& firmwareVersion) const
-    {
-        string line;
-        std::ifstream file(VERSIONFile);
-        if (file.is_open()) {
-            while (getline(file, line)) {
-                if (line.find("SDK_VERSION") != std::string::npos) {
-                    std::size_t position = line.find('=');
-                    if (position != std::string::npos) {
-                        firmwareVersion.assign(line.substr(position + 1, string::npos));
-                        break;
+    private:
+        inline void UpdateFirmwareVersion(string& firmwareVersion) const
+        {
+            string line;
+            std::ifstream file(VERSIONFile);
+            if (file.is_open()) {
+                while (getline(file, line)) {
+                    if (line.find("SDK_VERSION") != std::string::npos) {
+                        std::size_t position = line.find('=');
+                        if (position != std::string::npos) {
+                            firmwareVersion.assign(line.substr(position + 1, string::npos));
+                            break;
+                        }
                     }
                 }
+                file.close();
             }
-            file.close();
         }
-    }
 
-    inline void UpdateChipset(string& chipset) const
-    {
-        string line;
-        std::ifstream file(CPUInfoFile);
-        if (file.is_open()) {
-            while (getline(file, line)) {
-                if (line.find("Hardware") != std::string::npos) {
-                    std::size_t position = line.find(": ");
-                    if (position != std::string::npos) {
-                        chipset.assign(line.substr(position + 2, string::npos));
+        inline void UpdateIdentifier()
+        {
+            string line;
+            std::ifstream file(CPUInfoFile);
+            if (file.is_open()) {
+                while (getline(file, line)) {
+                    if (line.find("Serial") != std::string::npos) {
+                        std::size_t position = line.find(": ");
+                        if (position != std::string::npos) {
+                            _identity.assign(line.substr(position + 2, string::npos));
+                        }
                     }
                 }
+                file.close();
             }
-            file.close();
         }
-    }
 
-    inline void UpdateIdentifier()
-    {
-        string line;
-        std::ifstream file(CPUInfoFile);
-        if (file.is_open()) {
-            while (getline(file, line)) {
-                if (line.find("Serial") != std::string::npos) {
-                    std::size_t position = line.find(": ");
-                    if (position != std::string::npos) {
-                        _identity.assign(line.substr(position + 2, string::npos));
-                    }
-                }
-            }
-            file.close();
-        }
-    }
-
-private:
-    string _chipset;
-    string _firmwareVersion;
-    string _identity;
-};
+    private:
+        string _firmwareVersion;
+        string _identity;
+    };
 
     SERVICE_REGISTRATION(DeviceImplementation, 1, 0);
 }
