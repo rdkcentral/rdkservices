3fe472d6842308a5a4d55f5c5593c9c9816cfe3c
PaviP98 <92713403+PaviP98@users.noreply.github.com>
2022-05-12 18:47:41 +0530

    Update MaintenanceManager.cpp

--- MaintenanceManager/MaintenanceManager.cpp
+++ MaintenanceManager/MaintenanceManager.cpp
@@ -286,10 +286,6 @@ namespace WPEFramework {
                         LOGINFO("Starting Script (USM) :  %s \n", cmd.c_str());
                         system(cmd.c_str());
                     }
-                    //else {
-                      //  LOGINFO("Inside else. Executing break");
-                       // break;
-                    //}
                 }
             }
             /* Here in Solicited, we start with RFC so no
@@ -827,11 +823,8 @@ namespace WPEFramework {
             }
 
             m_abort_flag = true;
-            LOGINFO("EL: Notifiying all");
             task_thread.notify_all();
-            LOGINFO("EL: Checking thread is joinable");
             if(m_thread.joinable()){
-                LOGINFO("EL: thread is joinable. Executing join");
                 m_thread.join();
             }
         }
