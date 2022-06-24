845fb793965d76fc60d1a1b2533fad6e9cef3563
Sergey Borushevsky <Sergey_Borushevsky2@cable.comcast.com>
2022-06-24 23:52:11 +0000

    Added tests for setReportProfileStatus call

--- test/tests/TelemetryTest.cpp
+++ test/tests/TelemetryTest.cpp
@@ -16,6 +16,8 @@ protected:
     Core::ProxyType<Plugin::Telemetry> plugin;
     Core::JSONRPC::Handler& handler;
     Core::JSONRPC::Connection connection;
+    RfcApiImplMock rfcApiImplMock;
+    
     string response;
     ServiceMock service;
     Core::JSONRPC::Message message;
@@ -48,3 +50,33 @@ TEST_F(TelemetryTestFixture, RegisteredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("logApplicationEvent")));
 }
 
+
+TEST_F(TelemetryTestFixture, Plugin)
+{
+    EXPECT_CALL(rfcApiImplMock, setRFCParameter(::testing::_, ::testing::_, ::testing::_, ::testing::_ ))
+        .Times(2)
+        .WillOnce(::testing::Invoke(
+            [](char *pcCallerID, const char* pcParameterName, const char* pcParameterValue, DATA_TYPE eDataType) {
+
+                return WDMP_SUCCESS;
+            }))
+        .WillOnce(::testing::Invoke(
+            [](char *pcCallerID, const char* pcParameterName, const char* pcParameterValue, DATA_TYPE eDataType) {
+                return WDMP_SUCCESS;
+            }));
+
+    // Initialize
+    EXPECT_EQ(string(""), plugin->Initialize(nullptr));
+
+    // JSON-RPC methods
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setReportProfileStatus"), _T("{\"status\":\"STARTED\"}"), response));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+
+
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setReportProfileStatus"), _T("{\"status\":\"COMPLETE\"}"), response));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+
+    // Deinitialize
+    plugin->Deinitialize(nullptr);
+
+}
