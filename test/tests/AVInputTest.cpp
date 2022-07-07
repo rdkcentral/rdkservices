6f5a9c4e72ab11174d13a6b533771f8e73ce58aa
Dhivya Ilangovan <dhivya.dilangovan@sky.uk>
2022-07-07 20:55:31 +0530

    reert changes on test/tests/AVInputTest.cpp

--- test/tests/AVInputTest.cpp
+++ test/tests/AVInputTest.cpp
@@ -57,18 +57,6 @@ TEST_F(AVInputTestFixture, RegisteredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("numberOfInputs")));
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("currentVideoMode")));
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("contentProtected")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getInputDevices")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("writeEDID")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("readEDID")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getRawSPD")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getSPD")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setEdidVersion")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getEdidVersion")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("startInput")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("stopInput")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setVideoRectangle")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getSupportedGameFeatures")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getGameFeatureStatus")));
 }
 
 TEST_F(AVInputTestFixture, Plugin)
@@ -160,53 +148,6 @@ TEST_F(AVInputTestFixture, Plugin)
         .Times(1)
         .WillRepeatedly(::testing::Return(string("unknown")));
 
-    EXPECT_CALL(hdmiInputImplMock, getInputDevices())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, writeEDID())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, readEDID())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, getRawSPD())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, getSPD())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, setEdidVersion())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(true));
-
-    EXPECT_CALL(hdmiInputImplMock, getEdidVersion())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, startInput())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(Return(true));
-
-    EXPECT_CALL(hdmiInputImplMock, stopInput())
-        .Times(1)
-        .WillRepeatedly(::testing::ReturnReturn(true));
-
-    EXPECT_CALL(hdmiInputImplMock, setVideoRectangle())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(true));
-
-    EXPECT_CALL(hdmiInputImplMock, getSupportedGameFeatures())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
-
-    EXPECT_CALL(hdmiInputImplMock, getGameFeatureStatus())
-        .Times(1)
-        .WillRepeatedly(::testing::Return(string("unknown")));
 
     // IShell expectations
 
@@ -240,42 +181,6 @@ TEST_F(AVInputTestFixture, Plugin)
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("contentProtected"), _T("{}"), response));
     EXPECT_EQ(response, string("{\"isContentProtected\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getInputDevices"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getInputDevices\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("writeEDID"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"writeEDID\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("readEDID"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"readEDID\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getRawSPD"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getRawSPD\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getSPD"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getSPD\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEdidVersion"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"setEdidVersion\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getEdidVersion"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getEdidVersion\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("startInput"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"startInput\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("stopInput"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"stopInput\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setVideoRectangle"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"setVideoRectangle\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getSupportedGameFeatures"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getSupportedGameFeatures\":true,\"success\":true}"));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getGameFeatureStatus"), _T("{}"), response));
-    EXPECT_EQ(response, string("{\"getGameFeatureStatus\":true,\"success\":true}"));
 
     // JSON-RPC events
 
