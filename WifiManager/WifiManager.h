26bf310a7e56897aadc58bcc9883c1b6d7787240
ANANTHMARIMUTHU <74683978+tabbas651@users.noreply.github.com>
2022-07-11 15:16:06 -0400

    RDK-36464 : Improve Efficiency of WiFiManager RDKServices APIs phase-3 (#2872)
    Reason for change:  Improves overall performance of clients apps using RDKServices and reduce number of IARM_Bus_Call
    Test Procedure:please referred ticket
    Risks: Medium
    Signed-off-by: Thamim Razith <tabbas651@cable.comcast.com>
    (cherry picked from commit 251138c2610c9cc04abe145388dc8f359e48cb56)
    
    RDK-36464 : Improve Efficiency of WiFiManager RDKServices APIs phase-3 (#2822)
    
    * RDK-36464 : Improve Efficiency of WiFiManager RDKServices APIs phase-3
    
    Reason for change: Improves overall performance of clients apps using RDKServices and reduce number of IARM_Bus_Call
    Test Procedure:please referred ticket
    Risks: Medium
    Signed-off-by: Thamim Razith tabbas651@cable.comcast.com
    
    * Update WifiManagerState.cpp
    
    Co-authored-by: Karunakaran A <48997923+karuna2git@users.noreply.github.com>
    (cherry picked from commit a1dff9884a4261f01958e413285dcec1c502ff3f)
    
    RDK-36464 : Improve Efficiency of WiFiManager RDKServices APIs phase-3 (#2839)
    
    Reason for change: Based on wpa_state update the getConnected api response
    Test Procedure:please referred ticket
    Risks: Medium
    Signed-off-by: Thamim Razith tabbas651@cable.comcast.com
    (cherry picked from commit b4cbf4f084f3cff3d32dfa3948c37dcd2c4bb574)

--- WifiManager/WifiManager.h
+++ WifiManager/WifiManager.h
@@ -56,10 +56,10 @@ namespace WPEFramework {
 
             //Begin methods
             virtual uint32_t getQuirks(const JsonObject& parameters, JsonObject& response) const override;
-            virtual uint32_t getCurrentState(const JsonObject& parameters, JsonObject& response) const override;
+            virtual uint32_t getCurrentState(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t startScan(const JsonObject& parameters, JsonObject& response) const override;
             virtual uint32_t stopScan(const JsonObject& parameters, JsonObject& response) override;
-            virtual uint32_t getConnectedSSID(const JsonObject& parameters, JsonObject& response) const override;
+            virtual uint32_t getConnectedSSID(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t setEnabled(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t connect(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t disconnect(const JsonObject& parameters, JsonObject& response) override;
@@ -68,9 +68,9 @@ namespace WPEFramework {
             virtual uint32_t cancelWPSPairing(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t saveSSID(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t clearSSID(const JsonObject& parameters, JsonObject& response) override;
-            virtual uint32_t getPairedSSID(const JsonObject& parameters, JsonObject& response) const override;
-            virtual uint32_t getPairedSSIDInfo(const JsonObject& parameters, JsonObject& response) const override;
-            virtual uint32_t isPaired(const JsonObject& parameters, JsonObject& response) const override;
+            virtual uint32_t getPairedSSID(const JsonObject& parameters, JsonObject& response) override;
+            virtual uint32_t getPairedSSIDInfo(const JsonObject& parameters, JsonObject& response) override;
+            virtual uint32_t isPaired(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t setSignalThresholdChangeEnabled(const JsonObject& parameters, JsonObject& response) override;
             virtual uint32_t isSignalThresholdChangeEnabled(const JsonObject& parameters, JsonObject& response) const;
             virtual uint32_t getSupportedSecurityModes(const JsonObject& parameters, JsonObject& response) override;
