ab14b41699732d4a957a1eef768f2e8aab763df8
Tamilselvan arumugam Kesavan <tamilselvan.arumugamkesavan@sky.uk>
2022-07-25 10:58:46 +0100

    RDK-35695  Implement Thunder API for CS plugin

---
+++ ControlSettings/ControlSettings.h
@@ -0,0 +1,96 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:
+*
+* Copyright 2022 Sky UK
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef CONTROLSETTINGS_H
+#define CONTROLSETTINGS_H
+
+#include <string>
+#include "libIARM.h"
+#include "libIBusDaemon.h"
+#include "libIBus.h"
+#include "iarmUtil.h"
+
+#include "dsError.h"
+#include "dsMgr.h"
+#include "hdmiIn.hpp"
+
+#include <iostream>
+
+#include "tvTypes.h"
+#include "tvLog.h"
+#include "tvSettings.h"
+#include <pthread.h>
+#include "Module.h"
+#include "utils.h"
+#include "tvTypes.h"
+#include "tvError.h"
+
+#include "tr181api.h"
+#include <sys/stat.h>
+#include <vector>
+#include "ControlSettingsTV.h"
+#include "ControlSettingsSTB.h"
+
+namespace WPEFramework {
+namespace Plugin {
+
+    class ControlSettings : public DEVICE_TYPE {
+
+    private:
+        ControlSettings(const ControlSettings&) = delete;
+        ControlSettings& operator=(const ControlSettings&) = delete;
+   public : 
+	DECLARE_JSON_RPC_METHOD(getAspectRatio)
+        DECLARE_JSON_RPC_METHOD(setAspectRatio)
+        DECLARE_JSON_RPC_METHOD(getVideoFormat)
+        DECLARE_JSON_RPC_METHOD(getVideoFrameRate)
+        DECLARE_JSON_RPC_METHOD(getVideoResolution)
+
+   public:
+        ControlSettings();
+        ~ControlSettings();
+        static ControlSettings* _instance;
+        static ControlSettings* getInstance() { return ControlSettings::_instance; }
+	void NotifyVideoFormatChange(tvVideoHDRFormat_t format);
+        void NotifyVideoResolutionChange(tvResolutionParam_t resolution);
+        void NotifyVideoFrameRateChange(tvVideoFrameRate_t frameRate);
+
+    private:
+        uint8_t _skipURL;
+        int m_currentHdmiInResoluton;
+        int m_videoZoomMode;
+        bool m_isDisabledHdmiIn4KZoom;
+	void InitializeIARM();
+        void DeinitializeIARM();
+        bool isIARMConnected();
+        bool IARMinit();
+        static void dsHdmiVideoModeEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
+        static void dsHdmiStatusEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
+
+    public:
+        //   IPlugin methods
+        // -------------------------------------------------------------------------------------------------------
+        const std::string Initialize(PluginHost::IShell* service);
+        void Deinitialize(PluginHost::IShell* service);
+	virtual void AddRef() const { }
+	virtual uint32_t Release() const {return 0; }
+   };
+}
+}
+#endif
---
+++ ControlSettings/ControlSettingsSTB.h
@@ -0,0 +1,62 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:
+*
+* Copyright 2022 Sky UK
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef ControlSettingsSTB_H
+#define ControlSettingsSTB_H
+
+#include "string.h"
+
+#include "tvTypes.h"
+#include "tvLog.h"
+#include "tvSettings.h"
+#include <pthread.h>
+#include "Module.h"
+#include "tvTypes.h"
+#include "tvError.h"
+
+#include "AbstractPlugin.h"
+#include "utils.h"
+#include "ControlSettingsCommon.h"
+
+namespace WPEFramework {
+namespace Plugin {
+
+class ControlSettingsSTB : public AbstractPlugin {
+    private:
+        ControlSettingsSTB(const ControlSettingsSTB&) = delete;
+        ControlSettingsSTB& operator=(const ControlSettingsSTB&) = delete;
+
+	DECLARE_JSON_RPC_METHOD(getVolume)
+        DECLARE_JSON_RPC_METHOD(setVolume)
+
+    public:
+	ControlSettingsSTB();
+	~ControlSettingsSTB();
+        ControlSettingsSTB *instance;
+        void Initialize();
+        void Deinitialize();
+        virtual int UpdatePQParamsToCache( const char *action, const char *tr181ParamName, const char *pqmode, const char *source, const char *format, tvPQParameterIndex_t pqParamIndex, int params[] ) = 0;
+    
+    protected:
+        static void dsHdmiEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
+};
+
+}//namespace Plugin
+}//namespace WPEFramework
+#endif
