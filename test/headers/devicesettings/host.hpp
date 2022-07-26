1dbf4ce337537f46e04a33d8f4500820d25485e6
MManja792 <MManja792@comcast.com>
2022-07-26 14:06:40 -0400

    system unit test compile issue resolved

--- test/headers/devicesettings/host.hpp
+++ test/headers/devicesettings/host.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "sleepMode.hpp"
 #include "audioOutputPort.hpp"
 #include "dsTypes.h"
 #include "list.hpp"
@@ -7,22 +8,7 @@
 #include "videoOutputPort.hpp"
 
 namespace device {
-	template <class T>
-using List = std::vector<T>;
-
-	class SleepMode {
-
-	public:
-        static SleepMode & getInstance(int id);
-        static SleepMode & getInstance(const std::string &name);
-        List<SleepMode> getSleepModes();
-        SleepMode(int id);
-		string toString();
-        virtual ~SleepMode();
-
-};
-
-class HostImpl {
+	class HostImpl {
 public:
     virtual ~HostImpl() = default;
 
