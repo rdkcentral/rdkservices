22d180564ce24174dd6aa83e070e7f94378e60ae
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2022-08-08 14:48:56 +0300

    Remove unnecessary files after pr#2990

--- SystemServices/SystemServices.h
+++ SystemServices/SystemServices.h
@@ -40,7 +40,6 @@ using std::ofstream;
 #include "platformcaps/platformcaps.h"
 #if defined(USE_IARMBUS) || defined(USE_IARM_BUS)
 #include "libIARM.h"
-#include "irMgr.h"
 #include "pwrMgr.h"
 #include "host.hpp"
 #include "sleepMode.hpp"
