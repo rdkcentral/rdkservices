5dfc5e332a95ba81f4c42618659f6f5743b22249
Marcin Koczwara <marcin.koczwara@consult.red>
2022-08-09 13:24:33 +0200

    RDK-37519 Removed verification of methods
    It turned out that absence of this was not a bug but a feature.

--- RustBridge/RustBridge.h
+++ RustBridge/RustBridge.h
@@ -275,8 +275,8 @@ namespace WPEFramework {
             bool InternalMessage(const string& message, const string& parameters);
 
             bool HasMethodSupport(const VersionMap::const_iterator& index, const string& method) const {
-                bool result = false;
-
+                bool result = true;
+/*
                 if (index != _supportedVersions.cend()) {
                     result = (std::find(index->second.cbegin(), index->second.cend(), method) != index->second.cend());
                 }
@@ -288,7 +288,7 @@ namespace WPEFramework {
                         index++;
                     }
                 }
-
+*/
                 return (result);
             }
             state Destination(const string& designator, string& handler) const
