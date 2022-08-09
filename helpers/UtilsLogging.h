9e17c33e83f76207dab6e8cee1b78a7f6e9adf53
ANANTHMARIMUTHU <37086488+anand-ky@users.noreply.github.com>
2022-08-09 09:29:20 -0400

    Merge pull request #2998 from npoltorapavlo/helpers_clean_upo
    Utils separation of concern

--- helpers/UtilsLogging.h
+++ helpers/UtilsLogging.h
@@ -2,13 +2,6 @@
 
 #include <syscall.h>
 
-#undef LOGINFO
-#undef LOGWARN
-#undef LOGERR
-#undef LOG_DEVICE_EXCEPTION0
-#undef LOG_DEVICE_EXCEPTION1
-#undef LOG_DEVICE_EXCEPTION2
-
 #define LOGINFO(fmt, ...) do { fprintf(stderr, "[%d] INFO [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
 #define LOGWARN(fmt, ...) do { fprintf(stderr, "[%d] WARN [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
 #define LOGERR(fmt, ...) do { fprintf(stderr, "[%d] ERROR [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
