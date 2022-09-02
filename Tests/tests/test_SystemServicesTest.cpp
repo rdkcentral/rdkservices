c6c1550b2d312893d25633868b286448127fbee7
MManja792 <MManja792@comcast.com>
2022-09-01 22:20:21 -0400

    removed the Gtest flag

--- Tests/tests/test_SystemServicesTest.cpp
+++ Tests/tests/test_SystemServicesTest.cpp
@@ -19,7 +19,9 @@
 
 #include <gtest/gtest.h>
 #include "SystemServices.h"
+#include "IarmBusMock.h"
 #include "RfcApiMock.h"
+#include "WrapsMock.h"
 using namespace WPEFramework;
 using namespace std;
 
@@ -30,28 +32,27 @@ const string iarmName = _T("Thunder_Plugins");
 class SystemServicesTest : public::testing::Test
 {
     protected:
-    Core::ProxyType<Plugin::SystemServices> systemplugin;
-    Core::JSONRPC::Handler& handler;
-    Core::JSONRPC::Handler& handlerV2;
-    Core::JSONRPC::Connection connection;
+    IarmBusImplMock iarmBusImplMock;
     RfcApiImplMock rfcApiImplMock;
+    WrapsImplMock wrapsImplMock;
+    Core::ProxyType<Plugin::SystemServices> systemplugin;
+    Core::JSONRPC::Connection connection;
+    Core::JSONRPC::Handler *handler;
+    Core::JSONRPC::Handler *handlerV2;
     string response;
-    
-private:
-    /* data */
+ 
 public:
     SystemServicesTest()
-    :systemplugin(Core::ProxyType<Plugin::SystemServices>::Create())
-    ,handler(*systemplugin)
-    ,handlerV2(*(systemplugin->GetHandler(2)))
-    ,connection(1,0)
+    :connection(1,0)
     {
-	 RfcApi::getInstance().impl = &rfcApiImplMock;
+	    IarmBus::getInstance().impl = &iarmBusImplMock;
+        RfcApi::getInstance().impl = &rfcApiImplMock;
+        Wraps::getInstance().impl = &wrapsImplMock;
     }
    
     virtual void SetUp()
     {
-	}
+    }
 
     virtual void TearDown()
     {
@@ -59,48 +60,86 @@ public:
 
     ~SystemServicesTest()
     {
-	RfcApi::getInstance().impl = nullptr;
+        IarmBus::getInstance().impl = nullptr;
+        RfcApi::getInstance().impl = nullptr;
+        Wraps::getInstance().impl = nullptr;	
     }
 };
 
-
-TEST_F(SystemServicesTest, RegisteredMethods)
+TEST_F(SystemServicesTest, RegisterMethods)
 {
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("requestSystemUptime")));
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("fireFirmwarePendingReboot")));
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("setFirmwareAutoReboot")));
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("setFirmwareRebootDelay")));
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("getLastFirmwareFailureReason")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getDownloadedFirmwareInfo")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getFirmwareDownloadPercent")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getFirmwareUpdateState")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setTimeZoneDST")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getTimeZoneDST")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setTerritory")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getTerritory")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("hasRebootBeenRequested")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getPreviousRebootInfo")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getPreviousRebootInfo2")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getPreviousRebootReason")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getMilestones")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("isOptOutTelemetry")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setOptOutTelemetry")));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getSystemVersions")));
+    EXPECT_CALL(iarmBusImplMock, IARM_Bus_Call)
+        .Times(1)
+        .WillOnce(
+                [](const char* ownerName, const char* methodName, void* arg, size_t argLen) {
+                    EXPECT_TRUE(strcmp(methodName, "DaemonSysModeChange") == 0);
+                    return IARM_RESULT_SUCCESS;
+                });
+
+    EXPECT_CALL(wrapsImplMock, system(::testing::_))
+        .Times(1)
+        .WillOnce(::testing::Invoke(
+            [&](const char* command) {
+                EXPECT_EQ(string(command), string(_T("rm -f /opt/warehouse_mode_active")));
+
+                return 0;
+            }));
+
+	    EXPECT_CALL(rfcApiImplMock, getRFCParameter(::testing::_, ::testing::_, ::testing::_))
+        .Times(1)
+        .WillOnce(::testing::Invoke(
+            [](char *pcCallerID, const char* pcParameterName, RFC_ParamData_t* pstParamData) {
+                EXPECT_EQ(strcmp(pcParameterName, "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Power.PwrMgr2.Enable"), 0);
+                return WDMP_SUCCESS;
+        }));
+
+        systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+        handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+        handlerV2 = (&((Core::JSONRPC::Handler&)(*(systemplugin->GetHandler(2)))));
+
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("requestSystemUptime")));
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Exists(_T("fireFirmwarePendingReboot")));
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Exists(_T("setFirmwareAutoReboot")));
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Exists(_T("setFirmwareRebootDelay")));
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Exists(_T("getLastFirmwareFailureReason")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getDownloadedFirmwareInfo")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getFirmwareDownloadPercent")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getFirmwareUpdateState")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("setTimeZoneDST")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getTimeZoneDST")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("setTerritory")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getTerritory")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("hasRebootBeenRequested")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getPreviousRebootInfo")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getPreviousRebootInfo2")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getPreviousRebootReason")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getMilestones")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("isOptOutTelemetry")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("setOptOutTelemetry")));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Exists(_T("getSystemVersions")));
 }
 
 TEST_F(SystemServicesTest, SystemUptime)
 {
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("requestSystemUptime"), _T("{}"), response));
+    systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("requestSystemUptime"), _T("{}"), response));
 
     EXPECT_THAT(response, ::testing::MatchesRegex(_T("\\{"
 				    "\"systemUptime\":\"[0-9]+.[0-9]+\","
 				    "\"success\":true"
 				    "\\}")));
 }
-    
 
 TEST_F(SystemServicesTest, PendingReboot)
 {
+    systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+    handlerV2 = (&((Core::JSONRPC::Handler&)(*(systemplugin->GetHandler(2)))));
+
+
 	EXPECT_CALL(rfcApiImplMock, setRFCParameter(::testing::_, ::testing::_, ::testing::_, ::testing::_))
         .Times(1)
         .WillOnce(::testing::Invoke(
@@ -110,10 +149,9 @@ TEST_F(SystemServicesTest, PendingReboot)
 		return WDMP_SUCCESS;
 	}));
 	
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Invoke(connection, _T("fireFirmwarePendingReboot"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Invoke(connection, _T("fireFirmwarePendingReboot"), _T("{}"),response));
 	EXPECT_EQ(response,string("{\"success\":true}"));
 }
-
 
 TEST_F(SystemServicesTest, AutoReboot)
 {
@@ -126,61 +164,72 @@ TEST_F(SystemServicesTest, AutoReboot)
                 return WDMP_SUCCESS;
         }));
 
-        EXPECT_EQ(Core::ERROR_NONE, handlerV2.Invoke(connection, _T("setFirmwareAutoReboot"), _T("{\"enable\":true}"),response));
-        EXPECT_EQ(response,string("{\"success\":true}"));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handlerV2 = (&((Core::JSONRPC::Handler&)(*(systemplugin->GetHandler(2)))));
+
+    EXPECT_EQ(Core::ERROR_NONE, handlerV2->Invoke(connection, _T("setFirmwareAutoReboot"), _T("{\"enable\":true}"),response));
+    EXPECT_EQ(response,string("{\"success\":true}"));
 }
 
 TEST_F(SystemServicesTest, RebootDelay)
 {
-        EXPECT_CALL(rfcApiImplMock, setRFCParameter(::testing::_, ::testing::_, ::testing::_, ::testing::_))
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handlerV2 = (&((Core::JSONRPC::Handler&)(*(systemplugin->GetHandler(2)))));
+
+    EXPECT_CALL(rfcApiImplMock, setRFCParameter(::testing::_, ::testing::_, ::testing::_, ::testing::_))
         .Times(1)
         .WillOnce(::testing::Invoke(
             [](char *pcCallerID, const char* pcParameterName, const char* pcParameterValue, DATA_TYPE eDataType) {
                 EXPECT_EQ(strcmp(pcCallerID, "thunderapi"), 0);
                 EXPECT_EQ(strcmp(pcParameterName, "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AutoReboot.fwDelayReboot"), 0);
                 return WDMP_SUCCESS;
-        }));
-
-    EXPECT_EQ(Core::ERROR_NONE, handlerV2.Invoke(connection, _T("setFirmwareRebootDelay"), _T("{\"delaySeconds\":10}"),response));
+		}));
+    EXPECT_EQ(Core::ERROR_NONE, handlerV2->Invoke(connection, _T("setFirmwareRebootDelay"), _T("{\"delaySeconds\":10}"),response));
     EXPECT_EQ(response,string("{\"success\":true}"));
 }
 
 TEST_F(SystemServicesTest, Firmware)
 {
-	EXPECT_EQ(Core::ERROR_NONE, handlerV2.Invoke(connection, _T("getLastFirmwareFailureReason"), _T("{}"),response));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+    handlerV2 = (&((Core::JSONRPC::Handler&)(*(systemplugin->GetHandler(2)))));
+
+	EXPECT_EQ(Core::ERROR_NONE, handlerV2->Invoke(connection, _T("getLastFirmwareFailureReason"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"failReason\":\"Invalid Request\",\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getDownloadedFirmwareInfo"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getDownloadedFirmwareInfo"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"currentFWVersion\":\"PX051AEI_VBN_2203_sprint_20220331225312sdy_NG\",\"downloadedFWVersion\":\"\",\"downloadedFWLocation\":\"\",\"isRebootDeferred\":false,\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getFirmwareDownloadPercent"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getFirmwareDownloadPercent"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"downloadPercent\":-1,\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getFirmwareUpdateState"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getFirmwareUpdateState"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"firmwareUpdateState\":0,\"success\":true}"));
 }
 
 TEST_F(SystemServicesTest, Timezone)
 {
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setTimeZoneDST"), _T("{\"timeZone\":\"America/New_York\"}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("setTimeZoneDST"), _T("{\"timeZone\":\"America/New_York\"}"),response));
 	EXPECT_EQ(response,string("{\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getTimeZoneDST"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getTimeZoneDST"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"timeZone\":\"America\\/New_York\",\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setTimeZoneDST"), _T("{\"timeZone\":\"America/Costa_Rica\"}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("setTimeZoneDST"), _T("{\"timeZone\":\"America/Costa_Rica\"}"),response));
     EXPECT_EQ(response,string("{\"success\":true}"));
 
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getTimeZoneDST"), _T("{}"),response));
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getTimeZoneDST"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"timeZone\":\"America\\/Costa_Rica\",\"success\":true}"));
-}
 
+}
 
 TEST_F(SystemServicesTest, InvalidTerritory)
 {
-	EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"US\",\"region\":\"US-NYC\"}"),response));
-    EXPECT_EQ(response,string(""));
+    systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
 
 	EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"USA\",\"region\":\"U-NYC\"}"),response));
 	EXPECT_EQ(response,string(""));
@@ -196,68 +245,80 @@ TEST_F(SystemServicesTest, InvalidTerritory)
     
 	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getTerritory"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"territory\":\"\",\"region\":\"\",\"success\":true}"));
-
 }
 
 TEST_F(SystemServicesTest, ValidTerritory)
 {
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"USA\",\"region\":\"US-NYC\"}"),response));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"USA\",\"region\":\"US-NYC\"}"),response));
     EXPECT_EQ(response,string("{\"success\":true}"));
 
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getTerritory"), _T("{}"),response));
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getTerritory"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"territory\":\"USA\",\"region\":\"US-NYC\",\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"GBR\",\"region\":\"GB-EGL\"}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("setTerritory"), _T("{\"territory\":\"GBR\",\"region\":\"GB-EGL\"}"),response));
     EXPECT_EQ(response,string("{\"success\":true}"));
 
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getTerritory"), _T("{}"),response));
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getTerritory"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"territory\":\"GBR\",\"region\":\"GB-EGL\",\"success\":true}"));
-
 }
 
 
 TEST_F(SystemServicesTest, reboot)
 {
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("hasRebootBeenRequested"), _T("{}"),response));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("hasRebootBeenRequested"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"rebootRequested\":false,\"success\":true}"));
-	
 }
 
 TEST_F(SystemServicesTest, rebootReason)
 {
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getPreviousRebootInfo"), _T("{}"),response));
-      EXPECT_EQ(response,string("{\"timeStamp\":\"18.08.2022_09:51.38\",\"reason\":\"Triggered from SystemServices! MAINTENANCE_REBOOT\",\"source\":\"HAL_CDL_notify_mgr_event\",\"customReason\":\"MAINTENANCE_REBOOT\",\"otherReason\":\"MAINTENANCE_REBOOT\",\"success\":true}"));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getPreviousRebootInfo"), _T("{}"),response));
+    EXPECT_EQ(response,string("{\"timeStamp\":\"18.08.2022_09:51.38\",\"reason\":\"Triggered from SystemServices! MAINTENANCE_REBOOT\",\"source\":\"HAL_CDL_notify_mgr_event\",\"customReason\":\"MAINTENANCE_REBOOT\",\"otherReason\":\"MAINTENANCE_REBOOT\",\"success\":true}"));
 	
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getPreviousRebootInfo2"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getPreviousRebootInfo2"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"rebootInfo\":{\"timestamp\":\"Thu Aug 18 13:51:39 UTC 2022\",\"source\":\"HAL_CDL_notify_mgr_event\",\"reason\":\"OPS_TRIGGERED\",\"customReason\":\"Unknown\",\"lastHardPowerReset\":\"Thu Aug 18 13:51:39 UTC 2022\"},\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getPreviousRebootReason"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getPreviousRebootReason"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"reason\":\"OPS_TRIGGERED\",\"success\":true}"));
-
 }
 
 TEST_F(SystemServicesTest, MileStones)
 {
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getMilestones"), _T("{}"),response));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+    EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getMilestones"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"milestones\":[\"RDK_STARTED:35755\"],\"deprecated\":true,\"success\":true}"));
 }
 
 TEST_F(SystemServicesTest, Telemetry)
 {
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("isOptOutTelemetry"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("isOptOutTelemetry"), _T("{}"),response));
 	EXPECT_EQ(response,string("{\"Opt-Out\":false,\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setOptOutTelemetry"), _T("{\"Opt-Out\":true}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("setOptOutTelemetry"), _T("{\"Opt-Out\":true}"),response));
     EXPECT_EQ(response,string("{\"success\":true}"));
 
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("isOptOutTelemetry"), _T("{}"),response));
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("isOptOutTelemetry"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"Opt-Out\":true,\"success\":true}"));
-
 }
 
 TEST_F(SystemServicesTest, SystemVersions)
 {
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getSystemVersions"), _T("{}"),response));
+	systemplugin = Core::ProxyType<Plugin::SystemServices>::Create();
+    handler = (&((Core::JSONRPC::Handler&)(*(systemplugin))));
+
+	EXPECT_EQ(Core::ERROR_NONE, handler->Invoke(connection, _T("getSystemVersions"), _T("{}"),response));
     EXPECT_EQ(response,string("{\"stbVersion\":\"PX051AEI_VBN_2203_sprint_20220331225312sdy_NG\",\"receiverVersion\":\"000.36.0.0\",\"stbTimestamp\":\"Fri 05 Aug 2022 16:14:54 AP UTC\",\"success\":true}"));
 }
