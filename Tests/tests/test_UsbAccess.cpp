df5715656a0cdc8a0518c80a0d916136e4fcb973
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2022-09-02 16:12:56 +0300

    DELIA-57823 : Improve tests
    Reason for change:
    * Large tests should be split.
    * Tests need only things they use,
      the fixture should provide only those things.
    * Standalone servers are not easy to mock,
      but in-place C++ sockets are.
    * According to Google, having more constraints
      than necessary is bad.
      If a test over-specifies, it doesn’t leave enough
      freedom to the implementation.
      Should use ON_CALL by default, and only use EXPECT_CALL
      when intend to verify that the call is made.
    * Workflow can set file permissions
      but the content should be set by tests.
    * According to Google, fixture constructor/destructor
      should be preferred over SetUp()/TearDown()
      unless using ASSERT_xx.
    
    Test Procedure: It builds
    Risks: Low
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>

--- Tests/tests/test_UsbAccess.cpp
+++ Tests/tests/test_UsbAccess.cpp
@@ -7,46 +7,28 @@
 
 using namespace WPEFramework;
 
-class UsbAccessTestFixture : public ::testing::Test {
+class UsbAccessTest : public ::testing::Test {
 protected:
     Core::ProxyType<Plugin::UsbAccess> plugin;
     Core::JSONRPC::Handler& handler;
     Core::JSONRPC::Handler& handlerV2;
     Core::JSONRPC::Connection connection;
-    UdevImplMock udevImplMock;
-    WrapsImplMock wrapsImplMock;
     string response;
 
-    UsbAccessTestFixture()
+    UsbAccessTest()
         : plugin(Core::ProxyType<Plugin::UsbAccess>::Create())
         , handler(*(plugin))
         , handlerV2(*(plugin->GetHandler(2)))
         , connection(1, 0)
     {
     }
-    virtual ~UsbAccessTestFixture()
-    {
-    }
-
-    virtual void SetUp()
-    {
-        Udev::getInstance().impl = &udevImplMock;
-        Wraps::getInstance().impl = &wrapsImplMock;
-    }
-
-    virtual void TearDown()
-    {
-        Udev::getInstance().impl = nullptr;
-        Wraps::getInstance().impl = nullptr;
-    }
 };
 
-TEST_F(UsbAccessTestFixture, RegisteredMethods)
+TEST_F(UsbAccessTest, RegisteredMethods)
 {
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getFileList")));
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("createLink")));
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("clearLink")));
-
     EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("getFileList")));
     EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("createLink")));
     EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("clearLink")));
@@ -56,8 +38,14 @@ TEST_F(UsbAccessTestFixture, RegisteredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handlerV2.Exists(_T("ArchiveLogs")));
 }
 
-TEST_F(UsbAccessTestFixture, UpdateFirmware)
+TEST_F(UsbAccessTest, UpdateFirmware)
 {
+    UdevImplMock udevImplMock;
+    WrapsImplMock wrapsImplMock;
+
+    Udev::getInstance().impl = &udevImplMock;
+    Wraps::getInstance().impl = &wrapsImplMock;
+
     EXPECT_CALL(wrapsImplMock, system(::testing::_))
         .Times(1)
         .WillOnce(::testing::Invoke(
@@ -71,4 +59,7 @@ TEST_F(UsbAccessTestFixture, UpdateFirmware)
     EXPECT_EQ(response, string("{\"success\":true}"));
 
     EXPECT_EQ(Core::ERROR_GENERAL, handlerV2.Invoke(connection, _T("updateFirmware"), _T("{\"fileName\":\"/tmp\';reboot;/my.bin\"}"), response));
+
+    Udev::getInstance().impl = nullptr;
+    Wraps::getInstance().impl = nullptr;
 }
