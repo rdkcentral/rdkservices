10eddec679b0815f8e0237ab43ed09b5e1a6e9d2
Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
2022-09-06 16:34:54 -0400

    Merge pull request #3143 from npoltorapavlo/DELIA-57823
    DELIA-57823 : Improve tests

--- Tests/tests/test_PersistentStore.cpp
+++ Tests/tests/test_PersistentStore.cpp
@@ -17,33 +17,92 @@
  * limitations under the License.
  */
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "PersistentStoreMock.h"
+#include "PersistentStore.h"
+
+#include "FactoriesImplementation.h"
 #include "ServiceMock.h"
+
+namespace {
+class PersistentStoreMock : public WPEFramework::Plugin::PersistentStore {
+public:
+    virtual ~PersistentStoreMock() = default;
+
+    MOCK_METHOD(std::vector<string>, LegacyLocations, (), (const, override));
+};
+}
 
 using namespace WPEFramework;
 
-class PersistentStoreTestFixture : public ::testing::Test {
+class PersistentStoreTest : public ::testing::Test {
 protected:
     Core::ProxyType<PersistentStoreMock> plugin;
     Core::JSONRPC::Handler& handler;
     Core::JSONRPC::Connection connection;
-    ServiceMock service;
     string response;
 
-    PersistentStoreTestFixture()
+    PersistentStoreTest()
         : plugin(Core::ProxyType<PersistentStoreMock>::Create())
         , handler(*plugin)
         , connection(1, 0)
     {
     }
-    virtual ~PersistentStoreTestFixture()
+    virtual ~PersistentStoreTest() = default;
+};
+
+class PersistentStoreInitializedTest : public PersistentStoreTest {
+protected:
+    ServiceMock service;
+
+    PersistentStoreInitializedTest()
+        : PersistentStoreTest()
     {
+        ON_CALL(service, ConfigLine())
+            .WillByDefault(
+                ::testing::Return("{"
+                                  "\"path\":\"/tmp/rdkservicestore\","
+                                  "\"key\":null,"
+                                  "\"maxsize\":20,"
+                                  "\"maxvalue\":10"
+                                  "}"));
+        ON_CALL(*plugin, LegacyLocations)
+            .WillByDefault(::testing::Return(std::vector<string>()));
+
+        EXPECT_EQ(string(""), plugin->Initialize(&service));
+    }
+    virtual ~PersistentStoreInitializedTest() override
+    {
+        plugin->Deinitialize(&service);
     }
 };
 
-TEST_F(PersistentStoreTestFixture, registeredMethods)
+class PersistentStoreInitializedEventTest : public PersistentStoreInitializedTest {
+protected:
+    Core::JSONRPC::Message message;
+    FactoriesImplementation factoriesImplementation;
+    PluginHost::IDispatcher* dispatcher;
+
+    PersistentStoreInitializedEventTest()
+        : PersistentStoreInitializedTest()
+    {
+        PluginHost::IFactories::Assign(&factoriesImplementation);
+
+        dispatcher = static_cast<PluginHost::IDispatcher*>(
+            plugin->QueryInterface(PluginHost::IDispatcher::ID));
+        dispatcher->Activate(&service);
+    }
+    virtual ~PersistentStoreInitializedEventTest() override
+    {
+        dispatcher->Deactivate();
+        dispatcher->Release();
+
+        PluginHost::IFactories::Assign(nullptr);
+    }
+};
+
+TEST_F(PersistentStoreTest, registeredMethods)
 {
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setValue")));
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("getValue")));
@@ -55,7 +114,7 @@ TEST_F(PersistentStoreTestFixture, registeredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("flushCache")));
 }
 
-TEST_F(PersistentStoreTestFixture, paramsMissing)
+TEST_F(PersistentStoreTest, paramsMissing)
 {
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{}"), response));
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("getValue"), _T("{}"), response));
@@ -64,7 +123,7 @@ TEST_F(PersistentStoreTestFixture, paramsMissing)
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("deleteNamespace"), _T("{}"), response));
 }
 
-TEST_F(PersistentStoreTestFixture, paramsEmpty)
+TEST_F(PersistentStoreTest, paramsEmpty)
 {
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"\",\"key\":\"\",\"value\":\"\"}"), response));
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("getValue"), _T("{\"namespace\":\"\",\"key\":\"\"}"), response));
@@ -73,7 +132,7 @@ TEST_F(PersistentStoreTestFixture, paramsEmpty)
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("deleteNamespace"), _T("{\"namespace\":\"\"}"), response));
 }
 
-TEST_F(PersistentStoreTestFixture, notInitialized)
+TEST_F(PersistentStoreTest, notInitialized)
 {
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"1\"}"), response));
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("getValue"), _T("{\"namespace\":\"test\",\"key\":\"a\"}"), response));
@@ -85,193 +144,107 @@ TEST_F(PersistentStoreTestFixture, notInitialized)
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("flushCache"), _T("{}"), response));
 }
 
-TEST_F(PersistentStoreTestFixture, jsonRpc)
+TEST_F(PersistentStoreInitializedEventTest, jsonRpc)
 {
-    EXPECT_CALL(service, ConfigLine())
+    Core::Event onValueChanged(false, true);
+
+    EXPECT_CALL(service, Submit(::testing::_, ::testing::_))
         .Times(1)
-        .WillOnce(
-            ::testing::Return("{"
-                              "\"path\":\"/tmp/rdkservicestore\","
-                              "\"key\":null,"
-                              "\"maxsize\":20,"
-                              "\"maxvalue\":10"
-                              "}"));
+        .WillOnce(::testing::Invoke(
+            [&](const uint32_t, const Core::ProxyType<Core::JSON::IElement>& json) {
+                string text;
+                EXPECT_TRUE(json->ToString(text));
+                EXPECT_EQ(text, string(_T("{"
+                                          "\"jsonrpc\":\"2.0\","
+                                          "\"method\":\"org.rdk.PersistentStore.onValueChanged\","
+                                          "\"params\":{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"1\"}"
+                                          "}")));
 
-    ON_CALL(*plugin, LegacyLocations)
-        .WillByDefault(
-            ::testing::Return(std::vector<string>()));
+                onValueChanged.SetEvent();
 
-    EXPECT_CALL(*plugin, event_onValueChanged)
-        .Times(1)
-        .WillOnce(
-            ::testing::Return());
+                return Core::ERROR_NONE;
+            }));
 
-    EXPECT_EQ(string(""), plugin->Initialize(&service));
+    handler.Subscribe(0, _T("onValueChanged"), _T("org.rdk.PersistentStore"), message);
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"1\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+
+    EXPECT_EQ(Core::ERROR_NONE, onValueChanged.Lock());
+
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getValue"), _T("{\"namespace\":\"test\",\"key\":\"a\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"value\":\"1\",\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"value\":\"1\",\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getNamespaces"), _T("{}"), response));
-    EXPECT_EQ(response,
-        _T("{\"namespaces\":[\"test\"],\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"namespaces\":[\"test\"],\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getStorageSize"), _T("{}"), response));
-    EXPECT_EQ(response,
-        _T("{\"namespaceSizes\":{\"test\":2},\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"namespaceSizes\":{\"test\":2},\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getKeys"), _T("{\"namespace\":\"test\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"keys\":[\"a\"],\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"keys\":[\"a\"],\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("deleteKey"), _T("{\"namespace\":\"test\",\"key\":\"a\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("deleteNamespace"), _T("{\"namespace\":\"test\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("flushCache"), _T("{}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
 
-    plugin->Deinitialize(&service);
+    handler.Unsubscribe(0, _T("onValueChanged"), _T("org.rdk.PersistentStore"), message);
 }
 
-TEST_F(PersistentStoreTestFixture, maxValue)
+TEST_F(PersistentStoreInitializedTest, maxValue)
 {
-    EXPECT_CALL(service, ConfigLine())
-        .Times(1)
-        .WillOnce(
-            ::testing::Return("{"
-                              "\"path\":\"/tmp/rdkservicestore\","
-                              "\"key\":null,"
-                              "\"maxsize\":20,"
-                              "\"maxvalue\":10"
-                              "}"));
-
-    ON_CALL(*plugin, LegacyLocations)
-        .WillByDefault(
-            ::testing::Return(std::vector<string>()));
-
-    EXPECT_EQ(string(""), plugin->Initialize(&service));
-
     EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"123456789123456789\"}"), response));
-
-    plugin->Deinitialize(&service);
 }
 
-TEST_F(PersistentStoreTestFixture, onValueChanged)
+TEST_F(PersistentStoreInitializedEventTest, onStorageExceeded)
 {
-    EXPECT_CALL(service, ConfigLine())
+    Core::Event onStorageExceeded(false, true);
+
+    EXPECT_CALL(service, Submit(::testing::_, ::testing::_))
         .Times(1)
-        .WillOnce(
-            ::testing::Return("{"
-                              "\"path\":\"/tmp/rdkservicestore\","
-                              "\"key\":null,"
-                              "\"maxsize\":20,"
-                              "\"maxvalue\":10"
-                              "}"));
+        .WillOnce(::testing::Invoke(
+            [&](const uint32_t, const Core::ProxyType<Core::JSON::IElement>& json) {
+                string text;
+                EXPECT_TRUE(json->ToString(text));
+                EXPECT_EQ(text, string(_T("{"
+                                          "\"jsonrpc\":\"2.0\","
+                                          "\"method\":\"org.rdk.PersistentStore.onStorageExceeded\","
+                                          "\"params\":{}"
+                                          "}")));
 
-    ON_CALL(*plugin, LegacyLocations)
-        .WillByDefault(
-            ::testing::Return(std::vector<string>()));
+                onStorageExceeded.SetEvent();
 
-    EXPECT_CALL(*plugin, event_onValueChanged("test", "a", "12345"))
-        .Times(1)
-        .WillOnce(
-            ::testing::Return());
+                return Core::ERROR_NONE;
+            }));
 
-    EXPECT_EQ(string(""), plugin->Initialize(&service));
+    handler.Subscribe(0, _T("onStorageExceeded"), _T("org.rdk.PersistentStore"), message);
 
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"12345\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"123456789\"}"), response));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"b\",\"value\":\"12345678\"}"), response));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+    EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"c\",\"value\":\"1\"}"), response));
+
+    EXPECT_EQ(Core::ERROR_NONE, onStorageExceeded.Lock());
+
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("deleteNamespace"), _T("{\"namespace\":\"test\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
 
-    plugin->Deinitialize(&service);
+    handler.Unsubscribe(0, _T("onStorageExceeded"), _T("org.rdk.PersistentStore"), message);
 }
 
-TEST_F(PersistentStoreTestFixture, onStorageExceeded)
+TEST_F(PersistentStoreTest, legacyLocation)
 {
+    ServiceMock service;
+
     EXPECT_CALL(service, ConfigLine())
-        .Times(1)
-        .WillOnce(
-            ::testing::Return("{"
-                              "\"path\":\"/tmp/rdkservicestore\","
-                              "\"key\":null,"
-                              "\"maxsize\":20,"
-                              "\"maxvalue\":20"
-                              "}"));
-
-    ON_CALL(*plugin, LegacyLocations)
-        .WillByDefault(
-            ::testing::Return(std::vector<string>()));
-
-    EXPECT_CALL(*plugin, event_onValueChanged("test", "a", "123456789123456789"))
-        .Times(1)
-        .WillOnce(
-            ::testing::Return());
-    EXPECT_CALL(*plugin, event_onStorageExceeded())
-        .Times(1)
-        .WillOnce(
-            ::testing::Return());
-
-    EXPECT_EQ(string(""), plugin->Initialize(&service));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"a\",\"value\":\"123456789123456789\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
-    EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"b\",\"value\":\"1\"}"), response));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getValue"), _T("{\"namespace\":\"test\",\"key\":\"a\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"value\":\"123456789123456789\",\"success\":true}"));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("deleteNamespace"), _T("{\"namespace\":\"test\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
-
-    plugin->Deinitialize(&service);
-}
-
-TEST_F(PersistentStoreTestFixture, setupLegacyLocation)
-{
-    EXPECT_CALL(service, ConfigLine())
-        .Times(1)
+        .Times(2)
         .WillOnce(
             ::testing::Return("{"
                               "\"path\":\"/tmp/path/to/legacy/location/store\","
                               "\"key\":null,"
                               "\"maxsize\":20,"
                               "\"maxvalue\":10"
-                              "}"));
-
-    ON_CALL(*plugin, LegacyLocations)
-        .WillByDefault(
-            ::testing::Return(std::vector<string>()));
-
-    EXPECT_CALL(*plugin, event_onValueChanged("test", "d", "abc"))
-        .Times(1)
-        .WillOnce(
-            ::testing::Return());
-
-    EXPECT_EQ(string(""), plugin->Initialize(&service));
-
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"d\",\"value\":\"abc\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
-
-    plugin->Deinitialize(&service);
-}
-
-TEST_F(PersistentStoreTestFixture, useLegacyLocation)
-{
-    if (Core::File(string("/tmp/rdkservicestore")).Exists()) {
-        EXPECT_TRUE(Core::File(string("/tmp/rdkservicestore")).Destroy());
-        EXPECT_FALSE(Core::File(string("/tmp/rdkservicestore")).Exists());
-    }
-
-    EXPECT_CALL(service, ConfigLine())
-        .Times(1)
+                              "}"))
         .WillOnce(
             ::testing::Return("{"
                               "\"path\":\"/tmp/rdkservicestore\","
@@ -279,21 +252,29 @@ TEST_F(PersistentStoreTestFixture, useLegacyLocation)
                               "\"maxsize\":20,"
                               "\"maxvalue\":10"
                               "}"));
-
     EXPECT_CALL(*plugin, LegacyLocations)
-        .Times(1)
-        .WillOnce(
-            ::testing::Return(std::vector<string>{
-                "/tmp/path/to/legacy/location/store" }));
+        .Times(2)
+        .WillOnce(::testing::Return(std::vector<string>()))
+        .WillOnce(::testing::Return(std::vector<string>{ "/tmp/path/to/legacy/location/store" }));
+
+    EXPECT_EQ(string(""), plugin->Initialize(&service));
+
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setValue"), _T("{\"namespace\":\"test\",\"key\":\"d\",\"value\":\"abc\"}"), response));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
+
+    plugin->Deinitialize(&service);
+
+    if (Core::File(string("/tmp/rdkservicestore")).Exists()) {
+        EXPECT_TRUE(Core::File(string("/tmp/rdkservicestore")).Destroy());
+        EXPECT_FALSE(Core::File(string("/tmp/rdkservicestore")).Exists());
+    }
 
     EXPECT_EQ(string(""), plugin->Initialize(&service));
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getValue"), _T("{\"namespace\":\"test\",\"key\":\"d\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"value\":\"abc\",\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"value\":\"abc\",\"success\":true}"));
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("deleteNamespace"), _T("{\"namespace\":\"test\"}"), response));
-    EXPECT_EQ(response,
-        _T("{\"success\":true}"));
+    EXPECT_EQ(response, _T("{\"success\":true}"));
 
     plugin->Deinitialize(&service);
 }
