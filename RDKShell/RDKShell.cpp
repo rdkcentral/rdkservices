147732dea20103cddfac8d00fc38927067946aa8
ANANTHMARIMUTHU <renjith_warier@comcast.com>
2022-09-08 18:59:05 +0000

    RDK-32992 - Add Ermgr blacklist APIs

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -51,8 +51,8 @@
 
 
 #define API_VERSION_NUMBER_MAJOR 1
-#define API_VERSION_NUMBER_MINOR 2
-#define API_VERSION_NUMBER_PATCH 2
+#define API_VERSION_NUMBER_MINOR 3
+#define API_VERSION_NUMBER_PATCH 0
 
 const string WPEFramework::Plugin::RDKShell::SERVICE_NAME = "org.rdk.RDKShell";
 //methods
