701fb83af30bc41045bc081973677965e264581f
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2022-09-12 09:04:53 -0400

    Update DisplaySettings.cpp
    RDKCOM-3090: resolutions not common.
    
    Reason for change:
    Updating realtek devices with additional resolution
    that was missing.
    with framerates
    Test Procedure: None
    Risks: Low
    Priority: P2
    Signed-off-by:Hayden Gfeller <Hayden_Gfeller@comcast.com>
    
    Change-Id: Iaa084a0673fc2040205de2c39230ae943d834a1d

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -1086,6 +1086,7 @@ namespace WPEFramework {
 		if(tvResolutions & dsTV_RESOLUTION_1080p50)supportedTvResolutions.emplace_back("1080p50");
                 if(tvResolutions & dsTV_RESOLUTION_1080p60)supportedTvResolutions.emplace_back("1080p60");
                 if(tvResolutions & dsTV_RESOLUTION_2160p30)supportedTvResolutions.emplace_back("2160p30");
+		if(tvResolutions & dsTV_RESOLUTION_2160p50)supportedTvResolutions.emplace_back("2160p50");
                 if(tvResolutions & dsTV_RESOLUTION_2160p60)supportedTvResolutions.emplace_back("2160p60");
             }
             catch(const device::Exception& err)
