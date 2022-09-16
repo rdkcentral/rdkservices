8242627cf2a5f6a58b737c0f8a85f6b0ae3ef8cf
ANANTHMARIMUTHU <Nikita_Poltorapavlo2@cable.comcast.com>
2022-09-16 18:00:15 +0300

    update changelog and api version

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -76,7 +76,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 7
+#define API_VERSION_NUMBER_PATCH 8
 
 static bool isCecArcRoutingThreadEnabled = false;
 static bool isCecEnabled = false;
