107faa05b49ce4b8f2bc365a4f7ced83bc40be13
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2022-09-20 14:10:06 +0300

    RDK-37578: More tests for System
    Reason for change:
    - Negative tests for setFirmwareRebootDelay,
      setFirmwareAutoReboot.
    - Tests for enableMoca, queryMocaStatus,
      updateFirmware, getMode, setMode,
      setDeepSleepTimer, setNetworkStandbyMode,
      getNetworkStandbyMode, setPreferredStandbyMode,
      getPreferredStandbyMode, getAvailableStandbyModes,
      getWakeupReason, getLastWakeupKeyCode.
    
    Comments:
    - "reboot" uses kill via system() and checks Netflix
      shutdown via sleep(10); which looks incorrect.
      Should not test that.
    - "getDeviceInfo" is too complex to understand or test.
      Should not test that.
    - "getFirmwareUpdateInfo" uses system() to read file
      which looks incorrect.
      Should not test that.
    
    Test Procedure: Unit tests
    Risks: Low
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>

--- Tests/headers/rdk/ds/sleepMode.hpp
+++ Tests/headers/rdk/ds/sleepMode.hpp
@@ -1,53 +1,44 @@
-
 #pragma once
-#include <vector>
+
 #include "list.hpp"
 
-#ifndef UNUSED
-#define UNUSED(x) (void)(x)
-#endif
-
 namespace device {
-    class SleepMode;
-    class SleepModeImpl {
-        public:
-            virtual ~SleepModeImpl() = default;
-            virtual SleepMode& getInstance(int id) = 0;
-            virtual SleepMode& getInstance(const std::string& name) = 0;
-            virtual List<SleepMode> getSleepModes() = 0;
-            virtual const std::string& toString() const;
-    };
+class SleepMode;
+class SleepModeImpl {
+public:
+    virtual ~SleepModeImpl() = default;
+    virtual List<SleepMode> getSleepModes() = 0;
+    virtual const std::string& toString() const = 0;
+};
 
-    class SleepMode /*: public DSConstant*/ {
-        public:
-            SleepModeImpl* impl;
+class SleepMode {
+public:
+    SleepModeImpl* impl;
 
-            static SleepMode& getInstance()
-            {
-                static SleepMode instance;
-                return instance;
-            }
+    static SleepMode& getInstance()
+    {
+        static SleepMode instance;
+        return instance;
+    }
 
-            static SleepMode& getInstance(int id)
-            {
-                UNUSED(id);
-                return getInstance();
-            }
+    static SleepMode& getInstance(int)
+    {
+        return getInstance();
+    }
 
-            static SleepMode& getInstance(const std::string& name)
-            {
-                UNUSED(name);
-                return getInstance();
-            }
+    static SleepMode& getInstance(const std::string&)
+    {
+        return getInstance();
+    }
 
-            List<SleepMode> getSleepModes()
-            {
-                return impl->getSleepModes();
-            }
+    List<SleepMode> getSleepModes()
+    {
+        return impl->getSleepModes();
+    }
 
-            const std::string& toString() const
-            {
-                return impl->toString();
-            }
-    };
+    const std::string& toString() const
+    {
+        return impl->toString();
+    }
+};
 }
