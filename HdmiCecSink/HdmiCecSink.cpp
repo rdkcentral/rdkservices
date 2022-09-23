6e5321e5dff66d049659b0542826b42e983a4827
dautapankumardora <90027158+dautapankumardora@users.noreply.github.com>
2022-09-23 12:49:41 +0100

    Updated API_VERSION HdmiCecSink.cpp

--- HdmiCecSink/HdmiCecSink.cpp
+++ HdmiCecSink/HdmiCecSink.cpp
@@ -147,7 +147,7 @@ static int32_t HdmiArcPortID = -1;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 1
+#define API_VERSION_NUMBER_PATCH 2
 
 namespace WPEFramework
 {
