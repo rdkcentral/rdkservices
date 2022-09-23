d4e744ff2383d429765d30b62f0eb56d2483bd16
Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
2022-09-23 11:06:24 -0400

    Merge pull request #3224 from npoltorapavlo/RDK-37578_10
    RDK-37578: More tests for System

---
+++ Tests/mocks/COMLinkMock.h
@@ -0,0 +1,18 @@
+#ifndef RDKSERVICES_TESTS_MOCKS_COMLINKMOCK_H_
+#define RDKSERVICES_TESTS_MOCKS_COMLINKMOCK_H_
+
+#include <gmock/gmock.h>
+
+#include "Module.h"
+
+class COMLinkMock : public WPEFramework::PluginHost::IShell::ICOMLink {
+public:
+    virtual ~COMLinkMock() = default;
+
+    MOCK_METHOD(void, Register, (WPEFramework::RPC::IRemoteConnection::INotification*), (override));
+    MOCK_METHOD(void, Unregister, (WPEFramework::RPC::IRemoteConnection::INotification*), (override));
+    MOCK_METHOD(WPEFramework::RPC::IRemoteConnection*, RemoteConnection, (const uint32_t), (override));
+    MOCK_METHOD(void*, Instantiate, (const WPEFramework::RPC::Object&, const uint32_t, uint32_t&, const string&, const string&), (override));
+};
+
+#endif //RDKSERVICES_TESTS_MOCKS_COMLINKMOCK_H_
