5ab724414cb67c38166e9f207a68afdd61638796
shashank.kumar@sky.uk <shashank.kumar@sky.uk>
2022-09-29 16:09:31 +0100

    AMLOGIC-3747: [Passthru] Audio loss on HDMI Hotplug during MAT playback
    Reason for change: SAD request was missed intermittently durin Hotplug
    Test Procedure: As described in ticket
    Risks: Low
    
    Signed-off-by: shashank.kumar@sky.uk <shashank.kumar@sky.uk>

--- DisplaySettings/CHANGELOG.md
+++ DisplaySettings/CHANGELOG.md
@@ -15,6 +15,10 @@ All notable changes to this RDK Service will be documented in this file.
 * Changes in CHANGELOG should be updated when commits are added to the main or release branches. There should be one CHANGELOG entry per JIRA Ticket. This is not enforced on sprint branches since there could be multiple changes for the same JIRA ticket during development. 
 
 * For more details, refer to [versioning](https://github.com/rdkcentral/rdkservices#versioning) section under Main README.
+## [1.0.11] - 2022-10-17
+### Fixed
+- Fixed No Audio issue while playing Dolby Mat and HDMI hotplug
+
 ## [1.0.10] - 2022-10-04
 ### Fixed
 - Fixed warnings that are treated as errors with "-Wall -Werror"
--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -76,7 +76,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 10
+#define API_VERSION_NUMBER_PATCH 11
 
 static bool isCecArcRoutingThreadEnabled = false;
 static bool isCecEnabled = false;
@@ -4147,7 +4147,7 @@ namespace WPEFramework {
                     device::AudioOutputPort aPort = device::Host::getInstance().getAudioOutputPort(audioPort);
 
                     aPort.getSupportedARCTypes(&types);
-                    if((aPort.isConnected()) && (m_hdmiCecAudioDeviceDetected)) {
+                    if((aPort.isConnected()) && (m_hdmiInAudioDeviceConnected)) {
                         LOGINFO("DisplaySettings::setEnableAudioPort Configuring User set Audio mode before starting ARC/eARC Playback...\n");
                         if(aPort.getStereoAuto() == true) {
                             if(types & dsAUDIOARCSUPPORT_eARC) {
