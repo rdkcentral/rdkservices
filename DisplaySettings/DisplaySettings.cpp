a5b7bc56ab0146e966aa0ec20b75c1f84c738f10
shashank.kumar@sky.uk <shashank.kumar@sky.uk>
2022-09-29 16:09:31 +0100

    AMLOGIC-3747: [Passthru] Audio loss on HDMI Hotplug during MAT playback
    Reason for change: SAD request was missed intermittently durin Hotplug
    Test Procedure: As described in ticket
    Risks: Low
    
    Signed-off-by: shashank.kumar@sky.uk <shashank.kumar@sky.uk>

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -79,7 +79,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 13
+#define API_VERSION_NUMBER_PATCH 14
 
 static bool isCecArcRoutingThreadEnabled = false;
 static bool isCecEnabled = false;
@@ -4167,7 +4167,7 @@ namespace WPEFramework {
                     device::AudioOutputPort aPort = device::Host::getInstance().getAudioOutputPort(audioPort);
 
                     aPort.getSupportedARCTypes(&types);
-                    if((aPort.isConnected()) && (m_hdmiCecAudioDeviceDetected)) {
+                    if((aPort.isConnected()) && (m_hdmiCecAudioDeviceDetected || m_hdmiInAudioDeviceConnected)) {
                         LOGINFO("DisplaySettings::setEnableAudioPort Configuring User set Audio mode before starting ARC/eARC Playback...\n");
                         if(aPort.getStereoAuto() == true) {
                             if(types & dsAUDIOARCSUPPORT_eARC) {
