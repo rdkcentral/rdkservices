5e43baab007d60939849c7671d2e51b8aea2a76f
evelum102 <Ezhilarasi_Velumani@comcast.com>
2022-09-30 18:06:15 +0000

    DELIA-52333: events trigger
    Signed-off-by: Ezhilarasi Velumani Ezhilarasi_Velumani@comcast.com

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -4163,6 +4163,7 @@ namespace WPEFramework {
 
                     gRdkShellMutex.lock();
                     result = CompositorController::launchApplication(client, uri, mimeType, topmost, focus);
+		    RdkShell::CompositorController::addListener(client, mEventListener);
                     gRdkShellMutex.unlock();
 
                     if (!result)
