ee05ac58ab7b0556253f22f14e9eefcde48b5f4f
Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
2022-10-03 11:44:42 -0400

    Merge pull request #3232 from sverkoye/sprint/2208
    RDKCMF-8858 RDKShell: also check for gRdkShellSurfaceModeEnabled in l…

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -4112,7 +4112,7 @@ namespace WPEFramework {
                     }
 
                     string runtimeDir = getenv("XDG_RUNTIME_DIR");
-                    string display = runtimeDir + "/" + uri;
+                    string display = runtimeDir + "/" + (gRdkShellSurfaceModeEnabled ? RDKSHELL_SURFACECLIENT_DISPLAYNAME : uri);
 
                     // Set mime type
                     if (!setMimeType(client, mimeType))
