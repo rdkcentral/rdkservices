2e385e8c2675706cdc8af5799a530318e0b51692
srikanth-vv <107277821+srikanth-vv@users.noreply.github.com>
2022-10-06 09:22:23 -0400

    Added versioning & changelog

--- UnifiedCASManagement/UnifiedCASManagement.cpp
+++ UnifiedCASManagement/UnifiedCASManagement.cpp
@@ -19,6 +19,10 @@
 
 #include "UnifiedCASManagement.h"
 
+#define API_VERSION_NUMBER_MAJOR 1
+#define API_VERSION_NUMBER_MINOR 0
+#define API_VERSION_NUMBER_PATCH 0
+
 namespace WPEFramework {
 
     namespace Plugin {
