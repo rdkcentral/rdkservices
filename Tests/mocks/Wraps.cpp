c484d3db7b4469c0abb744c00d77624847bb730c
srikanth-vv <107277821+srikanth-vv@users.noreply.github.com>
2022-10-28 08:23:13 -0400

    RDK-38096  - Corrected syslog passthrough

--- Tests/mocks/Wraps.cpp
+++ Tests/mocks/Wraps.cpp
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <syslog.h>
 #include "Wraps.h"
 
 extern "C" int __wrap_system(const char* command)
@@ -11,16 +12,12 @@ extern "C" FILE* __wrap_popen(const char* command, const char* type)
     return Wraps::getInstance().popen(command, type);
 }
 
-extern "C" FILE* __real_syslog(int pri, const char *fmt, ...);
 //This function will be called for syslog() in the code (added -Wl,-wrap,syslog)
 extern  "C" void __wrap_syslog(int pri, const char *fmt, ...)
 {
-    char strFmt[256];
     va_list args;
     va_start(args, fmt);
-    vsprintf(strFmt, fmt, args);
+    Wraps::getInstance().syslog(pri, fmt, args);
+    vsyslog(pri, fmt, args);
     va_end(args);
-
-    Wraps::getInstance().syslog(pri, strFmt);
-    __real_syslog(pri, fmt);
 }
