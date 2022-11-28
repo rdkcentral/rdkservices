047e1e3f7c91550faeb9c6de9f068141d4dd908e
zhilarasi <102503263+zhilarasi@users.noreply.github.com>
2022-11-28 18:14:05 +0530

    Update rdkshell.cpp

--- Tests/mocks/rdkshell.cpp
+++ Tests/mocks/rdkshell.cpp
@@ -1,9 +1,4 @@
 #include "rdkshell.h"
-#define RDKSHELL_SPLASH_SCREEN_FILE_CHECK "/tmp/.rdkshellsplash"
-#define RDKSHELL_FLAGS_SHIFT        8
-#define RDKSHELL_FLAGS_CONTROL      16
-#define RDKSHELL_FLAGS_ALT          32
-
 #define RDKSHELL_FPS 40
 int gCurrentFramerate = RDKSHELL_FPS;
 
