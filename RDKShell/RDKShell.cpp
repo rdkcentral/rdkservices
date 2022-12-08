e835ec961e24bc4a2f63a9bcf64e8e5e7e22906f
evelum102 <Ezhilarasi_Velumani@comcast.com>
2022-12-08 12:32:09 +0000

    RDK-38411: Unit test for RDKShell

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -1109,6 +1109,11 @@ namespace WPEFramework {
         RDKShell::~RDKShell()
         {
             //LOGINFO("dtor");
+	    if(mClientsMonitor != nullptr)
+            {
+                    std::cout << "def" << std::endl;
+                    delete mClientsMonitor;
+            }
         }
 
         const string RDKShell::Initialize(PluginHost::IShell* service )
