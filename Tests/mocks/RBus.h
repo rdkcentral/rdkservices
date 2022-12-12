a09c8ae97e13e49dc1364d03c5b0edbd52601407
sborushevsky <sborushevsky@users.noreply.github.com>
2022-12-12 14:15:46 +0200

    RDK-38254 : Added Unit Tests for uploadReport call. (#3478)
    * RDK-38254 : Added Unit Tests for uploadReport call.
    
    * Got rid of sleep call before the callback.
    
    * Added tests for 2 more failed cases.
    
    * Used ON_CALL instead of EXPECT_CALL where it's not needed.
    
    * Used testing::Return in some cases.

--- Tests/mocks/RBus.h
+++ Tests/mocks/RBus.h
@@ -13,12 +13,27 @@ struct _rbusHandle
 
 typedef struct _rbusHandle* rbusHandle_t;
 
+ struct _rbusObject
+{
+};
+typedef struct _rbusObject* rbusObject_t;
+
+struct _rbusValue
+{
+};
+typedef struct _rbusValue* rbusValue_t;
+
+typedef void (*rbusMethodAsyncRespHandler_t)(rbusHandle_t handle, char const* methodName, rbusError_t error, rbusObject_t params);
+
+
 class RBusApiImpl {
 public:
     virtual ~RBusApiImpl() = default;
 
     virtual rbusError_t rbus_open(rbusHandle_t* handle, char const* componentName) = 0;
-    virtual rbusError_t rbus_setBoolean(rbusHandle_t handle, char const* paramName, bool paramVal) = 0;
+    virtual rbusError_t rbusMethod_InvokeAsync(rbusHandle_t handle, char const* methodName, rbusObject_t inParams, rbusMethodAsyncRespHandler_t callback,  int timeout) = 0;
+    virtual rbusValue_t rbusObject_GetValue(rbusObject_t object, char const* name) = 0;
+    virtual char const* rbusValue_GetString(rbusValue_t value, int* len) = 0;
     virtual rbusError_t rbus_close(rbusHandle_t handle) = 0;
 };
 
@@ -37,9 +52,19 @@ public:
         return getInstance().impl->rbus_open(handle, componentName);
     }
 
-    static rbusError_t rbus_setBoolean(rbusHandle_t handle, char const* paramName, bool paramVal)
+    static rbusError_t rbusMethod_InvokeAsync(rbusHandle_t handle, char const* methodName, rbusObject_t inParams, rbusMethodAsyncRespHandler_t callback,  int timeout)
     {
-        return getInstance().impl->rbus_setBoolean(handle, paramName, paramVal);
+        return getInstance().impl->rbusMethod_InvokeAsync(handle, methodName, inParams, callback, timeout);
+    }
+
+    static rbusValue_t rbusObject_GetValue(rbusObject_t object, char const* name)
+    {
+        return getInstance().impl->rbusObject_GetValue(object, name);
+    }
+
+    static char const* rbusValue_GetString(rbusValue_t value, int* len)
+    {
+        return getInstance().impl->rbusValue_GetString(value, len);
     }
 
     static rbusError_t rbus_close(rbusHandle_t handle)
@@ -49,5 +74,7 @@ public:
 };
 
 constexpr auto rbus_open = &RBusApi::rbus_open;
-constexpr auto rbus_setBoolean = &RBusApi::rbus_setBoolean;
+constexpr auto rbusMethod_InvokeAsync = &RBusApi::rbusMethod_InvokeAsync;
+constexpr auto rbusObject_GetValue = &RBusApi::rbusObject_GetValue;
+constexpr auto rbusValue_GetString = &RBusApi::rbusValue_GetString;
 constexpr auto rbus_close = &RBusApi::rbus_close;
