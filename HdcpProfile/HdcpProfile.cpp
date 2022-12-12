d3ca7e6060003ecbc546213edff1d89eeabd91d8
Mikolaj Staworzynski <mikolaj.staworzynski@redembedded.com>
2022-12-12 10:56:06 +0100

    OMWAPPI-921: use IARM_Bus_RemoveEventHandler instead of ...
    ... IARM_Bus_UnRegisterEventHandler
    
    It is complementary function for IARM_Bus_RegisterEventHandler.
    
    While using IARM_Bus_UnRegisterEventHandler the following could happen:
    (1) Assume usage of HdmiCec and HdcpProfile plugins, both in in-process mode
    (2) Activate both plugins - they both register for IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG
    (3) Deactivate one of the plugin (for example HdcpProfile): IARM_Bus_UnRegisterEventHandler would de-register both listeners registrered in point (2)
    (4) As a result HdcpProfile plugin will not receive IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG events anymore.
    
    Change introduced for 5 used plugins: HdmiCec, HdcpProfile, XCast, DisplayInfo and DisplaySettings
    Additionally DisplaySettings now de-register for all the events - some of them are not there previously

--- HdcpProfile/HdcpProfile.cpp
+++ HdcpProfile/HdcpProfile.cpp
@@ -108,8 +108,8 @@ namespace WPEFramework
             if (Utils::IARM::isConnected())
             {
                 IARM_Result_t res;
-                IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG) );
-                IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDCP_STATUS) );
+                IARM_CHECK( IARM_Bus_RemoveEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG, dsHdmiEventHandler) );
+                IARM_CHECK( IARM_Bus_RemoveEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDCP_STATUS, dsHdmiEventHandler) );
             }
         }
 
