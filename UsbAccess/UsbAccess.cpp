bb53049d7de525673d0c504ad37456f26a8ae3f2
ANANTHMARIMUTHU <107277821+srikanth-vv@users.noreply.github.com>
2023-01-09 13:33:35 -0500

    RDKAPI-1 - Enhance UsbAccess RDK Service to support multiple USB drives
    Initial check-in.
    Implemented all methods and tested on AXI6.
    Pending tasks: Needs to test on Hisense, remove debug LOGINFO.
    
    (cherry picked from commit 85b160450165507793419be8f682d36ad3c54541)
    
    Modified regex
    
    (cherry picked from commit 0f208fa02cf83d0db23d381ff96ea7f55a2ff26f)
    
    Removed debug LOGs
    
    (cherry picked from commit 16cedb386ca778c8d6ea85abf7b0afc6792c48c9)
    
    RDKAPI-1 - Enhance UsbAccess RDK Service to support multiple USB drives
    
    (cherry picked from commit 8fb8732c92701f7f1b46c4adf37a23f14ef9200c)
    
    Corrected documentation parameters
    
    (cherry picked from commit 3821cf1631d5b6db1ce123b09417bc75b8799f8e)
    
    RDKAPI-1 - Enhance UsbAccess RDK Service to support multiple USB drives
    
    (cherry picked from commit 2752534e4e3ae5cfdecc1573fa738d470d56e6e6)
    
    RDKAPI-1 - Enhance UsbAccess RDK Service to support multiple USB drives
    
    (cherry picked from commit 948fab313941b6b2aba0c324b333d1f508d24566)
    (cherry picked from commit 20516fa2b0579c2b07d4f7e839f0cc1e174d1d8c)

--- UsbAccess/UsbAccess.cpp
+++ UsbAccess/UsbAccess.cpp
@@ -31,7 +31,7 @@ const string WPEFramework::Plugin::UsbAccess::EVT_ON_USB_MOUNT_CHANGED = "onUSBM
 const string WPEFramework::Plugin::UsbAccess::EVT_ON_ARCHIVE_LOGS = "onArchiveLogs";
 const string WPEFramework::Plugin::UsbAccess::REGEX_BIN = "[\\w-]*\\.{0,1}[\\w-]*\\.bin";
 const string WPEFramework::Plugin::UsbAccess::REGEX_FILE =
-        "[\\w-]*\\.{0,1}[\\w-]*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc|ts)";
+        "[\\w-]*\\.{0,1}[\\w ()-]*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc|ts)";
 const string WPEFramework::Plugin::UsbAccess::PATH_DEVICE_PROPERTIES = "/etc/device.properties";
 const std::list<string> WPEFramework::Plugin::UsbAccess::ADDITIONAL_FW_PATHS {"UsbTestFWUpdate", "UsbProdFWUpdate"};
 const string WPEFramework::Plugin::UsbAccess::ARCHIVE_LOGS_SCRIPT = "/lib/rdk/usbLogUpload.sh";
@@ -129,6 +129,47 @@ namespace Plugin {
                 mod_time = st.st_mtime;
             return mod_time;
         }
+
+        bool isAbsPath(const string& mountPath, const string& givenPath) {
+            if(givenPath.rfind(mountPath, 0) == 0)
+                return true;
+            else
+                return false;
+        }
+
+        //From usb number 2, it returns the next available number
+        int nextAvailableNumber(const std::set<int>& numList) {
+            int nNum = 2;
+            for(auto const& it : numList) {
+                if(it != nNum) {
+                    break;
+                }
+                nNum++;
+            }
+            return nNum;
+        }
+
+        //Returns if the incomingPath already has existing link
+        bool isSymlinkExists(const string& availableLink, const string& incomingPath)
+        {
+            bool bLinkExists = false;
+            char targetPath[256];
+            int len = readlink(availableLink.c_str(), targetPath, sizeof(targetPath));
+            if(len == -1)
+            {
+            	LOGWARN("readlink error");
+            }
+            else
+            {
+                targetPath[len] = '\0';
+                if(incomingPath.compare(string(targetPath)) == 0)
+                {
+                    LOGWARN("Symlink already exists");
+                    bLinkExists = true;
+                }
+            }
+            return bLinkExists;
+        }
     }
 
     SERVICE_REGISTRATION(UsbAccess, API_VERSION_NUMBER_MAJOR, API_VERSION_NUMBER_MINOR, API_VERSION_NUMBER_PATCH);
@@ -189,7 +230,24 @@ namespace Plugin {
         std::list<string> paths;
         getMounted(paths);
         if (!paths.empty())
-            result = getFileList(joinPaths(*paths.begin(), pathParam), files, REGEX_FILE, true);
+        {
+            string absPath;
+            //Loop through all the paths to match for absolute path
+            for(auto const& it : paths)
+            {
+                if(isAbsPath(it, pathParam))
+                {
+                    absPath = pathParam;
+                    break;
+                }
+            }
+            //If absolute path is not found, treat it as relative path
+            if(absPath.empty())
+            {
+                absPath = joinPaths(*paths.begin(), pathParam);
+            }
+            result = getFileList(absPath, files, REGEX_FILE, true);
+        }
 
         if (!result)
             response["error"] = "not found";
@@ -217,11 +275,63 @@ namespace Plugin {
 
         std::list<string> paths;
         getMounted(paths);
+
+        string baseURL = LINK_URL_HTTP;
+		string pathParam;
+		if (parameters.HasLabel("path"))
+			pathParam = parameters["path"].String();
+        else if(!paths.empty())
+            pathParam = *paths.begin();
+
+		bool bLinkExists = false;
         if (!paths.empty())
-            result = createLink(*paths.begin(), LINK_PATH);
+        {
+            // Check the symlink already exists with first usbdrive
+            if(isSymlinkExists(LINK_PATH, pathParam))
+                bLinkExists = true;
+            else {
+                //Loop through all the existing IDs and set bLinkExists to true, if it already has the link
+                for(auto itr : m_CreatedLinkIds)
+                {
+                    string linkPath = LINK_PATH+std::to_string(itr);
+                    bLinkExists = isSymlinkExists(linkPath, pathParam);
+                    if(bLinkExists) {
+                        baseURL = baseURL + std::to_string(itr);
+                        break;
+                    }
+                }
+            }
+
+            if(!bLinkExists)
+            {
+                //For the first usb drive, create link with out number suffix
+                if((*paths.begin()).compare(pathParam) == 0 )
+                {
+                    result = createLink(*paths.begin(), LINK_PATH);
+                }
+                else
+                {
+                    //Loop through all the paths for exact match and create link
+                    for(auto const& it : paths)
+                    {
+                        if(it.compare(pathParam) == 0)
+                        {
+                            int nUsbNum = nextAvailableNumber(m_CreatedLinkIds);
+                            baseURL = baseURL + std::to_string(nUsbNum);    //Add number suffix
+                            result = createLink(it, LINK_PATH+std::to_string(nUsbNum));
+                            if (result)
+                                m_CreatedLinkIds.insert(nUsbNum);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
         if (result)
-            response["baseURL"] = LINK_URL_HTTP;
+	        response["baseURL"] = baseURL;
+        else if (bLinkExists)
+            response["error"] = "symlink already exists: " + baseURL;
         else
             response["error"] = "could not create symlink";
 
@@ -234,7 +344,27 @@ namespace Plugin {
 
         bool result = false;
 
-        result = clearLink(LINK_PATH);
+        int nUsbNum = 1;
+        string urlParam;
+        if (parameters.HasLabel("baseURL"))
+            urlParam = parameters["baseURL"].String();
+
+        if(urlParam.empty() || (urlParam.compare(LINK_URL_HTTP)) == 0)
+        {
+            result = clearLink(LINK_PATH);
+        }
+        else
+        {
+            //Validate incoming path
+            std::smatch match;
+            if (regex_search(urlParam, match, std::regex("http://localhost:50050/usbdrive[0-9]+")) &&  match.size() >= 1) {
+                nUsbNum = std::stoi(urlParam.substr(urlParam.find_last_not_of("0123456789") + 1));
+                result = clearLink(LINK_PATH+std::to_string(nUsbNum));
+                if (result)
+                    m_CreatedLinkIds.erase(nUsbNum);
+            }
+        }
+
         if (!result)
             response["error"] = "could not remove symlink";
 
@@ -343,6 +473,7 @@ namespace Plugin {
         if (archiveLogsThread.joinable())
             archiveLogsThread.join();
 
+        m_oArchiveParams = parameters;
         archiveLogsThread = std::thread(&UsbAccess::archiveLogsInternal, this);
 
         returnResponse(true);
@@ -352,22 +483,66 @@ namespace Plugin {
     {
         ArchiveLogsError error = ScriptError;
 
+        string usbFilePath;
         std::list<string> paths;
         getMounted(paths);
         if (paths.empty())
             error = NoUSB;
         else
         {
-            string script = (ARCHIVE_LOGS_SCRIPT + " " + *paths.begin());
-            int rc = runScript(script.c_str());
-            LOGINFO("'%s' exit code: %d", script.c_str(), rc);
-            error = static_cast<ArchiveLogsError>(rc);
+            string pathParam;
+            if (m_oArchiveParams.HasLabel("path"))
+                pathParam = m_oArchiveParams["path"].String();
+
+            string usbPath;
+            //In case no path is provided, take the first from the mounted paths
+            if(pathParam.empty())
+            {
+                usbPath = *paths.begin();
+            }
+            else
+            {
+                //Loop through all the paths for exact match and create link
+                for(auto const& it : paths)
+                {
+                    if(it.compare(pathParam) == 0)
+                    {
+                        usbPath = pathParam;
+                        break;
+                    }
+                }
+            }
+
+            error = NoUSB;
+            if(!usbPath.empty())
+            {
+                string script = (ARCHIVE_LOGS_SCRIPT + " " + usbPath);
+                FILE* fp = popen(script.c_str(), "r");
+                if (NULL != fp) {
+                    char buf[256];
+                    while(fgets(buf, sizeof(buf), fp) != NULL)
+                    {
+                        usbFilePath = buf; //Capture file path returned by the script
+                        //Remove /n character at the end
+                        usbFilePath.erase(std::remove(usbFilePath.begin(), usbFilePath.end(), '\n'), usbFilePath.end());
+                    }
+                }
+                else {
+                    LOGERR("Cannot run command\n");
+                    error = ScriptError;
+                }
+
+                int rc = pclose(fp);
+                // int rc = runScript(script.c_str());
+                LOGINFO("'%s' exit code: %d", script.c_str(), rc);
+                error = static_cast<ArchiveLogsError>(rc);
+            }
         }
 
-        onArchiveLogs(error);
+        onArchiveLogs(error, usbFilePath);
     }
 
-    void UsbAccess::onArchiveLogs(ArchiveLogsError error)
+    void UsbAccess::onArchiveLogs(ArchiveLogsError error, const string& filePath)
     {
         JsonObject params;
         auto it = ARCHIVE_LOGS_ERRORS.find(error);
@@ -375,6 +550,7 @@ namespace Plugin {
             it = ARCHIVE_LOGS_ERRORS.find(ScriptError);
         params["error"] = it->second;
         params["success"] = (error == None);
+        params["path"] = filePath;
         sendNotify(EVT_ON_ARCHIVE_LOGS.c_str(), params);
     }
 
