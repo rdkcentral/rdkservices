186d61c7bd8660072ee60a336e5c983119fb1c20
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2023-02-01 13:02:52 -0500

    Updating to use NiceMock

--- Tests/tests/test_HdmiCec.cpp
+++ Tests/tests/test_HdmiCec.cpp
@@ -51,22 +51,24 @@ protected:
 };
 class HdmiCecDsTest : public HdmiCecTest {
 protected:
-    LibCCECImplMock libCCECImplMock;
-    ConnectionImplMock connectionImplMock;
+    testing::NiceMock<LibCCECImplMock> libCCECImplMock;
+    testing::NiceMock<ConnectionImplMock> connectionImplMock;
     HdmiCecDsTest()
         : HdmiCecTest()
     {
         LibCCEC::getInstance().impl = &libCCECImplMock;
+        Connection::getInstance().impl = &connectionImplMock;
     }
     virtual ~HdmiCecDsTest() override
     {
         LibCCEC::getInstance().impl = nullptr;
+        Connection::getInstance().impl = nullptr;
     }
 };
 
 class HdmiCecInitializedTest : public HdmiCecTest {
 protected:
-    IarmBusImplMock iarmBusImplMock;
+    testing::NiceMock<IarmBusImplMock> iarmBusImplMock;
     IARM_EventHandler_t cecMgrEventHandler;
     IARM_EventHandler_t dsHdmiEventHandler;
  
@@ -98,6 +100,7 @@ protected:
     }
     virtual ~HdmiCecInitializedTest() override
     {
+        
 
         sleep(2);
         plugin->Deinitialize(nullptr);
@@ -109,7 +112,7 @@ protected:
 };
 class HdmiCecInitializedEventTest : public HdmiCecInitializedTest {
 protected:
-    ServiceMock service;
+    testing::NiceMock<ServiceMock> service;
     Core::JSONRPC::Message message;
     FactoriesImplementation factoriesImplementation;
     PluginHost::IDispatcher* dispatcher;
@@ -133,10 +136,10 @@ protected:
 };
 class HdmiCecInitializedEventDsTest : public HdmiCecInitializedEventTest {
 protected:
-    LibCCECImplMock libCCECImplMock;
-    ConnectionImplMock connectionImplMock;
-    DeviceTypeMock deviceTypeImplMock;
-    LogicalAddressImplMock logicalAddressImplMock;
+    testing::NiceMock<DeviceTypeMock> deviceTypeImplMock;
+    testing::NiceMock<LogicalAddressImplMock> logicalAddressImplMock;
+    testing::NiceMock<LibCCECImplMock> libCCECImplMock;
+    testing::NiceMock<ConnectionImplMock> connectionImplMock;
      
 
     HdmiCecInitializedEventDsTest()
@@ -147,12 +150,24 @@ protected:
 	    DeviceType::getInstance().impl = &deviceTypeImplMock;
         LogicalAddress::getInstance().impl = &logicalAddressImplMock;
 
+        //Setenable needs to run firzt, as it turns everything on, locally.
+	    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
+        EXPECT_EQ(response, string("{\"success\":true}"));
+
+
         
 
     }
 
     virtual ~HdmiCecInitializedEventDsTest() override
     {
+
+        //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
+        sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
+        ON_CALL(connectionImplMock, close())
+            .WillByDefault(::testing::Return());
+        EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
+        EXPECT_EQ(response, string("{\"success\":true}"));
         
         LibCCEC::getInstance().impl = nullptr;
         Connection::getInstance().impl = nullptr;
@@ -174,43 +189,6 @@ TEST_F(HdmiCecTest, RegisteredMethods)
     
 }
 
-TEST_F(HdmiCecInitializedEventDsTest, setEnabled)
-{
-
-    //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
-    EXPECT_CALL(connectionImplMock, open())
-        .Times(1)
-        .WillOnce(::testing::Invoke(
-            [&]() {
-                
-            }));
-    EXPECT_CALL(connectionImplMock, addFrameListener(::testing::_))
-        .Times(1)
-        .WillOnce(::testing::Invoke(
-            [&](FrameListener *listener) {
-                //FrameListener only contains a destructor, and a void function. No values to check
-            }));
-
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-
-    EXPECT_CALL(connectionImplMock, close())
-        .Times(1)
-        .WillOnce(::testing::Invoke(
-            [&]() {
-                
-            }));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-}
-
 TEST_F(HdmiCecDsTest, getEnabledFalse)
 {
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getEnabled"), _T(""), response));
@@ -220,129 +198,50 @@ TEST_F(HdmiCecDsTest, getEnabledFalse)
 
 TEST_F(HdmiCecInitializedEventDsTest, getEnabledTrue)
 {
-    //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
-
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
     //Get enabled just checks if CEC is on, which is a global variable.
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getEnabled"), _T(""), response));
     EXPECT_EQ(response, string("{\"enabled\":true,\"success\":true}"));
 
-
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
 }
 
 
 TEST_F(HdmiCecInitializedEventDsTest, sendMessage)
 {
-    //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
     ON_CALL(connectionImplMock, sendAsync(::testing::_))
         .WillByDefault(::testing::Invoke(
             [&](const CECFrame &frame) {
                 EXPECT_EQ(frame.MAX_LENGTH, 128);
                 
             }));
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("sendMessage"), _T("{\"message\": \"P4IwAA==\"}"), response));
     EXPECT_EQ(response, string("{\"success\":true}"));
-
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-
 }
 
 
 TEST_F(HdmiCecInitializedEventDsTest, getActiveSourceStatusTrue)
 {
-
-    //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("xi6"));
-    
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
-    //sendMessage() with the proper message sets ActiveSource to true.
+    //ActiveSource is a local variable, no mocked functions to check.
+    //Calling the sendMessage() function with the proper message sets ActiveSource to true.
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("sendMessage"), _T("{\"message\": \"P4IwAA==\"}"), response));
     EXPECT_EQ(response, string("{\"success\":true}"));
-
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getActiveSourceStatus"), _T(""), response));
     EXPECT_EQ(response, string("{\"status\":true,\"success\":true}"));
 
 
-     //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-
-
 }
 TEST_F(HdmiCecInitializedEventDsTest, getActiveSourceStatusFalse)
 {
-
-    //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
-
-    //Not using sendMessage, such that active source is set to false, as it is by default)
-    
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
-
+    //ActiveSource is a local variable, no mocked functions to check.
+    //Active source is false by default.
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getActiveSourceStatus"), _T(""), response));
     EXPECT_EQ(response, string("{\"status\":false,\"success\":true}"));
-
-
-     //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-
-
 }
 
 
 TEST_F(HdmiCecInitializedEventDsTest, getCECAddress)
 {
-    //setting HdmiCec to enabled.
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
-
-
-
     ON_CALL(libCCECImplMock, getPhysicalAddress(::testing::_))
         .WillByDefault(::testing::Invoke(
             [&](uint32_t *physAddress) {
@@ -352,94 +251,51 @@ TEST_F(HdmiCecInitializedEventDsTest, getCECAddress)
     IARM_Bus_CECMgr_Status_Updated_Param_t eventData;
 
     //Active Source Status update sets the address/logical address and what not to non-default values
-    eventData.logicalAddress =42;
+    eventData.logicalAddress = 42;
     
-    handler.Subscribe(0, _T("onActiveSourceStatusUpdate"), _T("client.events.onActiveSourceStatusUpdate"), message);
-
 	ON_CALL(deviceTypeImplMock, toString())
         .WillByDefault(::testing::Return("New"));
     cecMgrEventHandler(IARM_BUS_CECMGR_NAME, IARM_BUS_CECMGR_EVENT_STATUS_UPDATED, &eventData , 0);
 
-
-    handler.Unsubscribe(0, _T("onActiveSourceStatusUpdate"), _T("client.events.onActiveSourceStatusUpdate"), message);
-
-
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getCECAddresses"), _T(""), response));
     EXPECT_EQ(response, string("{\"CECAddresses\":{\"physicalAddress\":12345,\"logicalAddress\":42,\"deviceType\":\"New\"},\"success\":true}"));
 
-
-
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
+    
 }
 
 
 
 TEST_F(HdmiCecInitializedEventDsTest, onDevicesChanged)
 {
- //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
-    
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
 
+    EXPECT_CALL(libCCECImplMock, getPhysicalAddress(::testing::_))
+        .Times(1)
+        .WillOnce(::testing::Invoke(
+            [&](uint32_t *physAddress) {
+                *physAddress = (uint32_t)12345;
+            }));
+    EXPECT_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
+            .Times(1)
+            .WillOnce(::testing::Return(1));
 
     ASSERT_TRUE(dsHdmiEventHandler != nullptr);
 
-
     IARM_Bus_DSMgr_EventData_t eventData;
-    eventData.data.hdmi_in_connect.port =dsHDMI_IN_PORT_0;
-    eventData.data.hdmi_in_connect.isPortConnected = true;
-
-    handler.Subscribe(0, _T("onDeviceAdded"), _T("client.events.onDeviceAdded"), message);
+    eventData.data.hdmi_hpd.event =0;
 
     dsHdmiEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG, &eventData , 0);
-
-    handler.Unsubscribe(0, _T("onDeviceAdded"), _T("client.events.onDeviceAdded"), message);
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-
+    
 
 }
 
 TEST_F(HdmiCecInitializedEventDsTest, getDeviceList)
 {
 
-     //setting HdmiCec to enabled.
-    ON_CALL(libCCECImplMock, getLogicalAddress(::testing::_))
-        .WillByDefault(::testing::Return(1));
-	ON_CALL(deviceTypeImplMock, toString())
-        .WillByDefault(::testing::Return("New"));
-    
-	EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": true}"), response));
-        EXPECT_EQ(response, string("{\"success\":true}"));
     sleep(1); //Allow the thread that populates deviceList to actually populate before we run getDeviceList.
-
     //Calling the device list, which is a defualt list of the hdmiCec class. Kist grabs the deviceList.
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getDeviceList"), _T(""), response));
     EXPECT_THAT(response, ::testing::ContainsRegex(_T(".*[({\"logicalAddress\":[0-9]*,\"osdName\":\"[a-zA-Z0-9 ]*\",\"vendorID\":\"[a-zA-Z0-9 ]*\"})*.*")));
     EXPECT_THAT(response, ::testing::ContainsRegex(_T(".*\"numberofdevices\":[0-9]*,\"deviceList\":.*")));
     EXPECT_THAT(response, ::testing::ContainsRegex(_T(".*\"success\":true.*")));
-
-
-    //Turning off HdmiCec. otherwise we get segementation faults as things memory early while threads are still running
-    sleep(1);//short wait to allow setEnabled to reach thread loop, where it can exit safely without segmentation faults
-    ON_CALL(connectionImplMock, close())
-        .WillByDefault(::testing::Return());
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnabled"), _T("{\"enabled\": false}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
 
 }
