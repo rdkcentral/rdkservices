c70aea6d01915961472e832bd18aeb17d0556b3f
lakshmiravishankar <lakshmi.ravishankar@sky.uk>
2023-02-06 13:49:31 +0000

    RDKTV-19524 : Cleanup Headers for FactoryIF, Protect, EMP and Plugin

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -1138,8 +1138,8 @@ namespace WPEFramework {
             char* mac = new char[19];
             tFHError retAPIStatus;
             std::cout << "calling factory hal init\n";
-            factorySD1_init();
-            retAPIStatus = getEthernetMAC(&mac);
+            factory_protect_init_for_rdkshell();
+	    retAPIStatus = factory_protect_hal_getEthernetMAC(&mac);
             if(retAPIStatus == E_OK)
             {
                 if (strncasecmp(mac,"00:00:00:00:00:00",17) == 0)
@@ -5756,7 +5756,7 @@ namespace WPEFramework {
             #ifdef RDKSHELL_READ_MAC_ON_STARTUP
             char* mac = new char[19];
             tFHError retAPIStatus;
-            retAPIStatus = getEthernetMAC(&mac);
+            retAPIStatus = factory_protect_hal_getEthernetMAC(&mac);
             if(retAPIStatus == E_OK)
             {
                 if (strncasecmp(mac,"00:00:00:00:00:00",17) == 0)
