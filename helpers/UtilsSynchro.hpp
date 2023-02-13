3a907217a37c517304c70344538b1b2cef74411e
ANANTHMARIMUTHU <tomasz.karczewski@redembedded.com>
2023-02-13 12:59:06 +0000

    RDKCOM-3804: OMWAPPI-1077 introducing synchro utils
    Adding UtilsSynchro.hpp to provide tools for
    automatically synchronizing API calls for given
    plugin, and UtilsSynchroIarm.hpp to additionally
    synchronize iarm event handlers.
    
    (cherry picked from commit baf32b375cc4f61910ccb05caad11e00837efcfd)
    
    RDKCOM-3804: OMWAPPI-1077 locked API for DisplaySettings
    
    (cherry picked from commit 85f0f4b456df3a48935f23d7b06af61b3b40b097)
    
    RDKCOM-3804: OMWAPPI-1077 locked API for HdmiCec
    
    also fixing problem where pthread signal
    could've been skipped
    
    (cherry picked from commit 024fd2d76b06528ee3e0e994fe75690643224b8b)
    
    Signed-off-by: tomasz-karczewski-red <tomasz.karczewski@redembedded.com>

---
+++ helpers/UtilsSynchro.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <mutex>
+#include <plugins/plugins.h>
+#include <memory>
+#include "UtilsLogging.h"
+
+using namespace WPEFramework;
+
+namespace Utils {
+    namespace Synchro {
+
+        namespace {
+            // set when inside of getFunctionToCall wrapper (or locked IARM handler - see UtilsSynchroIarm.hpp)
+            thread_local bool isThreadUsingLockedApi = false;
+        }
+
+        // keeps API locks, one per specific class
+        template<class C>
+        struct ApiLocks {
+            static std::recursive_mutex mtx;
+        };
+
+        template <class C> std::recursive_mutex ApiLocks<C>::mtx;
+
+        template <typename METHOD, typename REALOBJECT>
+        std::function<uint32_t(REALOBJECT*, const WPEFramework::Core::JSON::VariantContainer&, WPEFramework::Core::JSON::VariantContainer&)>
+        getFunctionToCall(const std::string& debugname, const METHOD& method, REALOBJECT* objectPtr) {
+            return [debugname, method](REALOBJECT *obj, const WPEFramework::Core::JSON::VariantContainer& in, WPEFramework::Core::JSON::VariantContainer& out) -> uint32_t {
+                isThreadUsingLockedApi = true;
+                // printf("METHOD CALL, GETTING LOCK: REALOBJECT '%s', method: '%s' MUTEX:%p\n",typeid(REALOBJECT).name(), debugname.c_str(), &ApiLocks<REALOBJECT>::mtx); fflush(stdout);
+                std::lock_guard<std::recursive_mutex> lock(ApiLocks<REALOBJECT>::mtx);
+                LOGINFO("calling %s with lock: %p\n", debugname.c_str(), &ApiLocks<REALOBJECT>::mtx);
+                uint32_t ret;
+                try {
+                    ret = (obj->*method)(in, out);
+                } catch (...) {
+                    isThreadUsingLockedApi = false;
+                    throw;
+                }
+                isThreadUsingLockedApi = false;
+                return ret;
+            };
+        }
+
+        template <typename METHOD, typename REALOBJECT>
+        void RegisterLockedApi(const string& methodName, const METHOD& method, REALOBJECT* objectPtr)
+        {
+            using MethodType = decltype(getFunctionToCall(methodName, method, objectPtr));
+            objectPtr->PluginHost::JSONRPC::Register<Core::JSON::VariantContainer, Core::JSON::VariantContainer, MethodType, REALOBJECT>(methodName, getFunctionToCall(methodName, method, objectPtr), objectPtr);
+        }
+
+        template <typename METHOD, typename REALOBJECT>
+        void RegisterLockedApiForVersions(const string& methodName, const METHOD& method, REALOBJECT* objectPtr, const std::vector<uint8_t> versions)
+        {
+            objectPtr->PluginHost::JSONRPC::Register<METHOD,REALOBJECT>(methodName, getFunctionToCall(methodName, method, objectPtr), objectPtr, versions);
+        }
+
+        template <typename METHOD, typename REALOBJECT>
+        void RegisterLockedApiForHandler(Core::JSONRPC::Handler* handler, const string& methodName, const METHOD& method, REALOBJECT* objectPtr)
+        {
+            handler->Register<JsonObject, JsonObject>(methodName, getFunctionToCall(methodName, method, objectPtr), objectPtr);
+        }
+
+        /*
+            This guard can unlock & re-lock api mutex to prevent deadlock possible when calling other plugins via Invoke
+            (could deadlock in case when that other plugin called Invoke on this plugin at the same time, or tried to call
+            this plugin recursively, from the Invoke'd call).
+        */
+        template<class UsingClass>
+        struct UnlockApiGuard {
+            UnlockApiGuard() {
+                if (isThreadUsingLockedApi) {
+                    ApiLocks<UsingClass>::mtx.unlock();
+                }
+            }
+            ~UnlockApiGuard() {
+                if (isThreadUsingLockedApi) {
+                    ApiLocks<UsingClass>::mtx.lock();
+                }
+            }
+        };
+
+        template<class UsingClass>
+        struct LockApiGuard {
+            std::unique_lock<std::recursive_mutex> _lock;
+            LockApiGuard() : _lock(ApiLocks<UsingClass>::mtx) {}
+            void unlock() {
+                _lock.unlock();
+            }
+            void lock() {
+                _lock.lock();
+            }
+        };
+
+
+    } // Utils
+} // Synchro
