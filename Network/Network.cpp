2f1316cf514be6f3c47be83703eb381160732d3f
Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>
2023-03-19 23:10:08 -0400

    Updated the unused variables
    Signed-off-by: Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>

--- Network/Network.cpp
+++ Network/Network.cpp
@@ -231,6 +231,7 @@ namespace WPEFramework
         {
             if (Utils::IARM::init())
             {
+                IARM_Result_t res;
                 IARM_Result_t retVal = IARM_RESULT_SUCCESS;
 
 #ifndef NET_DISABLE_NETSRVMGR_CHECK
@@ -265,6 +266,7 @@ namespace WPEFramework
 
             if (Utils::IARM::isConnected())
             {
+                IARM_Result_t res;
                 IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_NM_SRV_MGR_NAME, IARM_BUS_NETWORK_MANAGER_EVENT_INTERFACE_ENABLED_STATUS) );
                 IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_NM_SRV_MGR_NAME, IARM_BUS_NETWORK_MANAGER_EVENT_INTERFACE_CONNECTION_STATUS) );
                 IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_NM_SRV_MGR_NAME, IARM_BUS_NETWORK_MANAGER_EVENT_INTERFACE_IPADDRESS) );
@@ -349,6 +351,7 @@ namespace WPEFramework
 
         void  Network::EnsureNetSrvMgrRunning()
         {
+            IARM_Result_t res;
             IARM_Result_t retVal = IARM_RESULT_SUCCESS;
 
             if (m_isPluginInited)
