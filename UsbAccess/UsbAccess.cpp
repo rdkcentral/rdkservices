f6d884c1cc2ae7cabc99d910ac02442784bbbd7a
srikanth-vv <107277821+srikanth-vv@users.noreply.github.com>
2023-03-28 13:22:32 -0400

    RDKTV-22205 - Expand regex to allow all valid files
    (cherry picked from commit f0f62b66ca63ca2b0619cf87713f12135758a1ac)
    (cherry picked from commit 898f8151db8246ad0f9f68479ead347787b9292a)

--- UsbAccess/UsbAccess.cpp
+++ UsbAccess/UsbAccess.cpp
@@ -31,7 +31,7 @@ const string WPEFramework::Plugin::UsbAccess::EVT_ON_USB_MOUNT_CHANGED = "onUSBM
 const string WPEFramework::Plugin::UsbAccess::EVT_ON_ARCHIVE_LOGS = "onArchiveLogs";
 const string WPEFramework::Plugin::UsbAccess::REGEX_BIN = "[\\w-]*\\.{0,1}[\\w-]*\\.bin";
 const string WPEFramework::Plugin::UsbAccess::REGEX_FILE =
-        "[\\w-]*\\.{0,1}[\\w ()-]*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc|ts)";
+				"^[^.].*\\.(png|jpg|jpeg|tiff|tif|bmp|mp4|mov|avi|mp3|wav|m4a|flac|mp4|aac|wma|txt|bin|enc|ts)$";
 const string WPEFramework::Plugin::UsbAccess::PATH_DEVICE_PROPERTIES = "/etc/device.properties";
 const std::list<string> WPEFramework::Plugin::UsbAccess::ADDITIONAL_FW_PATHS {"UsbTestFWUpdate", "UsbProdFWUpdate"};
 const string WPEFramework::Plugin::UsbAccess::ARCHIVE_LOGS_SCRIPT = "/lib/rdk/usbLogUpload.sh";
