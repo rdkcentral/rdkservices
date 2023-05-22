cd00030bf64d91a422fe65f582c62d40ffabea95
PaviP98 <92713403+PaviP98@users.noreply.github.com>
2023-05-22 12:57:48 +0530

    Update MaintenanceManager.h

--- MaintenanceManager/MaintenanceManager.h
+++ MaintenanceManager/MaintenanceManager.h
@@ -142,7 +142,6 @@ namespace WPEFramework {
                 std::map<string, DATA_TYPE> m_paramType_map;
 
                 PluginHost::IShell* m_service;
-                WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement>* thunder_client;
 
                 bool isDeviceOnline();
                 void task_execution_thread();
@@ -151,8 +150,8 @@ namespace WPEFramework {
                 bool checkAutoRebootFlag();
                 bool readRFC(const char *);
                 void setRFC(const char*, const char*, DATA_TYPE);
-                bool getThunderPluginHandle(const char*);
-                void knowWhoAmI();	
+                void getThunderPluginHandle(const char*);
+                bool knowWhoAmI();	
                 bool stopMaintenanceTasks();
                 bool checkNetwork();
                 bool getActivatedStatus(bool &skipFirmwareCheck);
