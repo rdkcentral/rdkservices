29b69f4a87c6bfa0c79f6f3c114cfd6ac8dbca8e
Binu Inbaraj <Binu_Inbaraj@cable.comcast.com>
2023-06-14 12:44:23 -0400

    Add initial version rdkwindowmanager

---
+++ RDKWindowManager/RDKWindowManager.h
@@ -0,0 +1,249 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2020 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#pragma once
+
+#include <mutex>
+#include "Module.h"
+#include <rdkshell/rdkshellevents.h>
+#include <rdkshell/rdkshell.h>
+#include <rdkshell/linuxkeys.h>
+
+namespace WPEFramework {
+
+    namespace Plugin {
+
+        class RDKWindowManager :  public PluginHost::IPlugin, public PluginHost::JSONRPC {
+        public:
+            RDKWindowManager();
+            virtual ~RDKWindowManager();
+            virtual const string Initialize(PluginHost::IShell* service) override;
+            virtual void Deinitialize(PluginHost::IShell* service) override;
+            virtual string Information() const override;
+
+            BEGIN_INTERFACE_MAP(RDKWindowManager)
+            INTERFACE_ENTRY(PluginHost::IPlugin)
+            INTERFACE_ENTRY(PluginHost::IDispatcher)
+            END_INTERFACE_MAP
+
+        public/*members*/:
+            static RDKWindowManager* _instance;
+
+        public /*constants*/:
+            static const string SERVICE_NAME;
+            //methods
+            static const string RDK_WINDOW_MANAGER_METHOD_MOVE_TO_FRONT;
+            static const string RDK_WINDOW_MANAGER_METHOD_MOVE_TO_BACK;
+            static const string RDK_WINDOW_MANAGER_METHOD_MOVE_BEHIND;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_FOCUS;
+            static const string RDK_WINDOW_MANAGER_METHOD_KILL;
+            static const string RDK_WINDOW_MANAGER_METHOD_ADD_KEY_INTERCEPT;
+            static const string RDK_WINDOW_MANAGER_METHOD_ADD_KEY_INTERCEPTS;
+            static const string RDK_WINDOW_MANAGER_METHOD_REMOVE_KEY_INTERCEPT;
+            static const string RDK_WINDOW_MANAGER_METHOD_ADD_KEY_LISTENER;
+            static const string RDK_WINDOW_MANAGER_METHOD_REMOVE_KEY_LISTENER;
+            static const string RDK_WINDOW_MANAGER_METHOD_ADD_KEY_METADATA_LISTENER;
+            static const string RDK_WINDOW_MANAGER_METHOD_REMOVE_KEY_METADATA_LISTENER;
+            static const string RDK_WINDOW_MANAGER_METHOD_INJECT_KEY;
+            static const string RDK_WINDOW_MANAGER_METHOD_GENERATE_KEYS;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_SCREEN_RESOLUTION;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_SCREEN_RESOLUTION;
+            static const string RDK_WINDOW_MANAGER_METHOD_CREATE_DISPLAY;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_CLIENTS;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_Z_ORDER;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_BOUNDS;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_BOUNDS;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_VISIBILITY;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_VISIBILITY;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_OPACITY;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_OPACITY;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_SCALE;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_SCALE;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_HOLE_PUNCH;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_HOLE_PUNCH;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_LOG_LEVEL;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_LOG_LEVEL;
+            static const string RDK_WINDOW_MANAGER_METHOD_ENABLE_INACTIVITY_REPORTING;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_INACTIVITY_INTERVAL;
+            static const string RDK_WINDOW_MANAGER_METHOD_RESET_INACTIVITY_TIME;
+            static const string RDK_WINDOW_MANAGER_METHOD_SCALE_TO_FIT;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_KEYREPEATS_ENABLED;
+            static const string RDK_WINDOW_MANAGER_METHOD_ENABLE_KEYREPEATS;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_TOPMOST;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_VIRTUAL_RESOLUTION;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_VIRTUAL_RESOLUTION;
+            static const string RDK_WINDOW_MANAGER_METHOD_ENABLE_VIRTUAL_DISPLAY;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_VIRTUAL_DISPLAY_ENABLED;
+            static const string RDK_WINDOW_MANAGER_METHOD_HIDE_ALL_CLIENTS;
+            static const string RDK_WINDOW_MANAGER_METHOD_ENABLE_LOGS_FLUSHING;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_LOGS_FLUSHING_ENABLED;
+            static const string RDK_WINDOW_MANAGER_METHOD_SHOW_CURSOR;
+            static const string RDK_WINDOW_MANAGER_METHOD_HIDE_CURSOR;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_CURSOR_SIZE;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_CURSOR_SIZE;
+            static const string RDK_WINDOW_MANAGER_METHOD_IGNORE_KEY_INPUTS;
+            static const string RDK_WINDOW_MANAGER_METHOD_ENABLE_INPUT_EVENTS;
+            static const string RDK_WINDOW_MANAGER_METHOD_KEY_REPEAT_CONFIG;
+            static const string RDK_WINDOW_MANAGER_METHOD_GET_GRAPHICS_FRAME_RATE;
+            static const string RDK_WINDOW_MANAGER_METHOD_SET_GRAPHICS_FRAME_RATE;
+
+            // events
+            static const string RDK_WINDOW_MANAGER_EVENT_ON_USER_INACTIVITY;
+
+            void notify(const std::string& event, const JsonObject& parameters);
+
+        private/*registered methods (wrappers)*/:
+
+            //methods ("parameters" here is "params" from the curl request)
+            uint32_t moveToFrontWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t moveToBackWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t moveBehindWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setFocusWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t killWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t addKeyInterceptWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t addKeyInterceptsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t removeKeyInterceptWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t addKeyListenersWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t removeKeyListenersWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t addKeyMetadataListenerWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t removeKeyMetadataListenerWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t injectKeyWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t generateKeyWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getScreenResolutionWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setScreenResolutionWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t createDisplayWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getClientsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getZOrderWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getBoundsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setBoundsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getVisibilityWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setVisibilityWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getOpacityWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setOpacityWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getScaleWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setScaleWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getHolePunchWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setHolePunchWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getLogLevelWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setLogLevelWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t enableInactivityReportingWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setInactivityIntervalWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t resetInactivityTimeWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t scaleToFitWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getKeyRepeatsEnabledWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t enableKeyRepeatsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setTopmostWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getVirtualResolutionWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setVirtualResolutionWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t enableVirtualDisplayWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getVirtualDisplayEnabledWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t hideAllClientsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t enableLogsFlushingWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getLogsFlushingEnabledWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t showCursorWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t hideCursorWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setCursorSizeWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getCursorSizeWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t ignoreKeyInputsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t enableInputEventsWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t keyRepeatConfigWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t getGraphicsFrameRateWrapper(const JsonObject& parameters, JsonObject& response);
+            uint32_t setGraphicsFrameRateWrapper(const JsonObject& parameters, JsonObject& response);
+
+        private/*internal methods*/:
+            RDKWindowManager(const RDKWindowManager&) = delete;
+            RDKWindowManager& operator=(const RDKWindowManager&) = delete;
+
+            bool moveToFront(const string& client);
+            bool moveToBack(const string& client);
+            bool moveBehind(const string& client, const string& target);
+            bool setFocus(const string& client);
+            bool kill(const string& client);
+            bool addKeyIntercept(const uint32_t& keyCode, const JsonArray& modifiers, const string& client);
+            bool addKeyIntercepts(const JsonArray& intercepts);
+            bool removeKeyIntercept(const uint32_t& keyCode, const JsonArray& modifiers, const string& client);
+            bool addKeyListeners(const string& client, const JsonArray& listeners);
+            bool removeKeyListeners(const string& client, const JsonArray& listeners);
+            bool addAnyKeyListener(const string& client, const JsonArray& listeners);
+            bool injectKey(const uint32_t& keyCode, const JsonArray& modifiers);
+            bool generateKey(const string& client, const JsonArray& keyInputs);
+            bool getScreenResolution(JsonObject& out);
+            bool setScreenResolution(const unsigned int w, const unsigned int h);
+            bool createDisplay(const string& client, const string& displayName, const uint32_t displayWidth = 0, const uint32_t displayHeight = 0,
+                const bool virtualDisplay = false, const uint32_t virtualWidth = 0, const uint32_t virtualHeight = 0,
+                const bool topmost = false, const bool focus = false);
+            bool getClients(JsonArray& clients);
+            bool getZOrder(JsonArray& clients);
+            bool getBounds(const string& client, JsonObject& bounds);
+            bool setBounds(const string& client, const unsigned int x, const unsigned int y, const unsigned int w, const unsigned int h);
+            bool getVisibility(const string& client, bool& visibility);
+            bool setVisibility(const string& client, const bool visible);
+            bool getOpacity(const string& client, unsigned int& opacity);
+            bool setOpacity(const string& client, const unsigned int opacity);
+            bool getScale(const string& client, double& scaleX, double& scaleY);
+            bool setScale(const string& client, const double scaleX, const double scaleY);
+            bool getHolePunch(const string& client, bool& holePunch);
+            bool setHolePunch(const string& client, const bool holePunch);
+            bool enableInactivityReporting(const bool enable);
+            bool setInactivityInterval(const uint32_t interval);
+            bool resetInactivityTime();
+            bool enableKeyRepeats(const bool enable);
+            bool getKeyRepeatsEnabled(bool& enable);
+            bool setTopmost(const string& callsign, const bool topmost, const bool focus);
+            bool getVirtualResolution(const std::string& client, uint32_t &virtualWidth, uint32_t &virtualHeight);
+            bool setVirtualResolution(const std::string& client, const uint32_t virtualWidth, const uint32_t virtualHeight);
+            bool enableVirtualDisplay(const std::string& client, const bool enable);
+            bool getVirtualDisplayEnabled(const std::string& client, bool &enabled);
+            void enableLogsFlushing(const bool enable);
+            void getLogsFlushingEnabled(bool &enabled);
+            bool showCursor();
+            bool hideCursor();
+            bool setCursorSize(uint32_t width, uint32_t height);
+            bool getCursorSize(uint32_t& width, uint32_t& height);
+            bool enableInputEvents(const JsonArray& clients, bool enable);
+
+        private/*classes */:
+
+            class RdkShellListener :  public RdkShell::RdkShellEventListener {
+
+              public:
+                RdkShellListener(RDKWindowManager* shell)
+                    : mShell(*shell)
+                {
+                }
+
+                ~RdkShellListener()
+                {
+                }
+
+                // Events listeners
+                virtual void onUserInactive(const double minutes);
+
+              private:
+                  RDKWindowManager& mShell;
+            };
+
+        private/*members*/:
+            bool mEnableUserInactivityNotification;
+            std::shared_ptr<RdkShell::RdkShellEventListener> mEventListener;
+            PluginHost::IShell* mCurrentService;
+        };
+
+    } // namespace Plugin
+} // namespace WPEFramework
