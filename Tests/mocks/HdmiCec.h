56bfeac44ca1634e04474e63077f30ca75c92728
kprasad96 <91622631+kprasad96@users.noreply.github.com>
2023-06-18 18:41:17 +0100

    Update HdmiCec.h

--- Tests/mocks/HdmiCec.h
+++ Tests/mocks/HdmiCec.h
@@ -815,7 +815,7 @@ public:
 class ReportFeatures : public DataBlock {
 public:
     Op_t opCode(void) const {return REPORT_FEATURES;}
-    ReportFeatures(const Version &version,const AllDeviceTypes &allDeviceTypes,const std::vector<RcProfile> rc_Profile,std::vector<DeviceFeatures> device_Features) {}
+    ReportFeatures(const Version &version,const AllDeviceTypes &allDeviceTypes,const std::vector<RcProfile> rc_Profile,std::vector<DeviceFeatures> device_Features) : version(version), allDeviceTypes(allDeviceTypes){}
 
     Version version;
     AllDeviceTypes allDeviceTypes;
