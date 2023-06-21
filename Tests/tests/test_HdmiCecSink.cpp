2399cd2b42508b34190deb2468377a581a1ae56c
kprasad96 <91622631+kprasad96@users.noreply.github.com>
2023-06-21 08:03:44 +0100

    Update test_HdmiCecSink.cpp

--- Tests/tests/test_HdmiCecSink.cpp
+++ Tests/tests/test_HdmiCecSink.cpp
@@ -26,6 +26,7 @@ protected:
     IARM_EventHandler_t pwrMgrModeChangeEventHandler;
     IARM_EventHandler_t dsHdmiEventHandler;
     IARM_EventHandler_t dsHdmiCecSinkEventHandler;
+    string response;
 
     HdmiCecSinkTest()
         : plugin(Core::ProxyType<Plugin::HdmiCecSink>::Create())
@@ -346,6 +347,6 @@ TEST_F(HdmiCecSinkTest, getCecVersion)
                 return WDMP_SUCCESS;
             }));
 
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getCecVersion"), _T("{}"), response));
+    //EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getCecVersion"), _T("{}"), response));
     EXPECT_EQ(response, _T("{\"CECVersion\":\"1.4\",\"success\":true}"));
 }
