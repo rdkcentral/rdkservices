98847fbc52ebee8c72b168436fd9b8ebe5dd94c9
Vishnu Dinakaran <59993407+vdinak240@users.noreply.github.com>
2023-06-30 16:32:38 +0530

    RDK-42114:Changes on TTS thunder plugin for upstreaming ITextToSpeech (#4126) (#4168)
    Reason for change: Refine TTS COM-RPC interface
    Test Procedure: Mentioned in ticket
    Risks: Low
    
    Signed-off-by: vdinak240 <Vishnu_Dinakaran@comcast.com>
    Co-authored-by: emutavchi <emutavchi@users.noreply.github.com>
    Co-authored-by: Ramasamy Thalavay Pillai <ramasamy_thalavaypillai@comcast.com>

--- TextToSpeech/TextToSpeechImplementation.h
+++ TextToSpeech/TextToSpeechImplementation.h
@@ -21,9 +21,9 @@
 
 #include "Module.h"
 #include <interfaces/Ids.h>
+#include <interfaces/ITextToSpeech.h>
 #include "tracing/Logging.h"
 
-#include "ITextToSpeech.h"
 #include "impl/TTSManager.h"
 #include "impl/TTSConfiguration.h"
 #include <vector>
@@ -31,7 +31,7 @@
 namespace WPEFramework {
 namespace Plugin {
 
-    class TextToSpeechImplementation : public Exchange::ITextToSpeech, public TTS::TTSEventCallback {
+    class TextToSpeechImplementation : public Exchange::ITextToSpeech, public PluginHost::IStateControl, public TTS::TTSEventCallback {
     public:
         enum Event {
                 STATE_CHANGED,
@@ -49,10 +49,10 @@ namespace Plugin {
 
         class EXTERNAL Job : public Core::IDispatch {
         protected:
-             Job(TextToSpeechImplementation *tts, Event event, string &data)
+             Job(TextToSpeechImplementation *tts, Event event, JsonValue &params)
                 : _tts(tts)
                 , _event(event)
-                , _data(data) {
+                , _params(params) {
                 if (_tts != nullptr) {
                     _tts->AddRef();
                 }
@@ -69,18 +69,18 @@ namespace Plugin {
             }
 
        public:
-            static Core::ProxyType<Core::IDispatch> Create(TextToSpeechImplementation *tts, Event event, string data) {
-                return (Core::ProxyType<Core::IDispatch>(Core::ProxyType<Job>::Create(tts, event, data)));
+            static Core::ProxyType<Core::IDispatch> Create(TextToSpeechImplementation *tts, Event event, JsonValue params) {
+                return (Core::proxy_cast<Core::IDispatch>(Core::ProxyType<Job>::Create(tts, event, params)));
             }
 
             virtual void Dispatch() {
-                _tts->Dispatch(_event, _data);
+                _tts->Dispatch(_event, _params);
             }
 
         private:
             TextToSpeechImplementation *_tts;
             const Event _event;
-            const string _data;
+            const JsonValue _params;
         };
 
     public:
@@ -88,21 +88,28 @@ namespace Plugin {
         TextToSpeechImplementation(const TextToSpeechImplementation&) = delete;
         TextToSpeechImplementation& operator=(const TextToSpeechImplementation&) = delete;
 
-        virtual uint32_t Configure(PluginHost::IShell* service);
-        virtual void Register(INotification* sink) override ;
-        virtual void Unregister(INotification* sink) override ;
+        virtual uint32_t Configure(PluginHost::IShell* service) override;
+        virtual void Register(Exchange::ITextToSpeech::INotification* sink) override ;
+        virtual void Unregister(Exchange::ITextToSpeech::INotification* sink) override ;
 
-        virtual uint32_t Enable(const string &input, string &output /* @out */) override ;
-        virtual uint32_t ListVoices(const string &input, string &output /* @out */) override ;
-        virtual uint32_t SetConfiguration(const string &input, string &output /* @out */) override ;
-        virtual uint32_t GetConfiguration(const string &input, string &output /* @out */) override ;
-        virtual uint32_t IsEnabled(const string &input, string &output /* @out */) override ;
-        virtual uint32_t Speak(const string &input, string &output /* @out */) override ;
-        virtual uint32_t Cancel(const string &input, string &output /* @out */) override ;
-        virtual uint32_t Pause(const string &input, string &output /* @out */) override ;
-        virtual uint32_t Resume(const string &input, string &output /* @out */) override ;
-        virtual uint32_t IsSpeaking(const string &input, string &output /* @out */) override ;
-        virtual uint32_t GetSpeechState(const string &input, string &output /* @out */) override ;
+        virtual PluginHost::IStateControl::state State() const override { return PluginHost::IStateControl::RESUMED; }
+        virtual uint32_t Request(const command state) override;
+        virtual void Register(IStateControl::INotification* notification) override {}
+        virtual void Unregister(IStateControl::INotification* notification) override {}
+
+        virtual uint32_t Enable(const bool enable) override;
+        virtual uint32_t Enable(bool &enable /* @out */) const override;
+        virtual uint32_t ListVoices(const string language,RPC::IStringIterator*& voices/* @out */) const override;
+        virtual uint32_t SetConfiguration(const Exchange::ITextToSpeech::Configuration &object,Exchange::ITextToSpeech::TTSErrorDetail &status/* @out */) override ;
+        virtual uint32_t SetFallbackText(const string scenario,const string value) override ;
+        virtual uint32_t SetAPIKey(const string apikey) override ;
+        virtual uint32_t SetPrimaryVolDuck(const uint8_t prim) override ;
+        virtual uint32_t GetConfiguration(Exchange::ITextToSpeech::Configuration &object/* @out */) const override;
+        virtual uint32_t Speak(const string text,uint32_t &speechid/* @out */,Exchange::ITextToSpeech::TTSErrorDetail &status/* @out */) override;
+        virtual uint32_t Cancel(const uint32_t speechid) override;
+        virtual uint32_t Pause(const uint32_t speechid,Exchange::ITextToSpeech::TTSErrorDetail &status /* @out */) override;
+        virtual uint32_t Resume(const uint32_t speechid,Exchange::ITextToSpeech::TTSErrorDetail &status /* @out */) override;
+        virtual uint32_t GetSpeechState(const  uint32_t speechid,Exchange::ITextToSpeech::SpeechState &state/* @out */) override;
 
         virtual void onTTSStateChanged(bool enabled) override ;
         virtual void onVoiceChanged(std::string voice) override ;
@@ -118,6 +125,7 @@ namespace Plugin {
 
         BEGIN_INTERFACE_MAP(TextToSpeechImplementation)
         INTERFACE_ENTRY(Exchange::ITextToSpeech)
+        INTERFACE_ENTRY(PluginHost::IStateControl)
         END_INTERFACE_MAP
 
     private:
@@ -125,13 +133,12 @@ namespace Plugin {
         mutable Core::CriticalSection _adminLock;
         std::list<Exchange::ITextToSpeech::INotification*> _notificationClients;
 
-        void dispatchEvent(Event, JsonObject &params);
-        void Dispatch(Event event, string data);
+        void dispatchEvent(Event, const JsonValue &params);
+        void Dispatch(Event event, const JsonValue params);
 
     public:
         TextToSpeechImplementation();
         virtual ~TextToSpeechImplementation();
-        void setResponseArray(JsonObject& response, const char* key, const std::vector<std::string>& items);
 
         friend class Job;
     };
