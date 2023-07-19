3031ab8576d7c8bcf3fef08c5960a686288c062e
Pavithra Pandian <pavithra_pandian@comcast.com>
2023-07-19 08:08:56 +0000

    RDKTV-24212: Experimental Change

--- Tests/mocks/maintenanceMGR.h
+++ Tests/mocks/maintenanceMGR.h
@@ -26,6 +26,7 @@
  typedef enum {
   IARM_BUS_MAINTENANCEMGR_EVENT_UPDATE=0, /* Event status as data */
   IARM_BUS_DCM_NEW_START_TIME_EVENT, /* Payload as Time */
+  IARM_BUS_NETWORK_MANAGER_EVENT_INTERNET_CONNECTION_CHANGED,
  }IARM_Bus_MaintMGR_EventId_t;
  
  /* Notification to rdkservice over IARM */
@@ -58,6 +59,9 @@
   struct _DCM_DATA{
   char start_time[MAX_TIME_LEN];
   }startTimeUpdate;
+  struct _NETWORK_DATA{
+  String internetStatus;
+  }internetConnectionStatus;
   struct _MAINT_STATUS_UPDATE{
   IARM_Maint_module_status_t status;
   }maintenance_module_status;
