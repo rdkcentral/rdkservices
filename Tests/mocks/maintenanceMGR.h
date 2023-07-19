8afe5ad0fae1f9a1cf8b427515f51e39c17c6e4d
PaviP98 <92713403+PaviP98@users.noreply.github.com>
2023-07-19 16:32:05 +0530

    Update maintenanceMGR.h

--- Tests/mocks/maintenanceMGR.h
+++ Tests/mocks/maintenanceMGR.h
@@ -22,6 +22,7 @@
  #define _IARM_BUS_MAINTENANCEMGR_H
  
  #define IARM_BUS_MAINTENANCE_MGR_NAME "MaintenanceMGR"
+ #define IARM_BUS_NM_SRV_MGR_NAME "NET_SRV_MGR"
  
  typedef enum {
   IARM_BUS_MAINTENANCEMGR_EVENT_UPDATE=0, /* Event status as data */
