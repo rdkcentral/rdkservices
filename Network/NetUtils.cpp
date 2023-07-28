95e4924f19da507d64faabc7075e65fc45c4da86
KaurSup <skaur@synamedia.com>
2023-07-28 22:33:15 +0530

    RDK-42974 - [RDKE] Network - Replace System Commands
    This is a subtask of RDK 42211
    
    Reason for change: Trying to make RDK-E lean, as part of this gerrit, updating the popen calls in Rdkservices, Network plugin
    Test Procedure: Basic sanity and tested using curl commands as specified here : https://github.com/rdkcentral/rdkservices/tree/sprint/23Q3/Network#readme
    Risks: Medium.
    Priority: P0

--- Network/NetUtils.cpp
+++ Network/NetUtils.cpp
@@ -28,7 +28,6 @@
 //Defines
 
 #define NETUTIL_DEVICE_PROPERTIES_FILE          "/etc/device.properties"
-#define COMMAND_RESULT_FILE "cmdresult"
 
 
 namespace WPEFramework {
@@ -77,91 +76,6 @@ namespace WPEFramework {
             static std::string empty("");
             auto it = interface_descriptions.find(interface.substr(0, interface.find(':'))); // look up "eth0" (real interface) for "eth0:0" (virtual interface) input also
             return (it != interface_descriptions.end()) ? it->second : empty;
-        }
-
-        /*
-         * Returns >= 0 on success
-         * Blocking process so run in background thread
-         *
-         * If 'result' is not NULL then it will be set to the return status of the command
-         *
-         * If 'outputfile' is not NULL then it should contain a name for the outpur file. The output of the command will
-         *     be dumped to '/tmp/[outputfile]x' where x is a unique number.
-         *     The filepath will be returned in 'output' and the file should be deleted by the calling process after use.
-         */
-        int NetUtils::execCmd(const char *command, std::string &output, bool *result, const char *outputfile)
-        {
-            std::string commandString;
-            std::string resultFile;
-            char buffer[MAX_OUTPUT_LENGTH];
-            FILE *pipe = NULL;
-            size_t length = 0;
-
-            output.clear();
-
-            commandString.assign(command);
-
-            // If we have an output file then pipe the output from the command to /tmp/outpufile
-            if (outputfile)
-            {
-                getTmpFilename(outputfile, output); //append a count to get a unique filename and return the filename in output
-                commandString += " > ";
-                commandString += output;
-            }
-
-            // If we have requested a result, store the result of the command in /tmp/cmdresult
-            if (result)
-            {
-                getTmpFilename(COMMAND_RESULT_FILE, resultFile); //append a count to get a unique filename
-                commandString += "; echo $? > ";
-                commandString += resultFile;
-            }
-
-            pipe = popen(commandString.c_str(), "r");
-            if (pipe == NULL)
-            {
-                LOGERR("%s: failed to open file '%s' for read mode with result: %s", __FUNCTION__,
-                        commandString.c_str(), strerror(errno));
-                return -1;
-            }
-
-            LOGWARN("%s: opened file '%s' for read mode", __FUNCTION__,
-                    commandString.c_str());
-
-            while (!feof(pipe) && fgets(buffer, MAX_OUTPUT_LENGTH, pipe) != NULL)
-            {
-                // If we are not dumping it to file, store the output
-                if (!outputfile)
-                {
-                    output += buffer;
-                }
-            }
-
-            // Strip trailing line feed from the output
-            if ((length = output.length()) > 0)
-            {
-                if (output[length-1] == '\n')
-                {
-                    output.erase(length-1);
-                }
-            }
-
-            // If we have requested a result, query the contents of the result file
-            if (result)
-            {
-                std::string commandResult = "1";
-                if (!getFile(resultFile.c_str(), commandResult, true)) //delete file after reading
-                {
-                    LOGERR("%s: failed to get command result '%s'", __FUNCTION__,resultFile.c_str());
-                }
-                else
-                {
-                    *result =  (commandResult == "0");
-                    LOGINFO("%s: command result '%s'", __FUNCTION__,((*result)?"true":"false"));
-                }
-            }
-
-            return pclose(pipe);
         }
 
         /*
@@ -235,14 +149,6 @@ namespace WPEFramework {
             }
 
             return false;
-        }
-
-        void NetUtils::getTmpFilename(const char *in, std::string &out)
-        {
-            std::lock_guard<std::mutex> lock(m_counterProtect);
-            out = "/tmp/";
-            out += in;
-            out += std::to_string(m_counter++);
         }
 
         bool NetUtils::isIPV6LinkLocal(const std::string& address)
