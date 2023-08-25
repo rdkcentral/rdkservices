ec8ee860cc67c6f37e1f91662f46f9235a270bf4
sumshank <110598711+sumshank@users.noreply.github.com>
2023-08-25 14:24:19 +0530

    Updated copy rights

--- Tests/L2_tests/tests/thunder_tests.cpp
+++ Tests/L2_tests/tests/thunder_tests.cpp
@@ -1,16 +1,3 @@
-/*
-  Copyright (c) Synamedia 2023
-
-  P R O P R I E T A R Y  & C O N F I D E N T I A L
-
-  The copyright of this code and related documentation together with
-  any other associated intellectual property rights are vested in
-  Synamedia and may not be used except in accordance with the terms
-  of the license that you have entered into with Synamedia.
-  Use of this material without an express license from Synamedia
-  shall be an infringement of copyright and any other intellectual
-  property rights that may be incorporated with this material.
-*/
 
 #include <cstdio>
 #include "Module.h"
