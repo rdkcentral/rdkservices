3b8e137f5efe6ea409206d6b3a221da425066dfd
Karunakaran A <48997923+karuna2git@users.noreply.github.com>
2023-08-29 11:18:54 -0400

    Update RDKShell.cpp
    Updated the Minor Version

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -52,7 +52,7 @@
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 4
-#define API_VERSION_NUMBER_PATCH 2
+#define API_VERSION_NUMBER_PATCH 3
 
 const string WPEFramework::Plugin::RDKShell::SERVICE_NAME = "org.rdk.RDKShell";
 //methods
