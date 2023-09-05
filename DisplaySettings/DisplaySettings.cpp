6451e0488a8d0e5a4bb0f970f09d8a5d68cd80ff
ANANTHMARIMUTHU <90027158+dautapankumardora@users.noreply.github.com>
2023-09-05 16:20:40 +0100

    Update DisplaySettings.cpp

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -85,7 +85,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 3
-#define API_VERSION_NUMBER_PATCH 2
+#define API_VERSION_NUMBER_PATCH 4
 
 static bool isCecEnabled = false;
 static int  hdmiArcPortId = -1;
