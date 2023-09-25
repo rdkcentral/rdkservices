e6b1052fc0033016638b88d1a415d341eacaf18d
ANANTHMARIMUTHU <48992974+apatel859@users.noreply.github.com>
2023-09-25 08:02:49 -0400

    Update RDKShell.cpp

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -52,7 +52,7 @@
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 4
-#define API_VERSION_NUMBER_PATCH 5
+#define API_VERSION_NUMBER_PATCH 6
 
 const string WPEFramework::Plugin::RDKShell::SERVICE_NAME = "org.rdk.RDKShell";
 //methods
