0416ff7ce59f6fd7cea4337740dd4c99dcf9d879
Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>
2023-10-23 13:26:48 -0400

    Updated the version information for the release
    Signed-off-by: Karunakaran A <karunakaran_amirthalingam@cable.comcast.com>

--- Network/Network.cpp
+++ Network/Network.cpp
@@ -34,7 +34,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 2
-#define API_VERSION_NUMBER_PATCH 0
+#define API_VERSION_NUMBER_PATCH 1
 
 /* Netsrvmgr Based Macros & Structures */
 #define IARM_BUS_NM_SRV_MGR_NAME "NET_SRV_MGR"
