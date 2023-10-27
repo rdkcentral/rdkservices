adc424dba499e746c161f228439b5e24e85c5377
Adrian Muzyka <adrian.muzyka@consult.red>
2023-10-27 09:43:15 +0200

    RDKShell: checkpoint native apps on each suspend state change

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -197,11 +197,6 @@ bool sForceResidentAppLaunch = false;
 static bool sRunning = true;
 bool needsScreenshot = false;
 
-#ifdef HIBERNATE_NATIVE_APPS_ON_SUSPENDED
-std::mutex nativeAppWasResumedMutex;
-map<string,bool> nativeAppWasResumed;
-#endif
-
 #define ANY_KEY 65536
 #define RDKSHELL_THUNDER_TIMEOUT 20000
 #define RDKSHELL_POWER_TIME_WAIT 2.5
@@ -569,10 +564,7 @@ namespace WPEFramework {
                 if (Utils::getRFCConfig("Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AppHibernate.Enable", param)
                     && strncasecmp(param.value, "true", 4) == 0)
                 {
-                    nativeAppWasResumedMutex.lock();
-                    if ((mCallSign.find("Netflix") != std::string::npos || mCallSign.find("Cobalt") != std::string::npos)
-                        && nativeAppWasResumed.find(mCallSign) != nativeAppWasResumed.end()
-                        && nativeAppWasResumed[mCallSign])
+                    if ((mCallSign.find("Netflix") != std::string::npos || mCallSign.find("Cobalt") != std::string::npos))
                     {
                         // call RDKShell.hibernate
                         std::thread requestsThread =
@@ -585,15 +577,9 @@ namespace WPEFramework {
 
                         requestsThread.detach();
                     }
-                    nativeAppWasResumedMutex.unlock();
                 }
 #endif
             }
-#ifdef HIBERNATE_NATIVE_APPS_ON_SUSPENDED
-            nativeAppWasResumedMutex.lock();
-            nativeAppWasResumed[mCallSign] = (state == PluginHost::IStateControl::RESUMED);
-            nativeAppWasResumedMutex.unlock();
-#endif
 
           }
 
@@ -896,11 +882,6 @@ namespace WPEFramework {
                 if ((currentState == PluginHost::IShell::DEACTIVATED) || (currentState == PluginHost::IShell::DESTROYED))
                 {
                      gApplicationsExitReason[service->Callsign()] = AppLastExitReason::DEACTIVATED;
-#ifdef HIBERNATE_NATIVE_APPS_ON_SUSPENDED
-                    nativeAppWasResumedMutex.lock();
-                    nativeAppWasResumed[service->Callsign()] = false;
-                    nativeAppWasResumedMutex.unlock();
-#endif
                 }
                 if(service->Reason() == PluginHost::IShell::FAILURE)
                 {
