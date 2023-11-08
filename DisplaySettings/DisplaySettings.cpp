ee066357a1a6eff7647e432ff58ed6c4621289c8
poojah58 <105851865+poojah58@users.noreply.github.com>
2023-11-09 03:41:29 +0530

    Update DisplaySettings.cpp for Version update

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -85,7 +85,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 3
-#define API_VERSION_NUMBER_PATCH 6
+#define API_VERSION_NUMBER_PATCH 7
 
 static bool isCecEnabled = false;
 static int  hdmiArcPortId = -1;
