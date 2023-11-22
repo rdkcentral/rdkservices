4fffa1e5e3a677150bcf47df71c2a06e56e18dba
jackogorman10 <91594911+jackogorman10@users.noreply.github.com>
2023-11-22 16:16:17 +0000

    RDK-45551: return modalias and firmware version for game controllers - MAIN
    Reason for change: return the modalias and firmware version string from rdkservices
    
    Test Procedure: use curl command to get device properties from rdkservices and you should be able to see the modalias and firmware parameters
    
    Risks: Low
    
    Signed-off-by: Jack O'Gorman <jack.ogorman@sky.uk>

--- Bluetooth/Bluetooth.cpp
+++ Bluetooth/Bluetooth.cpp
@@ -40,7 +40,7 @@
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 4
+#define API_VERSION_NUMBER_PATCH 5
 
 const string WPEFramework::Plugin::Bluetooth::SERVICE_NAME = "org.rdk.Bluetooth";
 const string WPEFramework::Plugin::Bluetooth::METHOD_START_SCAN = "startScan";
@@ -809,6 +809,9 @@ namespace WPEFramework
                 deviceDetails["signalStrength"] = std::to_string(deviceProperty.m_signalLevel);
                 deviceDetails["rssi"] =  std::to_string(deviceProperty.m_rssi);
                 deviceDetails["batteryLevel"] =  std::to_string(deviceProperty.m_batteryLevel);
+                deviceDetails["modalias"] = string(deviceProperty.m_modalias);
+                deviceDetails["firmwareRevision"] = string(deviceProperty.m_firmwareRevision);
+
                 for (int i = 0; i < deviceProperty.m_serviceInfo.m_numOfService; i++)
                 {
                     profileInfo += string(deviceProperty.m_serviceInfo.m_profileInfo[i].m_profile);
