3a67703832e1192dcb106276845dc19b31e30086
cmuhammedrafi <103924677+cmuhammedrafi@users.noreply.github.com>
2023-11-24 21:11:15 +0530

    DELIA-63960: Removed error log line and documentation updated (#4677)
    * Removed Log lines and updated md file
    
    * DELIA-63960: Removed error log lines for getCaptivePortalURI and Document updated

--- Network/Network.cpp
+++ Network/Network.cpp
@@ -1275,7 +1275,6 @@ typedef struct _IARM_BUS_NetSrvMgr_Iface_EventData_t {
 
             if(m_isPluginInited)
             {
-                LOGERR("Captive potal URI feature not include");
                 response["URI"] = connectivityMonitor.getCaptivePortalURI();
             }
             else
