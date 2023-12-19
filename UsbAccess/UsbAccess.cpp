dd1960e6c07de1dee13a5149d8b4dd766b9d2c08
agampa263 <akshaykumar_gampa@comcast.com>
2023-12-19 13:59:08 +0000

    RDKTV-26935: [FTA Sprint Pioneer] USB log upload failed writing error

--- UsbAccess/UsbAccess.cpp
+++ UsbAccess/UsbAccess.cpp
@@ -578,7 +578,7 @@ namespace Plugin {
             if(!usbPath.empty())
             {
                 string script = (ARCHIVE_LOGS_SCRIPT + " " + usbPath);
-                FILE* fp = popen(script.c_str(), "r");
+                FILE* fp = v_secure_popen("r","%s %s",ARCHIVE_LOGS_SCRIPT.c_str(),usbPath.c_str());
                 if (NULL != fp) {
                     char buf[256];
                     while(fgets(buf, sizeof(buf), fp) != NULL)
