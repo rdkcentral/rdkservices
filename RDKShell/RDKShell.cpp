f18c57766a578fc3aa8536486dedacccedd63115
Andrzej Surdej <Andrzej_Surdej@comcast.com>
2024-01-02 13:28:08 +0100

    Update version and changelog
    Signed-off-by: Andrzej Surdej <Andrzej_Surdej@comcast.com>

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -52,7 +52,7 @@
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 4
-#define API_VERSION_NUMBER_PATCH 13
+#define API_VERSION_NUMBER_PATCH 14
 
 const string WPEFramework::Plugin::RDKShell::SERVICE_NAME = "org.rdk.RDKShell";
 //methods
