a3a84007381198db5b8f0532588078bb11e832b8
agampa263 <akshaykumar_gampa@comcast.com>
2024-01-10 13:24:55 +0000

    RDK-45464:[Coverity] Various issues in rdkservices - Phase 3a

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -130,7 +130,7 @@ namespace WPEFramework
             for (auto it = m_runningItems.cbegin(); it != m_runningItems.cend(); ++it)
             {
                 unsigned int timerId = *it;
-                if (timerId < 0 || timerId >= m_timerItems.size())
+                if (timerId >= m_timerItems.size()) //CID 144111
                 {
                     LOGERR("Internal error: wrong timerId");
                     continue;
@@ -223,7 +223,7 @@ namespace WPEFramework
             for (auto it = m_runningItems.cbegin(); it != m_runningItems.cend(); ++it)
             {
                 unsigned int timerId = *it;
-                if (timerId < 0 || timerId >= m_timerItems.size())
+                if (timerId >= m_timerItems.size()) //cID 144114
                 {
                     LOGERR("Internal error: wrong timerId");
                     continue;
@@ -316,7 +316,8 @@ namespace WPEFramework
 
             item.state = INITIAL;
             item.interval = std::stod(parameters["interval"].String());
-
+            item.repeatInterval = 0.0;
+            item.remindBefore = 0.0;
             item.mode = GENERIC;
             if (parameters.HasLabel("mode"))
             {
@@ -352,7 +353,7 @@ namespace WPEFramework
             unsigned int timerId;
             getNumberParameter("timerId", timerId);
 
-            if (timerId >=0 && timerId < m_timerItems.size())
+            if (timerId < m_timerItems.size()) //CID 144115
             {
                 if (CANCELED != m_timerItems[timerId].state)
                 {
@@ -382,7 +383,7 @@ namespace WPEFramework
             unsigned int timerId;
             getNumberParameter("timerId", timerId);
 
-            if (timerId >=0 && timerId < m_timerItems.size())
+            if (timerId < m_timerItems.size())//CID 144112
             {
                 if (RUNNING == m_timerItems[timerId].state)
                 {
@@ -412,7 +413,7 @@ namespace WPEFramework
             unsigned int timerId;
             getNumberParameter("timerId", timerId);
 
-            if (timerId >=0 && timerId < m_timerItems.size())
+            if (timerId < m_timerItems.size()) //CID 144110
             {
                 if (SUSPENDED == m_timerItems[timerId].state)
                 {
@@ -443,7 +444,7 @@ namespace WPEFramework
             unsigned int timerId;
             getNumberParameter("timerId", timerId);
 
-            if (timerId >= 0 && timerId < m_timerItems.size())
+            if (timerId < m_timerItems.size()) //CID 144113
             {
                 getTimerStatus(timerId, response);
             }
@@ -492,6 +493,11 @@ namespace WPEFramework
                 memcpy(dataToSend.data, buf, dataToSend.length);
                 LOGINFO("Timer send CEC %s", SLEEP == m_timerItems[timerId].mode ? "Standby" : "Wake");
                 IARM_Bus_Call(IARM_BUS_CECMGR_NAME,IARM_BUS_CECMGR_API_Send,(void *)&dataToSend, sizeof(dataToSend));
+                if (res != IARM_RESULT_SUCCESS)
+                {
+                    LOGERR("ERROR - API_Send IARM_BUS_CECMGR FAILED, res: %d", (int)res);
+                    return STATUS_INVALID_STATE;
+                }
             }
 #endif
             JsonObject params;
