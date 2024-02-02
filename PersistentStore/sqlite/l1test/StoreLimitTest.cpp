cc8e18b8c7bd2cab6c0ec1ca78c5950042a80991
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2024-02-02 17:50:50 +0200

    RDK-45037 : Secure Storage Thunder Plugin
    Reason for change: Refactoring
    Tests written for all the test levels (L0,L1) for the entire plugin.
    Test Procedure: None
    Risks: None
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>

--- PersistentStore/sqlite/l1test/StoreLimitTest.cpp
+++ PersistentStore/sqlite/l1test/StoreLimitTest.cpp
@@ -1,98 +1,112 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "../Handle.h"
-#include "../Store2Type.h"
-#include "../StoreLimitType.h"
-
-using namespace WPEFramework;
-using namespace WPEFramework::Plugin;
+#include "../Store2.h"
+#include "../StoreLimit.h"
 
 using ::testing::Eq;
 using ::testing::Test;
+using ::WPEFramework::Exchange::IStore2;
+using ::WPEFramework::Exchange::IStoreLimit;
+using ::WPEFramework::Plugin::Sqlite::Store2;
+using ::WPEFramework::Plugin::Sqlite::StoreLimit;
 
-const std::string Path = "/tmp/sqlite/l1test/storelimittest";
-const uint32_t MaxSize = 100;
-const uint32_t MaxValue = 10;
-const uint32_t Limit = 50;
+const auto kPath = "/tmp/persistentstore/sqlite/l1test/storelimittest";
+const auto kMaxSize = 100;
+const auto kMaxValue = 10;
+const auto kLimit = 50;
+const auto kValue = "value_1";
+const auto kKey = "key_1";
+const auto kAppId = "app_id_1";
+const auto kNoTtl = 0;
+const auto kScope = IStoreLimit::ScopeType::DEVICE;
+const auto kEmpty = "";
+const auto kOversize = "this is too large";
+const auto kUnknown = "unknown";
+const auto kLimit20 = 20;
+const auto kLimit30 = 30;
+const auto kLimit40 = 40;
 
 class AStoreLimit : public Test {
 protected:
-    Exchange::IStoreLimit* limit;
-    ~AStoreLimit() override = default;
-    void SetUp() override
+    IStoreLimit* limit;
+    AStoreLimit()
+        : limit(WPEFramework::Core::Service<StoreLimit>::Create<IStoreLimit>(kPath, kMaxSize, kMaxValue))
     {
-        Core::File(Path).Destroy();
-        // File is destroyed
-
-        Core::SystemInfo::SetEnvironment(_T("PERSISTENTSTORE_PATH"), Path);
-        Core::SystemInfo::SetEnvironment(_T("PERSISTENTSTORE_MAXSIZE"), std::to_string(MaxSize));
-        Core::SystemInfo::SetEnvironment(_T("PERSISTENTSTORE_MAXVALUE"), std::to_string(MaxValue));
-        Core::SystemInfo::SetEnvironment(_T("PERSISTENTSTORE_LIMIT"), std::to_string(Limit));
-        limit = Core::Service<Sqlite::StoreLimitType<Sqlite::Handle>>::Create<Exchange::IStoreLimit>();
     }
-    void TearDown() override
+    ~AStoreLimit() override
     {
         limit->Release();
     }
 };
 
-TEST_F(AStoreLimit, SetsLimitForUnknownNamespaceWithoutError)
-{
-    EXPECT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "x", 10), Eq(Core::ERROR_NONE));
-}
-
-TEST_F(AStoreLimit, DoesNotGetLimitForUnknownNamespace)
+TEST_F(AStoreLimit, DoesNotGetNamespaceStorageLimitWhenNamespaceUnknown)
 {
     uint32_t value;
-    EXPECT_THAT(limit->GetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "x", value), Eq(Core::ERROR_NOT_EXIST));
+    EXPECT_THAT(limit->GetNamespaceStorageLimit(kScope, kUnknown, value), Eq(WPEFramework::Core::ERROR_NOT_EXIST));
 }
 
-TEST_F(AStoreLimit, DoesNotSetLimitForTooLargeNamespaceName)
+TEST_F(AStoreLimit, DoesNotSetNamespaceStorageLimitWhenNamespaceEmpty)
 {
-    EXPECT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "this is too large", 10), Eq(Core::ERROR_INVALID_INPUT_LENGTH));
+    EXPECT_THAT(limit->SetNamespaceStorageLimit(kScope, kEmpty, kLimit20), Eq(WPEFramework::Core::ERROR_INVALID_INPUT_LENGTH));
 }
 
-TEST_F(AStoreLimit, DoesNotSetLimitWhenAtMaxSize)
+TEST_F(AStoreLimit, DoesNotSetNamespaceStorageLimitWhenNamespaceOversize)
 {
-    for (int i = 0; i < 27; i++) {
-        EXPECT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns" + std::to_string(i), 10), Eq(Core::ERROR_NONE));
-    }
-    EXPECT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns", 10), Eq(Core::ERROR_NONE));
-    // Size is 100
-
-    EXPECT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "x", 10), Eq(Core::ERROR_INVALID_INPUT_LENGTH));
+    EXPECT_THAT(limit->SetNamespaceStorageLimit(kScope, kOversize, kLimit20), Eq(WPEFramework::Core::ERROR_INVALID_INPUT_LENGTH));
 }
 
-TEST_F(AStoreLimit, GetsDefaultLimitForExistingNamespace)
+TEST_F(AStoreLimit, SetsNamespaceStorageLimit)
 {
-    auto store2 = Core::Service<Sqlite::Store2Type<Sqlite::Handle>>::Create<Exchange::IStore2>();
-    ASSERT_THAT(store2->SetValue(Exchange::IStore2::ScopeType::DEVICE, "ns1", "key1", "value1", 0), Eq(Core::ERROR_NONE));
+    ASSERT_THAT(limit->SetNamespaceStorageLimit(kScope, kAppId, kLimit20), Eq(WPEFramework::Core::ERROR_NONE));
+    uint32_t value;
+    ASSERT_THAT(limit->GetNamespaceStorageLimit(kScope, kAppId, value), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(value, Eq(kLimit20));
+}
+
+TEST_F(AStoreLimit, SetsNamespaceStorageLimitWhenAlreadySet)
+{
+    ASSERT_THAT(limit->SetNamespaceStorageLimit(kScope, kAppId, kLimit30), Eq(WPEFramework::Core::ERROR_NONE));
+    uint32_t value;
+    ASSERT_THAT(limit->GetNamespaceStorageLimit(kScope, kAppId, value), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(value, Eq(kLimit30));
+    ASSERT_THAT(limit->SetNamespaceStorageLimit(kScope, kAppId, kLimit40), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(limit->GetNamespaceStorageLimit(kScope, kAppId, value), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(value, Eq(kLimit40));
+}
+
+TEST_F(AStoreLimit, DoesNotSetNamespaceStorageLimitWhenReachedMaxSize)
+{
+    auto store2 = WPEFramework::Core::Service<Store2>::Create<IStore2>(kPath, kMaxSize, kMaxValue, kLimit);
+    ASSERT_THAT(store2->DeleteNamespace(kScope, kAppId), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, "8InMXXU4hM", "YWKN74ODMf", "N0ed2C2h4n", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, "XhrICnuerw", "jPKODBDk5K", "d3BarkA5xF", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, "WNeBknDDI2", "GC96ZN6Fuq", "IBF2E1MLQh", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, "V92", "R1R", "rHk", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(limit->SetNamespaceStorageLimit(kScope, kAppId, kLimit), Eq(WPEFramework::Core::ERROR_INVALID_INPUT_LENGTH));
+    EXPECT_THAT(store2->DeleteNamespace(kScope, "8InMXXU4hM"), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(store2->DeleteNamespace(kScope, "XhrICnuerw"), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(store2->DeleteNamespace(kScope, "WNeBknDDI2"), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(store2->DeleteNamespace(kScope, "V92"), Eq(WPEFramework::Core::ERROR_NONE));
     store2->Release();
-    // Namespace added
-
-    uint32_t value;
-    EXPECT_THAT(limit->GetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", value), Eq(Core::ERROR_NONE));
-    EXPECT_THAT(value, Eq(Limit));
 }
 
-TEST_F(AStoreLimit, GetsLimitThatWasSet)
+TEST_F(AStoreLimit, EnforcesSetValueToFailWhenReachedDefaultLimit)
 {
-    ASSERT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", 30), Eq(Core::ERROR_NONE));
-    // Namespace limit set
-
-    uint32_t value;
-    EXPECT_THAT(limit->GetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", value), Eq(Core::ERROR_NONE));
-    EXPECT_THAT(value, Eq(30));
+    auto store2 = WPEFramework::Core::Service<Store2>::Create<IStore2>(kPath, kMaxSize, kMaxValue, kLimit);
+    ASSERT_THAT(store2->DeleteNamespace(kScope, kAppId), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, kAppId, "YWKN74ODMf", "N0ed2C2h4n", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, kAppId, "jPKODBDk5K", "d3BarkA5xF", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, kAppId, kKey, kValue, kNoTtl), Eq(WPEFramework::Core::ERROR_INVALID_INPUT_LENGTH));
+    store2->Release();
 }
 
-TEST_F(AStoreLimit, UpdatesLimitThatWasSet)
+TEST_F(AStoreLimit, EnforcesSetValueToFailWhenReachedLimit)
 {
-    ASSERT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", 30), Eq(Core::ERROR_NONE));
-    // Namespace limit set
-
-    ASSERT_THAT(limit->SetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", 40), Eq(Core::ERROR_NONE));
-    uint32_t value;
-    EXPECT_THAT(limit->GetNamespaceStorageLimit(Exchange::IStore2::ScopeType::DEVICE, "ns1", value), Eq(Core::ERROR_NONE));
-    EXPECT_THAT(value, Eq(40));
+    auto store2 = WPEFramework::Core::Service<Store2>::Create<IStore2>(kPath, kMaxSize, kMaxValue, kLimit);
+    ASSERT_THAT(store2->DeleteNamespace(kScope, kAppId), Eq(WPEFramework::Core::ERROR_NONE));
+    EXPECT_THAT(limit->SetNamespaceStorageLimit(kScope, kAppId, kLimit20), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, kAppId, "YWKN74ODMf", "N0ed2C2h4n", kNoTtl), Eq(WPEFramework::Core::ERROR_NONE));
+    ASSERT_THAT(store2->SetValue(kScope, kAppId, kKey, kValue, kNoTtl), Eq(WPEFramework::Core::ERROR_INVALID_INPUT_LENGTH));
+    store2->Release();
 }
