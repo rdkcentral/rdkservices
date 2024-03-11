2e9c141397f49670a25da9d22e6b5422c65b399d
aishwariya.bhaskar <aishwariya.bhaskar@sky.uk>
2024-03-11 09:26:56 +0000

    RDK-46919: RDKServices Update to add setMixerLevels API

--- Tests/mocks/devicesettings.h
+++ Tests/mocks/devicesettings.h
@@ -559,6 +559,7 @@ public:
     virtual bool isPortConnected(int8_t Port) const = 0;
     virtual std::string getCurrentVideoMode() const = 0;
     virtual void selectPort(int8_t Port,bool audioMix = false, int videoPlane = 0,bool topMost = false) const = 0;
+    virtual void setAudioMixerLevels(int gain, int volume) const = 0;
     virtual void scaleVideo(int32_t x, int32_t y, int32_t width, int32_t height) const = 0;
 
     virtual void getEDIDBytesInfo(int iHdmiPort, std::vector<uint8_t>& edid) const = 0;
