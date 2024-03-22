d1aeb48b02956f4395a1c4335d684165b12cf7d3
kumari_priyanka <kumari_priyanka@comcast.com>
2024-03-22 10:41:42 +0000

    Revert "RDKDEV-1002 : Upstream the Fix for RDKShell Events Not Getting Triggered in Rdkservices"
    This reverts commit d08deb4dff2b9926b299fdf55da790c92f3a1c5a.
    A new PR will be created for this commit

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -1058,7 +1058,7 @@ namespace WPEFramework {
                            sem_wait(&request->mSemaphore);
                        }
                        gRdkShellMutex.lock();
-		       RdkShell::CompositorController::addListener(service->Callsign(), mShell.mEventListener);
+                       RdkShell::CompositorController::addListener(clientidentifier, mShell.mEventListener);
                        gRdkShellMutex.unlock();
                        gPluginDataMutex.lock();
                        std::string className = service->ClassName();
@@ -1178,7 +1178,7 @@ namespace WPEFramework {
                     gRdkShellMutex.unlock();
                     sem_wait(&request->mSemaphore);
                     gRdkShellMutex.lock();
-		    RdkShell::CompositorController::removeListener(service->Callsign(), mShell.mEventListener);
+                    RdkShell::CompositorController::removeListener(service->Callsign(), mShell.mEventListener);
                     gRdkShellMutex.unlock();
                 }
                 
