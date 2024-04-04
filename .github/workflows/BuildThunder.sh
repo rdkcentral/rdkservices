19cfe5140a69a61ea320333456021a0a32ae7c25
tabbas651 <74683978+tabbas651@users.noreply.github.com>
2024-04-04 13:15:51 -0400

    Update BuildThunder.sh

--- .github/workflows/BuildThunder.sh
+++ .github/workflows/BuildThunder.sh
@@ -18,7 +18,7 @@ pip install jsonref
 
 git clone https://github.com/rdkcentral/ThunderTools.git
 
-cmake -G Ninja -S Thunder/Tools -B build/ThunderTools -DCMAKE_INSTALL_PREFIX="install/usr"
+cmake -G Ninja -S ThunderTools -B build/ThunderTools -DCMAKE_INSTALL_PREFIX="install/usr"
 
 cmake --build build/ThunderTools --target install
 
