4518ed3d8bb2256fd8b6d84a3dcdd119b52fc02f
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2024-04-09 17:19:02 +0300

    adapt ServiceMock to the recent changes in Thunder IShell interface

--- PersistentStore/l0test/ServiceMock.h
+++ PersistentStore/l0test/ServiceMock.h
@@ -23,6 +23,7 @@ public:
     MOCK_METHOD(WPEFramework::PluginHost::ISubSystem*, SubSystems, (), (override));
     MOCK_METHOD(uint32_t, Submit, (const uint32_t, const WPEFramework::Core::ProxyType<WPEFramework::Core::JSON::IElement>&), (override));
     MOCK_METHOD(void, Notify, (const string&), (override));
+    MOCK_METHOD(void, Notify, (const string&, const string&), (override));
     MOCK_METHOD(void*, QueryInterfaceByCallsign, (const uint32_t, const string&), (override));
     MOCK_METHOD(void, Register, (WPEFramework::PluginHost::IPlugin::INotification*), (override));
     MOCK_METHOD(void, Unregister, (WPEFramework::PluginHost::IPlugin::INotification*), (override));
