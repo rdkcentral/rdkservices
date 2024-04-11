ec3a9a470e0e01e9d52624bd51c7f5161ae74de3
yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>
2024-04-11 19:11:25 +0000

    RDKTV-29983 - [Element-X3] ENT-32102 Error observed when trying to connect through Miracast using Redmi Device
    Signed-off-by: yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>

--- Miracast/P2P/MiracastP2P.cpp
+++ Miracast/P2P/MiracastP2P.cpp
@@ -437,8 +437,8 @@ MiracastError MiracastP2P::set_WFDParameters(void)
         command = "SET p2p_ssid_postfix -Element-Xumo-TV";
         executeCommand(command, NON_GLOBAL_INTERFACE, retBuffer);
 
-        /* Set p2p_go_intent to 15 */
-        command = "SET p2p_go_intent 15";
+        /* Set p2p_go_intent to 14 */
+        command = "SET p2p_go_intent 14";
         executeCommand(command, NON_GLOBAL_INTERFACE, retBuffer);
 
         m_isWiFiDisplayParamsEnabled = true;
