bcb8ef09087430f9c3e2988950cb7fe38da5ae98
Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
2024-04-23 16:13:54 -0400

    Merge pull request #5178 from rdkcentral/DELIA-65017
    DELIA-65017 : Out-of-process improvements to PersistentStore

--- PersistentStore/l0test/PersistentStoreImplementationMock.h
+++ PersistentStore/l0test/PersistentStoreImplementationMock.h
@@ -1,17 +1,26 @@
 #pragma once
 
 #include <gmock/gmock.h>
+#include <interfaces/IStore.h>
 #include <interfaces/IStore2.h>
 #include <interfaces/IStoreCache.h>
 
-class Store2Mock : public WPEFramework::Exchange::IStore2,
-                   public WPEFramework::Exchange::IStoreCache,
-                   public WPEFramework::Exchange::IStoreInspector,
-                   public WPEFramework::Exchange::IStoreLimit {
+class PersistentStoreImplementationMock
+    : public WPEFramework::Exchange::IStore,
+      public WPEFramework::Exchange::IStore2,
+      public WPEFramework::Exchange::IStoreCache,
+      public WPEFramework::Exchange::IStoreInspector,
+      public WPEFramework::Exchange::IStoreLimit {
 public:
-    ~Store2Mock() override = default;
-    MOCK_METHOD(uint32_t, Register, (INotification*), (override));
-    MOCK_METHOD(uint32_t, Unregister, (INotification*), (override));
+    ~PersistentStoreImplementationMock() override = default;
+    MOCK_METHOD(uint32_t, Register, (IStore::INotification * notification), (override));
+    MOCK_METHOD(uint32_t, Unregister, (IStore::INotification * notification), (override));
+    MOCK_METHOD(uint32_t, SetValue, (const string& ns, const string& key, const string& value), (override));
+    MOCK_METHOD(uint32_t, GetValue, (const string& ns, const string& key, string& value), (override));
+    MOCK_METHOD(uint32_t, DeleteKey, (const string& ns, const string& key), (override));
+    MOCK_METHOD(uint32_t, DeleteNamespace, (const string& ns), (override));
+    MOCK_METHOD(uint32_t, Register, (IStore2::INotification*), (override));
+    MOCK_METHOD(uint32_t, Unregister, (IStore2::INotification*), (override));
     MOCK_METHOD(uint32_t, SetValue, (const IStore2::ScopeType scope, const string& ns, const string& key, const string& value, const uint32_t ttl), (override));
     MOCK_METHOD(uint32_t, GetValue, (const IStore2::ScopeType scope, const string& ns, const string& key, string& value, uint32_t& ttl), (override));
     MOCK_METHOD(uint32_t, DeleteKey, (const IStore2::ScopeType scope, const string& ns, const string& key), (override));
@@ -22,7 +31,8 @@ public:
     MOCK_METHOD(uint32_t, GetStorageSizes, (const IStoreInspector::ScopeType scope, INamespaceSizeIterator*& storageList), (override));
     MOCK_METHOD(uint32_t, GetNamespaceStorageLimit, (const IStoreLimit::ScopeType scope, const string& ns, uint32_t& size), (override));
     MOCK_METHOD(uint32_t, SetNamespaceStorageLimit, (const IStoreLimit::ScopeType scope, const string& ns, const uint32_t size), (override));
-    BEGIN_INTERFACE_MAP(Store2Mock)
+    BEGIN_INTERFACE_MAP(PersistentStoreImplementationMock)
+    INTERFACE_ENTRY(IStore)
     INTERFACE_ENTRY(IStore2)
     INTERFACE_ENTRY(IStoreCache)
     INTERFACE_ENTRY(IStoreInspector)
