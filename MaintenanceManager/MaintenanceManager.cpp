813e6bf6ae9f72f4f986161dc8be5b72b6f95a26
ANANTHMARIMUTHU <gomathishankar37@gmail.com>
2024-05-13 16:45:08 +0530

    Update MaintenanceManager.cpp

--- MaintenanceManager/MaintenanceManager.cpp
+++ MaintenanceManager/MaintenanceManager.cpp
@@ -518,6 +518,7 @@ namespace WPEFramework {
 
         void MaintenanceManager::setPartnerId(string partnerid)
         {
+	    LOGINFO("Initiate setPartnerId...");
             const char* authservice_callsign = "org.rdk.AuthService.1";
             PluginHost::IShell::state state;
             WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement>* thunder_client = nullptr;
@@ -534,6 +535,9 @@ namespace WPEFramework {
                     joGetParams["partnerId"] = partnerid;
 
                     thunder_client->Invoke<JsonObject, JsonObject>(5000, "setPartnerId", joGetParams, joGetResult);
+		    string responseJson;
+		    joGetResult.ToString(responseJson);
+	            LOGINFO("AuthService Response Data: %s", responseJson.c_str());
                     if (joGetResult.HasLabel("success") && joGetResult["success"].Boolean()) {
                         LOGINFO("Successfully set the partnerId via Authservice");
                     } else {
@@ -863,6 +867,7 @@ namespace WPEFramework {
 
                 // Set the RFC values for deviceInitializationContext parameters
                 setRFC(rfc_parameter.c_str(), paramValue.c_str(), rfc_dataType);
+		LOGINFO("deviceInitializationContext parameters set successfully via RFC");
 
                 if (strcmp(key.c_str(), "partnerId") == 0)
                 {
