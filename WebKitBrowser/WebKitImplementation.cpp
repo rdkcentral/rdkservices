732e1dd8847f51f003a74c745fb64b214c9518e0
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2024-05-23 15:12:00 +0200

    Adding base data and cache directories to WebsiteDataManager as persistent and volatile paths

--- WebKitBrowser/WebKitImplementation.cpp
+++ WebKitBrowser/WebKitImplementation.cpp
@@ -2854,6 +2854,8 @@ static GSourceFuncs _handlerIntervention =
                     "indexeddb-directory", indexedDBPath,
                     "origin-storage-ratio", originStorageRatio,
                     "total-storage-ratio", totalStorageRatio,
+                    "base-data-directory", _service->PersistentPath(),
+                    "base-cache-directory", _service->VolatilePath(),
                      nullptr);
                 g_free(indexedDBPath);
 #else
