76f47382e683f69e30759349c4e9133e702ff0d0
Gomathi Shankar <gomathishankar37@gmail.com>
2024-05-31 17:33:22 +0530

    LLAMA-14513: Delay Unsolicited Maintenance start event in WAI-enabled case (#5363)

--- MaintenanceManager/MaintenanceManager.cpp
+++ MaintenanceManager/MaintenanceManager.cpp
@@ -286,11 +286,22 @@ namespace WPEFramework {
             uint8_t i=0;
             string cmd="";
             bool internetConnectStatus=false;
+	    bool delayMaintenanceStarted = false;
+
             std::unique_lock<std::mutex> lck(m_callMutex);
             LOGINFO("Executing Maintenance tasks");
-            m_statusMutex.lock();
-            MaintenanceManager::_instance->onMaintenanceStatusChange(MAINTENANCE_STARTED);
-            m_statusMutex.unlock();
+
+#if defined(ENABLE_WHOAMI)
+	    /* Purposefully delaying MAINTENANCE_STARTED status to honor POWER compliance */
+	    if (UNSOLICITED_MAINTENANCE == g_maintenance_type) {
+	    	delayMaintenanceStarted = true;
+	    }
+#endif
+	    if (!delayMaintenanceStarted) {
+		m_statusMutex.lock();
+                MaintenanceManager::_instance->onMaintenanceStatusChange(MAINTENANCE_STARTED);
+                m_statusMutex.unlock();
+	    }
 
             /* cleanup if not empty */
             if(!tasks.empty()){
@@ -351,6 +362,12 @@ namespace WPEFramework {
                 }
                 return;
             }
+
+	    if (delayMaintenanceStarted) {
+		m_statusMutex.lock();
+                MaintenanceManager::_instance->onMaintenanceStatusChange(MAINTENANCE_STARTED);
+                m_statusMutex.unlock();
+	    }
 
             LOGINFO("Reboot_Pending :%s",g_is_reboot_pending.c_str());
 
