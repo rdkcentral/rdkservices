8fa50c56a14ed7377c1864e0ce5dc7c5fc1c3eb0
viveksinghnarwaria <viveksingh_narwaria@comcast.com>
2024-06-28 15:14:05 +0000

    Updated the API lock implementation for HdcpProfile and XCast

--- XCast/XCast.cpp
+++ XCast/XCast.cpp
@@ -70,7 +70,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 19
+#define API_VERSION_NUMBER_PATCH 20
 
 namespace WPEFramework {
 
