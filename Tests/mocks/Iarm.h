39d9758a6b1c382c2d7638ee572b0d8ffd8a3535
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2024-08-13 13:39:49 -0400

    RDK-50542 - Fsr thunder plugin support to set and get fsr flag
    Reason for change:
    Add FSR functionality to systemservices
    Test Procedure: None
    Risks: Low
    Priority: P1
    
    Signed-off-by:Hayden Gfeller <Hayden_Gfeller@comcast.com>

--- Tests/mocks/Iarm.h
+++ Tests/mocks/Iarm.h
@@ -946,7 +946,7 @@ typedef struct _IARM_Bus_MFRLib_SetBLSplashScreen_Param{
 	char path[255];
 } IARM_Bus_MFRLib_SetBLSplashScreen_Param_t;
 
-typedef bool IARM_Bus_MFRLib_FsrFlag_Param; // true or false
+typedef bool IARM_Bus_MFRLib_FsrFlag_Param_t; // true or false
 
 #define IARM_BUS_CECMGR_API_isAvailable "isAvailable"
 #define IARM_BUS_DSMGR_API_dsHdmiInGetNumberOfInputs    "dsHdmiInGetNumberOfInputs"
