df26aff82394d0ac47ca58f725fa4f847ed698de
ANANTHMARIMUTHU <Sergey_Borushevsky2@cable.comcast.com>
2024-08-26 19:47:27 +0300

    Fixed version numbers.

--- UserSettings/UserSettings.cpp
+++ UserSettings/UserSettings.cpp
@@ -20,8 +20,8 @@
 #include "UserSettings.h"
 
 #define API_VERSION_NUMBER_MAJOR 1
-#define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 3
+#define API_VERSION_NUMBER_MINOR 1
+#define API_VERSION_NUMBER_PATCH 2
 
 namespace WPEFramework
 {
