a574111050a51544cf68a14cf44be49d7b08516d
Sergey Borushevsky <Sergey_Borushevsky2@cable.comcast.com>
2024-09-04 23:59:09 +0300

    Used COM-RPC instead of JSON-RPC to check status and activate plugin.

--- Telemetry/Telemetry.cpp
+++ Telemetry/Telemetry.cpp
@@ -116,10 +116,11 @@ namespace WPEFramework
         {
 
 #ifdef HAS_RBUS
-            if (!Utils::isPluginActivated(SYSTEMSERVICES_CALLSIGN))
-                Utils::activatePlugin(SYSTEMSERVICES_CALLSIGN);
-
-            if (Utils::isPluginActivated(SYSTEMSERVICES_CALLSIGN))
+            PluginHost::IShell::state state;
+            if ((Utils::getServiceState(service, SYSTEMSERVICES_CALLSIGN, state) != Core::ERROR_NONE) || (state != PluginHost::IShell::state::ACTIVATED))
+                Utils::activatePlugin(service, SYSTEMSERVICES_CALLSIGN);
+            
+            if ((Utils::getServiceState(service, SYSTEMSERVICES_CALLSIGN, state) == Core::ERROR_NONE) && (state == PluginHost::IShell::state::ACTIVATED))
             {
                 m_systemServiceConnection = Utils::getThunderControllerClient(SYSTEMSERVICES_CALLSIGN);
 
