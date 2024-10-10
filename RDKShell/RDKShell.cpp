583edb4898bd72b04e62deece4d098d7cdc49d7b
rpadin319 <renjith_warier@comcast.com>
2024-10-10 16:08:04 +0000

    RDKTV-32671 wait for graphics subsystem before RA is activated

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -53,7 +53,7 @@
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 6
-#define API_VERSION_NUMBER_PATCH 3
+#define API_VERSION_NUMBER_PATCH 4
 
 const string WPEFramework::Plugin::RDKShell::SERVICE_NAME = "org.rdk.RDKShell";
 //methods
@@ -1931,24 +1931,7 @@ namespace WPEFramework {
             {
                 waitForPersistentStore = false;
             }
-	    if(!factoryMacMatched){
-                   int32_t status = 0;
-                   std::string callsign("ResidentApp");
-                   JsonObject activateParams,response;
-                   activateParams.Set("callsign",callsign.c_str());
-                   JsonObject activateResult;
-                   auto thunderController = getThunderControllerClient();
-                   status = thunderController->Invoke<JsonObject, JsonObject>(RDKSHELL_THUNDER_TIMEOUT, "activate", activateParams, activateResult);
-                   std::cout << "Activating ResidentApp from RDKShell during bootup with Status:" << status << std::endl;
-                   if (status > 0){
-                           response["message"] = "resident app launch failed";
-                           std::cout << "resident app launch failed from rdkshell" << std::endl;
-                   }
-                  else {
-                        response["message"] = "resident app launch success";
-                        std::cout << "resident app launch success from rdkshell" << std::endl;
-                  }
-            }
+
             char* blockResidentApp = getenv("RDKSHELL_BLOCK_RESIDENTAPP_FACTORYMODE");
             if (NULL != blockResidentApp)
             {
@@ -1998,7 +1981,17 @@ namespace WPEFramework {
                         }
                         else
                         {
-                          std::cout << "not launching factory app as conditions not matched\n";
+                            std::cout << "Launch RA not waiting for persistent store\n";
+                            gRdkShellMutex.unlock();
+                            int32_t status = 0;
+                            std::string callsign("ResidentApp");
+                            JsonObject activateParams;
+                            activateParams.Set("callsign",callsign.c_str());
+                            JsonObject activateResult;
+                            auto thunderController = getThunderControllerClient();
+                            status = thunderController->Invoke<JsonObject, JsonObject>(RDKSHELL_THUNDER_TIMEOUT, "activate", activateParams, activateResult);
+                            gRdkShellMutex.lock();
+                            std::cout << "Bootup Activating ResidentApp from RDKShell without Persistentstore wait with Status:" << status << std::endl;
                         }
                     }
                 }
@@ -2060,7 +2053,17 @@ namespace WPEFramework {
                     }
                     else
                     {
-                        std::cout << "Not launching factory app as conditions not matched\n";
+                        std::cout << "Launch RA after waiting for persistent store\n";
+                        gRdkShellMutex.unlock();
+                        int32_t status = 0;
+                        std::string callsign("ResidentApp");
+                        JsonObject activateParams;
+                        activateParams.Set("callsign",callsign.c_str());
+                        JsonObject activateResult;
+                        auto thunderController = getThunderControllerClient();
+                        status = thunderController->Invoke<JsonObject, JsonObject>(RDKSHELL_THUNDER_TIMEOUT, "activate", activateParams, activateResult);
+                        gRdkShellMutex.lock();
+                        std::cout << "Bootup Activating ResidentApp from RDKShell after Persistentstore wait with Status:" << status << std::endl;
                     }
                   }
                   while (gCreateDisplayRequests.size() > 0)
