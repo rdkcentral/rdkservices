091a3540faae640b07912b69cab28696671a25bf
gsanto722 <grandhi_santoshkumar@comcast.com>
2024-10-17 13:09:42 +0000

    RDK-53549: Blocklist Flag Set and Get in systemservice
    Reason for change: Blocklist Flag Set and Get in systemservice.
    Test Procedure: Refer RDK-53549.
    Risks: Low
    Signed-off-by:gsanto722 <grandhi_santoshkumar@comcast.com>

--- Tests/L2Tests/L2TestsPlugin/tests/SystemService_L2Test.cpp
+++ Tests/L2Tests/L2TestsPlugin/tests/SystemService_L2Test.cpp
@@ -612,5 +612,5 @@ TEST_F(SystemService_L2Test,SystemServiceGetSetBlocklistFlag)
     EXPECT_FALSE(result["success"].Boolean());
 */
 
-    jsonrpc.Unsubscribe(JSON_TIMEOUT, _T("onBlocklistChanged"));
+//    jsonrpc.Unsubscribe(JSON_TIMEOUT, _T("onBlocklistChanged"));
 }
