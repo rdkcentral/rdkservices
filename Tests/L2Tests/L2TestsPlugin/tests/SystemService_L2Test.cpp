195cdda375d33a7709307984a3836bd2730379e1
gsanto722 <grandhi_santoshkumar@comcast.com>
2024-10-21 13:10:00 +0000

    RDK-53549: Blocklist Flag Set and Get in systemservice
    Reason for change: Blocklist Flag Set and Get in systemservice.
    Test Procedure: Refer RDK-53549.
    Risks: Low
    Signed-off-by:gsanto722 <grandhi_santoshkumar@comcast.com>

--- Tests/L2Tests/L2TestsPlugin/tests/SystemService_L2Test.cpp
+++ Tests/L2Tests/L2TestsPlugin/tests/SystemService_L2Test.cpp
@@ -624,6 +624,8 @@ TEST_F(SystemService_L2Test,SystemServiceGetSetBlocklistFlag)
     expected_status.FromString(message);
     EXPECT_CALL(async_handler, onBlocklistChanged(MatchRequestStatus(expected_status)))
         .WillOnce(Invoke(this, &SystemService_L2Test::onBlocklistChanged));
+    signalled = WaitForRequestStatus(JSON_TIMEOUT,SYSTEMSERVICEL2TEST_BLOCKLIST_CHANGED);
+    EXPECT_TRUE(signalled & SYSTEMSERVICEL2TEST_BLOCKLIST_CHANGED);
 
     params["blocklist"] = false;
 
@@ -631,9 +633,6 @@ TEST_F(SystemService_L2Test,SystemServiceGetSetBlocklistFlag)
     EXPECT_EQ(Core::ERROR_NONE, status);
 
     EXPECT_TRUE(result["success"].Boolean());
-    
-    signalled = WaitForRequestStatus(JSON_TIMEOUT,SYSTEMSERVICEL2TEST_BLOCKLIST_CHANGED);
-    EXPECT_TRUE(signalled & SYSTEMSERVICEL2TEST_BLOCKLIST_CHANGED);
 
     status = InvokeServiceMethod("org.rdk.System.1", "getBlocklistFlag", params, result);
     EXPECT_EQ(Core::ERROR_NONE, status);
