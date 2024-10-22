d98074bcc483c04fbc54265fe95c6bb5d2a187af
Adrian Muzyka <adrian.muzyka@consult.red>
2024-10-22 12:45:16 +0200

    RDK-53686 Analytics sysTime validation, generate sessionId and attributes source alignment

--- Tests/L1Tests/tests/test_Analytics.cpp
+++ Tests/L1Tests/tests/test_Analytics.cpp
@@ -23,25 +23,11 @@ protected:
 TEST_F(AnalyticsTest, RegisteredMethods)
 {
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("sendEvent")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setSessionId")));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("setTimeReady")));
 }
 
 TEST_F(AnalyticsTest, sendEvent)
 {
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("sendEvent"), _T("{\"eventName\":\"tile_impression\", \"eventVersion\":\"1\", \"eventSource\":\"ImmerseUI\", \"eventSourceVersion\":\"1.2\", \"cetList\":[\"cet1\",\"cet2\",\"cet3\"], \"eventPayload\": { \"event_trigger\": \"user_key_select\"}}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-}
-
-TEST_F(AnalyticsTest, setSessionId)
-{
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setSessionId"), _T("{\"sessionId\":\"123456789\"}"), response));
-    EXPECT_EQ(response, string("{\"success\":true}"));
-}
-
-TEST_F(AnalyticsTest, setTimeReady)
-{
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setTimeReady"), _T("{}"), response));
     EXPECT_EQ(response, string("{\"success\":true}"));
 }
 
