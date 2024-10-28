d6a2134b86b2998dd8b831649c1686f1cc1cb55f
Adrian Muzyka <adrian.muzyka@consult.red>
2024-10-28 12:46:50 +0100

    RDK-53686 Analytics JsonRpc uses autogenerated API

--- Analytics/Analytics.h
+++ Analytics/Analytics.h
@@ -22,6 +22,8 @@
 #include "Module.h"
 
 #include <interfaces/IAnalytics.h>
+#include <interfaces/json/JsonData_Analytics.h>
+#include <interfaces/json/JAnalytics.h>
 
 namespace WPEFramework {
 
@@ -47,16 +49,8 @@ namespace WPEFramework {
             Analytics& operator=(const Analytics&) = delete;
 
         public:
-            Analytics():
-                mConnectionId(0),
-                mAnalytics(nullptr)
-            {
-                RegisterAll();
-            }
-            virtual ~Analytics()
-            {
-                UnregisterAll();
-            }
+            Analytics();
+            virtual ~Analytics();
             virtual const string Initialize(PluginHost::IShell* shell) override;
             virtual void Deinitialize(PluginHost::IShell* service) override;
             virtual string Information() const override { return {}; }
@@ -71,11 +65,6 @@ namespace WPEFramework {
 
         private:
             void Deactivated(RPC::IRemoteConnection* connection);
-            // JSONRPC methods
-            void RegisterAll();
-            void UnregisterAll();
-
-            uint32_t SendEventWrapper(const JsonObject& parameters, JsonObject& response);
 
         private:
             PluginHost::IShell* mService;
