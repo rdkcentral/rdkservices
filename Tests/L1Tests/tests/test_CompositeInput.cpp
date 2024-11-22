c4008de10c2cad4aca70b088eeb70d0796dac460
aishwariya15 <75303024+aishwariya15@users.noreply.github.com>
2024-11-22 18:12:32 +0530

    Update test_CompositeInput.cpp

--- Tests/L1Tests/tests/test_CompositeInput.cpp
+++ Tests/L1Tests/tests/test_CompositeInput.cpp
@@ -378,7 +378,7 @@ TEST_F(CompositeInputInitializedEventDsTest, videoStreamInfoUpdate)
             [&](const uint32_t, const Core::ProxyType<Core::JSON::IElement>& json) {
                 string text;
                 EXPECT_TRUE(json->ToString(text));
-                EXPECT_EQ(text, string(_T("{\"jsonrpc\":\"2.0\",\"method\":\"client.events.videoStreamInfoUpdate.videoStreamInfoUpdate\",\"params\":{\"id\":0,\"locator\":\"hdmiin:\\/\\/localhost\\/deviceid\\/0\",\"width\":4096,\"height\":2160,\"progressive\":false,\"frameRateN\":24000,\"frameRateD\":1001}}")));
+                EXPECT_EQ(text, string(_T("{\"jsonrpc\":\"2.0\",\"method\":\"client.events.videoStreamInfoUpdate.videoStreamInfoUpdate\",\"params\":{\"id\":0,\"locator\":\"hdmiin:\\/\\/localhost\\/deviceid\\/0\",\"width\":720,\"height\":576,\"progressive\":false,\"frameRateN\":24000,\"frameRateD\":1001}}")));
                 return Core::ERROR_NONE;
             }));
     IARM_Bus_DSMgr_EventData_t eventData;
