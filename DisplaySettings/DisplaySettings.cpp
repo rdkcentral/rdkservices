5c1933f05c8726cc7dcc27a7db4a8aba743fc56d
Jeyasona <Jeyasona_Durairaj@comcast.com>
2025-02-07 11:01:40 +0000

    RDKTV-53675 : Assert failure in systemmode plugin
    Reason for change: Updating changelog and api version
    
    Test Procedure: Nil
    
    Risks: Low
    
    Signed-off-by: Jeyasona D <Jeyasona_Durairaj@comcast.com>

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -85,7 +85,7 @@ using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 2
 #define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 4
+#define API_VERSION_NUMBER_PATCH 5
 
 static bool isCecEnabled = false;
 static bool isResCacheUpdated = false;
