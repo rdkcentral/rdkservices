d5fe75984fb928dd3a3911d9cb3b130aafa01f93
srigayathry.pugazhenthi <srigayathry.pugazhenthi@sky.uk>
2025-02-11 17:45:33 +0000

    Test Commit

--- HdmiCecSource/HdmiCecSource.cpp
+++ HdmiCecSource/HdmiCecSource.cpp
@@ -434,6 +434,7 @@ namespace WPEFramework
  
        const string HdmiCecSource::Initialize(PluginHost::IShell* /* service */)
        {
+	   // Test commit
            LOGWARN("Initlaizing CEC_2");
            string msg;
            HdmiCecSource::_instance = this;
