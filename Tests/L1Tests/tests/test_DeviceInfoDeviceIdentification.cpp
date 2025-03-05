be99adf5e6ffbaa2f150e64f4c0e9bb8800b8804
ramkumarpraba <103193010+ramkumarpraba@users.noreply.github.com>
2025-03-05 20:23:07 +0530

    Update test_DeviceInfoDeviceIdentification.cpp

--- Tests/L1Tests/tests/test_DeviceInfoDeviceIdentification.cpp
+++ Tests/L1Tests/tests/test_DeviceInfoDeviceIdentification.cpp
@@ -55,11 +55,11 @@ namespace Plugin {
 
 class DeviceInfoDeviceIdentificationTest : public ::testing::Test {
 protected:
-    Core::ProxyType<Plugin::DeviceInfoDeviceIdentification> deviceInfoDeviceIdentification;
+    Core::ProxyType<Plugin::DeviceIdentification2> deviceInfoDeviceIdentification;
     Exchange::IDeviceIdentification2* interface;
 
     DeviceInfoDeviceIdentificationTest()
-        : deviceInfoDeviceIdentification(Core::ProxyType<Plugin::DeviceInfoDeviceIdentification>::Create())
+        : deviceInfoDeviceIdentification(Core::ProxyType<Plugin::DeviceIdentification2>::Create())
     {
         interface = static_cast<Exchange::IDeviceIdentification2*>(
             deviceInfoDeviceIdentification->QueryInterface(Exchange::IDeviceIdentification2::ID));
