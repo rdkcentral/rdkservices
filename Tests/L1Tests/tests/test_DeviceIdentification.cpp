2e1049ef452d4f5084027aa851892965a201f349
ramkumarpraba <Ramkumar_Prabaharan@comcast.com>
2025-03-05 15:34:31 +0000

    RDKTV-35484 Add deviceIdentification plugin details into deviceinfo
    Reason for change: Add chipset property to DeviceInfo Plugin
    Test Procedure: verify build success and basic test
    Risks: Low
    Priority: P1
    Signed-off-by: ramkumar_prabaharan@comcast.com

--- Tests/L1Tests/tests/test_DeviceIdentification.cpp
+++ Tests/L1Tests/tests/test_DeviceIdentification.cpp
@@ -10,39 +10,6 @@ using namespace WPEFramework;
 
 using ::testing::NiceMock;
 
-namespace {
-const string testArchitecture = _T("testArchitecture");
-const string testChipset = _T("testChipset");
-const string testFirmwareVersion = _T("testFirmwareVersion");
-const string testId = _T("testIdentity");
-const string deviceId = _T("WPEdGVzdElkZW50aXR5");
-}
-
-namespace WPEFramework {
-namespace Plugin {
-    class DeviceImplementation : public PluginHost::ISubSystem::IIdentifier {
-    public:
-        virtual ~DeviceImplementation() = default;
-
-        string Architecture() const override { return testArchitecture; }
-        string Chipset() const override { return testChipset; }
-        string FirmwareVersion() const override { return testFirmwareVersion; }
-        uint8_t Identifier(const uint8_t length, uint8_t buffer[]) const override
-        {
-            ::memcpy(buffer, testId.c_str(), testId.length());
-
-            return testId.length();
-        }
-
-        BEGIN_INTERFACE_MAP(DeviceImplementation)
-        INTERFACE_ENTRY(PluginHost::ISubSystem::IIdentifier)
-        END_INTERFACE_MAP
-    };
-
-    SERVICE_REGISTRATION(DeviceImplementation, 1, 0);
-}
-}
-
 class DeviceIdentificationTest : public ::testing::Test {
 protected:
     Core::ProxyType<Plugin::DeviceIdentification> plugin;
