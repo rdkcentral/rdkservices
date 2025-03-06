ee500332cd922c6f3af900d12c20389db34c88bb
Tamilselvan arumugam Kesavan <tamilselvan.arumugamkesavan@sky.uk>
2025-03-06 09:09:40 +0000

    RDKTV-35505 : Corrected minor number

--- AVOutput/AVOutput.cpp
+++ AVOutput/AVOutput.cpp
@@ -21,8 +21,8 @@
 #include "AVOutput.h"
 #include "UtilsIarm.h"
 
-#define API_VERSION_NUMBER_MAJOR 2
-#define API_VERSION_NUMBER_MINOR 0
+#define API_VERSION_NUMBER_MAJOR 1
+#define API_VERSION_NUMBER_MINOR 1
 #define API_VERSION_NUMBER_PATCH 0
 
 
