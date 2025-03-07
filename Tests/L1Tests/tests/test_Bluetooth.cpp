adaede60d0d6ab719642a00e708697f20d82d3c6
DarshanSubhashDesale <darshansubhash.d@tataelxsi.co.in>
2025-03-07 21:10:15 +0530

    Update test_Bluetooth.cpp #if0 teardown

--- Tests/L1Tests/tests/test_Bluetooth.cpp
+++ Tests/L1Tests/tests/test_Bluetooth.cpp
@@ -14,7 +14,7 @@ using namespace WPEFramework;
 
 using ::testing::NiceMock;
 
-#if 0
+
 class BluetoothTest : public ::testing::Test {
 protected:
     Core::ProxyType<Plugin::Bluetooth> bluetooth;
@@ -43,22 +43,23 @@ protected:
 	}
     }
 
- //    static void TearDownTestCase() {
- //        // Called once after all test cases have run
- //        std::cout << "Tearing down after all tests are run." << std::endl;
- //        // Clean up tasks such as releasing resources or resetting state
-	// if (p_iarmBusImplMock != nullptr) {
-	//     delete p_iarmBusImplMock;
-	//     p_iarmBusImplMock = nullptr;
-	//     IarmBus::setImpl(nullptr);
-	// }
+    #if 0
+    static void TearDownTestCase() {
+        // Called once after all test cases have run
+        std::cout << "Tearing down after all tests are run." << std::endl;
+        // Clean up tasks such as releasing resources or resetting state
+	if (p_iarmBusImplMock != nullptr) {
+	    delete p_iarmBusImplMock;
+	    p_iarmBusImplMock = nullptr;
+	    IarmBus::setImpl(nullptr);
+	}
 
-	// if(mockBluetoothManagerInstance != nullptr) {
-	//     delete mockBluetoothManagerInstance;
-	//     mockBluetoothManagerInstance = nullptr;
-	// }
- //    }
-
+	if(mockBluetoothManagerInstance != nullptr) {
+	    delete mockBluetoothManagerInstance;
+	    mockBluetoothManagerInstance = nullptr;
+	}
+    }
+    #endif
     void SetUp() override {
     }
 
@@ -2416,4 +2417,4 @@ TEST_F(BluetoothTest, EventCallbackTest) {
         ASSERT_EQ(BTRMGR_RESULT_SUCCESS, mockBluetoothManagerInstance->evBluetoothHandler(eventMsg));
     }
 }
-#endif
+
