046b899ecbf87293e4dea6a9aa2101540b1a72ac
DarshanSubhashDesale <darshansubhash.d@tataelxsi.co.in>
2025-03-11 00:49:29 +0530

    Update test_Bluetooth.cpp

--- Tests/L1Tests/tests/test_Bluetooth.cpp
+++ Tests/L1Tests/tests/test_Bluetooth.cpp
@@ -163,14 +163,14 @@ TEST_F(BluetoothTest, StartScanWrapper_DiscoveryInProgress) {
    
     // First call: Start discovery successfully
    
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("startScan"),
-        _T("{\"timeout\":30, \"profile\":\"LOUDSPEAKER\"}"), response));
-    EXPECT_EQ(response, "{\"status\":\"AVAILABLE\",\"success\":true}");
+    // EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("startScan"),
+    //     _T("{\"timeout\":30, \"profile\":\"LOUDSPEAKER\"}"), response));
+    // EXPECT_EQ(response, "{\"status\":\"AVAILABLE\",\"success\":true}");
 
 //     // Second call: Attempt to start discovery while already running
-//     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("startScan"),
-//         _T("{\"timeout\":30, \"profile\":\"HEADPHONES\"}"), response));
-//     EXPECT_EQ(response, "{\"status\":\"AVAILABLE\",\"success\":true}");
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("startScan"),
+        _T("{\"timeout\":-1, \"profile\":\"HEADPHONES\"}"), response));
+    EXPECT_EQ(response, "{\"status\":\"AVAILABLE\",\"success\":true}");
 }
 
 // #endif
