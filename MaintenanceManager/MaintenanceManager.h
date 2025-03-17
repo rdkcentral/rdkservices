46e4f750ca3d93a68e2b97b6c4a3528323df9e28
Gomathi Shankar <gomathishankar37@gmail.com>
2025-03-17 10:48:38 +0530

    Update MaintenanceManager.h

--- MaintenanceManager/MaintenanceManager.h
+++ MaintenanceManager/MaintenanceManager.h
@@ -34,28 +34,31 @@
 #include "pwrMgr.h"
 #include "maintenanceMGR.h" /* IARM INTERFACE HELPER */
 #endif /* USE_IARMBUS || USE_IARM_BUS */
-
 #include "sysMgr.h"
 #include "rfcapi.h"
 #include "cSettings.h"
 
+/* ---- LOGGING ---- */
 //#ifdef ENABLE_JOURNAL_LOGGING
 #include <systemd/sd-journal.h>
 #define JOURNAL_IDENTIFIER "MaintenanceManager"
+#define MM_FILE_NAME "MaintenanceManager.cpp"
 
-#define MM_LOG(priority, priority_str, format, ...)                             \
-    sd_journal_send("MESSAGE=%s [%s:%d] %s: " format,                           \
-                    priority_str, __FILE__, __LINE__, __func__, ##__VA_ARGS__,  \
-                    "PRIORITY=%i", priority,                                    \
-                    "SYSLOG_IDENTIFIER=%s", JOURNAL_IDENTIFIER,                 \
+#define MM_LOG(priority, priority_str, format, ...)                                 \
+    sd_journal_send("MESSAGE=%s [%s:%d] %s: " format,                               \
+                    priority_str, MM_FILE_NAME, __LINE__, __func__, ##__VA_ARGS__,  \
+                    "PRIORITY=%i", priority,                                        \
+                    "SYSLOG_IDENTIFIER=%s", JOURNAL_IDENTIFIER,                     \
                     NULL)
 
 #define MM_LOGINFO(format, ...) MM_LOG(LOG_INFO, "INFO", format, ##__VA_ARGS__)
 #define MM_LOGWARN(format, ...) MM_LOG(LOG_WARNING, "WARN", format, ##__VA_ARGS__)
 #define MM_LOGERR(format, ...)  MM_LOG(LOG_ERR, "ERROR", format, ##__VA_ARGS__)
-#ifdef DEBUG
-#define MM_LOGDEBUG(format, ...) MM_LOG(LOG_DEBUG, "DEBUG", format, ##__VA_ARGS__)
-#endif
+//#else /* else of ENABLE_JOURNAL_LOGGING */
+//#define MM_LOGINFO(format, ...) LOGINFO(format, ##__VA_ARGS__)
+//#define MM_LOGWARN(format, ...) LOGWARN(format, ##__VA_ARGS__)
+//#define MM_LOGERR(format, ...)  LOGERR(format, ##__VA_ARGS__)
+//#endif /* end of ENABLE_JOURNAL_LOGGING */
 
 /* MaintenanceManager Services Triggered Events. */
 #define EVT_ONMAINTMGRSAMPLEEVENT           "onSampleEvent"
