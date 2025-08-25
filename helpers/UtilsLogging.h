5b65ed4e209b9370f057cb89aa2e86b77cd3d6e0
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-08-25 12:51:36 +0530

    DELIA-68783 : Update UtilsLogging.h

--- helpers/UtilsLogging.h
+++ helpers/UtilsLogging.h
@@ -2,9 +2,9 @@
 
 #include <syscall.h>
 
-enum LogLevel {FATAL_LEVEL = 0, ERROR_LEVEL, WARNING_LEVEL, INFO_LEVEL, DEBUG_LEVEL};
+enum LogLevels {FATAL_LOGLEVEL = 0, ERROR_LOGLEVEL, WARNING_LOGLEVEL, INFO_LOGLEVEL, DEBUG_LOGLEVEL};
 
-static int gDefaultLogLevel = ERROR_LEVEL;
+static int DefaultLogLevel = ERROR_LOGLEVEL;
 
 namespace {
     template <typename T>
@@ -12,14 +12,14 @@ namespace {
         return &dummy;
     }
 
-    static const auto UNUSED_GLOBAL_VARIABLE_gDefaultLogLevel =
-        UNUSED_GLOBAL_VARIABLE_HELPER(gDefaultLogLevel);
+    static const auto UNUSED_GLOBAL_VARIABLE_DefaultLogLevel =
+        UNUSED_GLOBAL_VARIABLE_HELPER(DefaultLogLevel);
 }
 
-#define LOGDBG(fmt, ...) do { if(gDefaultLogLevel >= DEBUG_LEVEL) { fprintf(stderr, "[%d] DEBUG [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
-#define LOGINFO(fmt, ...) do { if(gDefaultLogLevel >= INFO_LEVEL) { fprintf(stderr, "[%d] INFO [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
-#define LOGWARN(fmt, ...) do { if(gDefaultLogLevel >= WARNING_LEVEL) { fprintf(stderr, "[%d] WARN [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
-#define LOGERR(fmt, ...) do { if(gDefaultLogLevel >= ERROR_LEVEL) { fprintf(stderr, "[%d] ERROR [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
+#define LOGDBG(fmt, ...) do { if(DefaultLogLevel >= DEBUG_LOGLEVEL) { fprintf(stderr, "[%d] DEBUG [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
+#define LOGINFO(fmt, ...) do { if(DefaultLogLevel >= INFO_LOGLEVEL) { fprintf(stderr, "[%d] INFO [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
+#define LOGWARN(fmt, ...) do { if(DefaultLogLevel >= WARNING_LOGLEVEL) { fprintf(stderr, "[%d] WARN [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
+#define LOGERR(fmt, ...) do { if(DefaultLogLevel >= ERROR_LOGLEVEL) { fprintf(stderr, "[%d] ERROR [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); }} while (0)
 
 #define LOG_DEVICE_EXCEPTION0() LOGWARN("Exception caught: code=%d message=%s", err.getCode(), err.what());
 #define LOG_DEVICE_EXCEPTION1(param1) LOGWARN("Exception caught" #param1 "=%s code=%d message=%s", param1.c_str(), err.getCode(), err.what());
