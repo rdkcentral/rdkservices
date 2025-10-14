b6802329637595ac1affbe1ad8def36d22b0cc2a
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2025-10-14 23:01:48 +0300

    Delia 69034 delia 68911 8.3 p1v (#6414)
    * Delia 68911 main (#6396)
    
    * DELIA-68911 : Send no "clutSize" in modifyPalettedWatermark (#6389)
    
    Reason for change: Skip palette size parameter.
    Because of watermark plugin, which
    by default having logic that works for
    palette size of 256*4. When same watermark
    plugin is interacted from aamp instead of
    content protection, palette size parameter
    is not passed and defaulted to 256*4 inside
    watermark plugin.
    Test Procedure: None
    Risks: None
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    Co-authored-by: Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
    
    * update changelog and api version
    
    ---------
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    Co-authored-by: Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
    Co-authored-by: apatel859 <48992974+apatel859@users.noreply.github.com>
    
    * Delia 69034 main (#6404)
    
    * DELIA-69034 : handling errors for content protection service (#6387)
    
    Reason for change: Add mapping for Invalid aspect dimension, and
    Watermark perceptibility errors. Fix mapping for DRM entitlement error.
    Test Procedure: None
    Risks: None
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    Co-authored-by: Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
    
    * RDKEMW-8549 : handling errors for content protection service (#6392)
    
    Reason for change: Typo fix.
    Test Procedure: None
    Risks: None
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    
    * update changelog and api version
    
    ---------
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    Co-authored-by: Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>
    Co-authored-by: Anand Kandasamy <37086488+anand-ky@users.noreply.github.com>
    Co-authored-by: apatel859 <48992974+apatel859@users.noreply.github.com>

--- ContentProtection/ContentProtection.cpp
+++ ContentProtection/ContentProtection.cpp
@@ -20,8 +20,8 @@
 #include "ContentProtection.h"
 
 #define API_VERSION_NUMBER_MAJOR 3
-#define API_VERSION_NUMBER_MINOR 0
-#define API_VERSION_NUMBER_PATCH 2
+#define API_VERSION_NUMBER_MINOR 1
+#define API_VERSION_NUMBER_PATCH 0
 
 namespace WPEFramework {
 namespace Plugin {
