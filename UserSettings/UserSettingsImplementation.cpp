6e041e75776ab21b9ef1d5308fc890392cd2b3a8
preeja33 <preeja_raveendran@comcast.com>
2025-10-29 22:34:28 +0530

    Update UserSettingsImplementation.cpp

--- UserSettings/UserSettingsImplementation.cpp
+++ UserSettings/UserSettingsImplementation.cpp
@@ -129,6 +129,7 @@ UserSettingsImplementation::~UserSettingsImplementation()
     LOGINFO("UserSettingsImplementation Destructor");    
     if(_remotStoreObject)
     {
+        _remotStoreObject->Unregister(&_storeNotification);
         _remotStoreObject->Release();
         _remotStoreObject = nullptr;
     }
