697c1715e7a7b34eb0ddee8efb945eb48852a731
preeja33 <preeja_raveendran@comcast.com>
2025-11-03 20:38:14 +0530

    Update DeviceInfoJsonRpc.cpp

--- DeviceInfo/DeviceInfoJsonRpc.cpp
+++ DeviceInfo/DeviceInfoJsonRpc.cpp
@@ -139,7 +139,7 @@ namespace Plugin {
                     result = Core::ERROR_GENERAL;
                 }
             }
-			if(_firmwareVersion->Pdri(value) == Core::ERROR_NONE)
+			if (_firmwareVersion->Pdri(value) == Core::ERROR_NONE)
 	        	response.Pdri = std::move(value);	
 		}
 
