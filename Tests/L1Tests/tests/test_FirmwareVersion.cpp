fd36ca492339e2226277500f4a4dd80d315d409f
preeja33 <preeja_raveendran@comcast.com>
2025-11-06 14:22:28 +0530

    Update test_FirmwareVersion.cpp

--- Tests/L1Tests/tests/test_FirmwareVersion.cpp
+++ Tests/L1Tests/tests/test_FirmwareVersion.cpp
@@ -88,11 +88,12 @@ TEST_F(FirmwareVersionTest, PdriSuccessWithVersion)
     fputs("1.2.3\n", tmpFile);
     fseek(tmpFile, 0, SEEK_SET);
 
-    EXPECT_CALL(wrapsImpl, v_secure_popen(::testing::StrEq("r"), ::testing::StrEq("/usr/bin/mfr_util --PDRIVersion"), ::testing::_))
+    EXPECT_CALL(WrapsImpl, v_secure_popen(::testing::StrEq("r"), ::testing::StrEq("/usr/bin/mfr_util --PDRIVersion"), ::testing::_))
         .WillOnce(::testing::Return(tmpFile));
     
-    EXPECT_CALL(wrapsImpl, v_secure_pclose(tmpFile))
-        .WillOnce(::testing::Return(0))
+    EXPECT_CALL(WrapsImpl, v_secure_pclose(tmpFile))
+        .WillOnce(::testing::Return(0));
+    
     string pdri;
     EXPECT_EQ(Core::ERROR_NONE, interface->Pdri(pdri));
     EXPECT_EQ(pdri, _T("1.2.3"));
