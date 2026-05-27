36ecbfaf8637674f95353ce4eda544c9d90435e9
dkumar798 <dinesh_kumar2@comcast.com>
2026-05-27 15:39:45 +0530

    Update SystemServicesHelper.h

--- SystemServices/SystemServicesHelper.h
+++ SystemServices/SystemServicesHelper.h
@@ -86,7 +86,30 @@
 #define TZ_ACCURACY_INTERIM "INTERIM"
 #define TZ_ACCURACY_FINAL   "FINAL"
 
-#define CAT_DWNLDPROGRESSFILE_AND_GET_INFO "awk '/DOWN:/ { for (i=1; i<=NF; i++) if ($i==\"DOWN:\" && $(i+1) ~ /^[0-9]+$/ && $(i+3) ~ /^[0-9]+$/) { if ($(i+3) > 0) print int(($(i+1) * 100) / $(i+3)); else print -1; } }' /opt/curl_progress | tail -n 1"
+#define CAT_DWNLDPROGRESSFILE_AND_GET_INFO \
+"sh -c '\
+v=$(awk '\''/DOWN:/ { \
+    for (i=1; i<=NF; i++) \
+        if ($i==\"DOWN:\" && $(i+1) ~ /^[0-9]+$/ && $(i+3) ~ /^[0-9]+$/) { \
+            if ($(i+3) > 0) \
+                print int(($(i+1) * 100) / $(i+3)); \
+            else \
+                print -1; \
+            exit \
+        } \
+}'\'' /opt/curl_progress); \
+\
+if [ -n \"$v\" ]; then \
+    echo \"$v\"; \
+else \
+    cat /opt/curl_progress | \
+    tr -s '\''\r'\'' '\''\n'\'' | \
+    tail -n 1 | \
+    sed '\''s/^ *//g'\'' | \
+    sed '\''/^[^M/G]*$/d'\'' | \
+    tr -s '\'' '\'' | \
+    cut -d '\'' '\'' -f3; \
+fi'"
 
 enum eRetval { E_NOK = -1,
     E_OK };
