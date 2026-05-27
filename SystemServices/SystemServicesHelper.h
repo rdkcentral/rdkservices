37814128c72ed1b92f7ff8c358572681da6874c5
dkumar798 <dinesh_kumar2@comcast.com>
2026-05-27 15:34:25 +0530

    RDK-61598: Fix getFirmwareDownloadPercent api failure during CDL Migration
    Reason for change: Fix getFirmwareDownloadPercent api failure during CDL Migration
    Test Procedure: to check getFirmwareDownloadPercent api
    Risks: Low
    version: minor

--- SystemServices/SystemServicesHelper.h
+++ SystemServices/SystemServicesHelper.h
@@ -86,7 +86,7 @@
 #define TZ_ACCURACY_INTERIM "INTERIM"
 #define TZ_ACCURACY_FINAL   "FINAL"
 
-#define CAT_DWNLDPROGRESSFILE_AND_GET_INFO "cat /opt/curl_progress | tr -s '\r' '\n' | tail -n 1 | sed 's/^ *//g' | sed '/^[^M/G]*$/d' | tr -s ' ' | cut -d ' ' -f3"
+#define CAT_DWNLDPROGRESSFILE_AND_GET_INFO "awk '/DOWN:/ { for (i=1; i<=NF; i++) if ($i==\"DOWN:\" && $(i+1) ~ /^[0-9]+$/ && $(i+3) ~ /^[0-9]+$/) { if ($(i+3) > 0) print int(($(i+1) * 100) / $(i+3)); else print -1; } }' /opt/curl_progress | tail -n 1"
 
 enum eRetval { E_NOK = -1,
     E_OK };
